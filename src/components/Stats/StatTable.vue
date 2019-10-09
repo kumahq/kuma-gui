04398e12d808fad56e944fd8e04d61bea72ecf0b
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2019-10-09 16:12:12 -0400

    chore: moved the stat-based components into a directory. created a component for displaying statistics in table format.

---
+++ src/components/Stats/StatTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="stat-table-container">
+    <table class="stat-table">
+      <thead>
+        <tr>
+          <th colspan="2">
+            <slot name="stat-heading"></slot>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in items" :key="index">
+          <td>{{ item.label }}</td>
+          <td>{{ item.value }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.stat-table-container {
+}
+
+.stat-table {
+}
+</style>
