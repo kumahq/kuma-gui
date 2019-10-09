fdf49b50ca218962f60891f9fbb58a696a809762
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2019-10-09 13:58:26 -0400

    component for displaying simple stats

---
+++ src/components/StatBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="stat">
+    <header class="stat__title">
+      <slot name="title"></slot>
+    </header>
+    <div class="stat__content">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.stat {
+}
+
+.stat__title {
+}
+
+.stat__content {
+}
+</style>
