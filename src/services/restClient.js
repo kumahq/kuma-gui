5d86b5b0afd22db338468d90180a1de789d350c3
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2019-11-01 14:48:25 -0400

    env added. REST client created.

--- src/services/restClient.js
+++ src/services/restClient.js
@@ -1,10 +1,37 @@
-import axios from 'axios'
-import Mock from '@/services/mock'
-
-// const api =
+import axios from 'axios';
+import Mock from '@/services/mock';
 
 export default class restClient {
   constructor (options) {
     const opts = options || {}
+
+    this.headers = { 'Cache-Control': 'no-cache' }
+    this.host = opts.url
+
+    this.client = axios.create({
+      baseURL: process.env.VUE_APP_KUMA_API,
+      headers: this.headers,
+      ...opts.axiosConfig
+    })
+  }
+
+  buildUrl (path) {
+    return `${this.host}${path}`
+  }
+
+  get (path, options) {
+    const opts = options || {}
+    const url = this.buildUrl(path)
+
+    const promise = this.client
+      .get(url, opts)
+      .then(response => {
+        return response.data
+      })
+      .catch(error => {
+        console.error(error)
+      })
+
+    return promise
   }
 }
