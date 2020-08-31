0a0bc7a8d4939c6eaad91562730678bb02f4d32b
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2020-08-31 09:38:15 -0400

    Kongponents updates (#60)
    * all Kongponents and Kongponent styles updated to their latest versions. slight eslint update.
    
    Signed-off-by: Daryn St. Pierre <daryn.st.pierre@konghq.com>
    
    * removed an unused computed property.
    
    Signed-off-by: Daryn St. Pierre <daryn.st.pierre@konghq.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,7 @@ module.exports = {
     // error on non-curly statements
     curly: "error",
     // allow debugger during development
-    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0
+    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
+    "no-unused-vars": "off"
   }
 };
