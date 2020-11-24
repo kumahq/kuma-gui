dc9295330272bd43b9c68d155e852230153e1a20
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2020-11-24 14:47:20 +0100

    fix(yaml) K8S YAML should contain mesh and namespace
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- src/components/Skeletons/YamlView.vue
+++ src/components/Skeletons/YamlView.vue
@@ -205,7 +205,7 @@ export default {
       const kubernetes = () => {
         const newObj = {}
         const sourceObj = Object.assign({}, this.content)
-        const { name, type, metadata } = sourceObj
+        const { name, mesh, type } = sourceObj
 
         // all other parts of the Kubernetes object have to be placed under `spec`
         const spec = () => {
@@ -224,15 +224,24 @@ export default {
           return false
         }
 
-        // we remove the `name` and `type` because they need to be placed under `metadata`
-        delete sourceObj.type
-        delete sourceObj.name
-
         // assemble the main part of our object
         newObj.apiVersion = 'kuma.io/v1alpha1'
         newObj.kind = type
-        newObj.metadata = {
-          name: name
+        if (mesh !== undefined) { // mesh can be empty on global scoped objects
+          newObj.mesh = sourceObj.mesh
+        }
+
+        if (name.includes('.')) { // if name from Kuma has '.' it means it's k8s name joined with a namespace by dot
+          const [k8sName, namespace] = name.split('.')
+
+          newObj.metadata = {
+            name: k8sName,
+            namespace: namespace
+          }
+        } else {
+          newObj.metadata = {
+            name: name
+          }
         }
 
         // if there are additional values, place them under `spec` accordingly
