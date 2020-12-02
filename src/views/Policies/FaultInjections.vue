9f2a99cf0c92b10f748aa826eb88d17551504598
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2020-12-02 16:45:51 +0100

    fix(policies) show pagination (#102)
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- src/views/Policies/FaultInjections.vue
+++ src/views/Policies/FaultInjections.vue
@@ -91,7 +91,7 @@
 </template>
 
 <script>
-import { getSome, stripTimes } from '@/helpers'
+import { getOffset, getSome, stripTimes } from '@/helpers'
 import EntityURLControl from '@/components/Utils/EntityURLControl'
 import sortEntities from '@/mixins/EntitySorter'
 import FormatForCLI from '@/mixins/FormatForCLI'
@@ -291,6 +291,13 @@ export default {
 
               // set the selected table row for the first item on page load
               this.$store.dispatch('updateSelectedTableRow', firstItem.name)
+
+              if (response.next) {
+                this.next = getOffset(response.next)
+                this.hasNext = true
+              } else {
+                this.hasNext = false
+              }
 
               this.tableData.data = query
                 ? [entityList]
