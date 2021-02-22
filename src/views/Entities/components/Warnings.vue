c91ce1e698f5fb209045a11802b0d4e0403700f3
Bart Smykla <bartek@smykla.com>
2021-02-22 14:26:38 +0100

    feat(warnings) show mismatch warning in zones view
    If zone CP and global CP versions are different, there will be
    warning shown in Zones view now
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- src/views/Entities/components/Warnings.vue
+++ src/views/Entities/components/Warnings.vue
@@ -28,10 +28,14 @@ import WarningZoneAndKumaDPVersionsIncompatible
   from '@/views/Entities/components/WarningZoneAndKumaDPVersionsIncompatible'
 import WarningUnsupportedKumaDPVersion from '@/views/Entities/components/WarningUnsupportedKumaDPVersion'
 import WarningWrongFormat from '@/views/Entities/components/WarningWrongFormat'
+import WarningZoneAndGlobalCPSVersionsIncompatible
+  from '@/views/Entities/components/WarningZoneAndGlobalCPSVersionsIncompatible'
+
 import {
   INCOMPATIBLE_UNSUPPORTED_ENVOY,
   INCOMPATIBLE_UNSUPPORTED_KUMA_DP,
   INCOMPATIBLE_WRONG_FORMAT,
+  INCOMPATIBLE_ZONE_AND_GLOBAL_CPS_VERSIONS,
   INCOMPATIBLE_ZONE_CP_AND_KUMA_DP_VERSIONS,
 } from '@/dataplane'
 
@@ -54,6 +58,8 @@ export default {
           return WarningWrongFormat
         case INCOMPATIBLE_ZONE_CP_AND_KUMA_DP_VERSIONS:
           return WarningZoneAndKumaDPVersionsIncompatible
+        case INCOMPATIBLE_ZONE_AND_GLOBAL_CPS_VERSIONS:
+          return WarningZoneAndGlobalCPSVersionsIncompatible
         default:
           return WarningDefault
       }
