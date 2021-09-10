38f143f52cb9b265c5fecf28e8c378f050a63468
Tomasz Wylężek <tomwylezek@gmail.com>
2021-09-10 10:59:19 +0200

    chore(kongponents) remove not needed packages
    Signed-off-by: Tomasz Wylężek <tomwylezek@gmail.com>

--- src/kongponents.js
+++ src/kongponents.js
@@ -9,16 +9,10 @@ import KCard from '@kongponents/kcard'
 import KEmptyState from '@kongponents/kemptystate'
 import KIcon from '@kongponents/kicon'
 import KTable from '@kongponents/ktable'
-import KToaster from '@kongponents/ktoaster'
-import KoolTip from '@kongponents/kooltip'
 import KClipboardProvider from '@kongponents/kclipboardprovider'
 import Krumbs from '@kongponents/krumbs'
 import KPop from '@kongponents/kpop'
-import KToggle from '@kongponents/ktoggle'
 import KTabs from '@kongponents/ktabs'
-import KModal from '@kongponents/kmodal'
-import KInput from '@kongponents/kinput'
-import KInputSwitch from '@kongponents/kinputswitch'
 
 Vue.component('KAlert', KAlert)
 Vue.component('KBadge', KBadge)
@@ -27,13 +21,7 @@ Vue.component('KCard', KCard)
 Vue.component('KEmptyState', KEmptyState)
 Vue.component('KIcon', KIcon)
 Vue.component('KTable', KTable)
-Vue.component('KToaster', KToaster)
-Vue.component('KoolTip', KoolTip)
 Vue.component('KClipboardProvider', KClipboardProvider)
 Vue.component('Krumbs', Krumbs)
 Vue.component('KPop', KPop)
 Vue.component('KTabs', KTabs)
-Vue.component('KToggle', KToggle)
-Vue.component('KInput', KInput)
-Vue.component('KInputSwitch', KInputSwitch)
-Vue.component('KModal', KModal)
