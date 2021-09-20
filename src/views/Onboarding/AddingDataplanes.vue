d924f18260e7113eb9e7a643a7ca1bc0273a5694
Tomasz Wylężek <tomwylezek@gmail.com>
2021-09-20 13:09:41 +0200

    feat(onboarding) add demo options
    Signed-off-by: Tomasz Wylężek <tomwylezek@gmail.com>

--- src/views/Onboarding/AddingDataplanes.vue
+++ src/views/Onboarding/AddingDataplanes.vue
@@ -4,28 +4,46 @@
       <OnboardingHeading title="4. Adding New Services" />
     </template>
     <template #content>
-      <div>
-        <KRadio
-          v-model="mode"
-          name="deployment"
-          value="demo"
-        >
-          Demo
-        </KRadio>
-        <KRadio
-          v-model="mode"
-          name="deployment"
-          value="manual"
-        >
-          Manually
-        </KRadio>
+      <div class="h-full w-full flex justify-center items-center">
+        <DemoOption
+          class="mr-24"
+          type="demo"
+          :active="mode === 'demo'"
+          title="Demo app"
+          sub-title="Pre-Configured"
+          @clicked="update('demo')"
+        />
+        <DemoOption
+          type="manually"
+          :active="mode === 'manually'"
+          title="Manually"
+          sub-title="Custom Config"
+          @clicked="update('manually')"
+        />
       </div>
     </template>
     <template #navigation>
       <OnboardingNavigation
         next-step="onboarding-adding-dpp-code"
         previous-step="onboarding-populating-mesh"
-      />
+      >
+        <template #selector>
+          <KRadio
+            v-model="mode"
+            name="deployment"
+            value="demo"
+          >
+            Demo
+          </KRadio>
+          <KRadio
+            v-model="mode"
+            name="deployment"
+            value="manually"
+          >
+            Manually
+          </KRadio>
+        </template>
+      </OnboardingNavigation>
     </template>
   </OnboardingPage>
 </template>
@@ -35,6 +53,7 @@ import { mapGetters, mapMutations } from 'vuex'
 import OnboardingNavigation from '@/views/Onboarding/components/OnboardingNavigation'
 import OnboardingHeading from '@/views/Onboarding/components/OnboardingHeading'
 import OnboardingPage from '@/views/Onboarding/components/OnboardingPage'
+import DemoOption from '@/views/Onboarding/components/DemoOption'
 
 export default {
   name: 'AddingDataplanes',
@@ -42,6 +61,7 @@ export default {
     OnboardingNavigation,
     OnboardingHeading,
     OnboardingPage,
+    DemoOption,
   },
   computed: {
     ...mapGetters({
---
+++ src/views/Onboarding/components/DemoOption.vue
@@ -0,0 +1,63 @@
+<template>
+  <div
+    :class="classes"
+    @click="$emit('clicked')"
+  >
+    <div>
+      <img :src="imageUrl">
+      <div class="ml-3">
+        <p class="uppercase font-bold tracking-wider">
+          {{ title }}
+        </p>
+        <p>{{ subTitle }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DemoOption',
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    subTitle: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {
+    classes() {
+      return ['add-option-box', this.active ? 'add-option-box--active' : '']
+    },
+    imageUrl() {
+      return require(`@/assets/images/new-service-${this.type}.svg?external`)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.add-option-box {
+  @apply w-64 h-64 p-12 cursor-pointer flex items-center justify-center;
+
+  border: 3px solid #c2c2c2;
+  border-radius: 5px;
+  box-shadow: 4px 4px 14px 4px rgba(103, 71, 128, 0.11);
+
+  &--active {
+    border-color: #7b2bbc;
+  }
+}
+</style>
