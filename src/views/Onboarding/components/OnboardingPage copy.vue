2b090e1e8908d94925796897d575519caccce3c3
Tomasz Wylężek <tomwylezek@gmail.com>
2021-10-19 17:04:51 +0200

    wip

--- src/views/Onboarding/components/OnboardingPage copy.vue
+++ src/views/Onboarding/components/OnboardingPage copy.vue
@@ -237,37 +237,21 @@ export default {
 }
 
 .screen {
-  --path1: path('M1444 893h252');
-  --path2: path('M1529 705h232M1452 603h237');
-  --path3: path('M1754 563l-332 332h-76M1444 935l121 121M263 859l156 156');
-  --path4: path('M742 781H556');
-  --path5: path('M697 736H513');
-  --path6: path('M695 783V577');
-  --path7: path('M261 1026V751M509 573V438M1502 415l291 290');
-  --path8: path('M698 912L26 240M1368 411v540l61 61 95-95M1434 3h267l193 192v354');
-  --path9: path('M517 411h342l138 138M1416 573v242l371 323');
-  --path10: path(
-    'M1486 817V612l-146-146M839 243h-97l-83 84v348M1698 1063V817l58-57h122M1069 299L558 810M696 1058H585L468 941V570L322 424'
-  );
-  --path11: path(
-    'M277 528l160 160 236-236 121 121M632 979h-45l-67-67v-86H0M106 669h275M70 707h331M207 745h210M85 784h356M1417 558h228M1609 634h203M528 946h76M619 604v131M1359 567l125 125M1332 594l156 156M1594 1070V959M381 632L260 753'
-  );
-  --path12: path('M605 817V497M1851 959h-518M944 570H390');
-  --path13: path('M638 912H342M635 1139V912');
   position: fixed;
-  width: 100vw;
-  height: 100vh;
   top: 0;
   left: 0;
+  bottom: 0;
+  right: 0;
+  margin: auto;
   z-index: -1;
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
 
   svg {
     width: 1800px;
-    min-height: 1080px;
-    position: absolute;
-    left: 50%;
-    // top: -100px;
-    transform: translateX(-50%);
+    height: auto;
+    flex-shrink: 0;
 
     foreignObject {
       position: relative;
@@ -444,3 +428,4 @@ export default {
   }
 }
 </style>
+
