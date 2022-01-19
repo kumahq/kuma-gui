c4d98a1d3c2902993d5432280dd50e83524c53c1
Tomasz Wylężek <tomwylezek@gmail.com>
2022-01-19 09:34:51 +0100

    feat(policies) add documentation link (#289)
    Signed-off-by: Tomasz Wylężek <tomwylezek@gmail.com>

---
+++ src/components/DocumentationLink/DocumentationLink.spec.ts
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/vue'
+import DocumentationLink from './DocumentationLink.vue'
+
+describe('DocumentationLink.vue', () => {
+  it('renders snapshot', () => {
+    const { container } = render(DocumentationLink, { props: { href: 'https://kuma.io/docs/1.3.2/policies/timeout/' } })
+
+    expect(container).toMatchSnapshot()
+  })
+})
