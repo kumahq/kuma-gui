85e0cccf71cb61656de0ee2ba2436edd17337c72
John Cowen <john.cowen@konghq.com>
2022-12-06 11:20:29 +0000

    Move to @utilities and useStore within useEnv for the minute
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/utilities/useEnv.ts
+++ src/utilities/useEnv.ts
@@ -1,20 +1,21 @@
+import { useStore } from '@/store/store'
 type EnvVars = {
-  KUMA_VERSION: string
   KUMA_DOCS_URL: string
   KUMA_UTM_QUERY_PARAMS: string
   KUMA_FEEDBACK_URL: string
 }
-export const useEnv = ({ version = '' }) => {
+export const useEnv = () => {
+  const store = useStore()
+  const docsVersion = store.getters['config/getKumaDocsVersion']
   const env: EnvVars = {
-    KUMA_VERSION: version,
-    KUMA_DOCS_URL: `${import.meta.env.VITE_DOCS_BASE_URL}/${version}/`,
+    KUMA_DOCS_URL: `${import.meta.env.VITE_DOCS_BASE_URL}/${docsVersion}/`,
     // remove the ? for now incase we need to append to an already query
     // param'ed URL
     KUMA_UTM_QUERY_PARAMS: import.meta.env.VITE_UTM.substring(1),
     KUMA_FEEDBACK_URL: import.meta.env.VITE_FEEDBACK_URL,
   }
 
-  return (key: keyof typeof env) => {
+  return (key: keyof EnvVars) => {
     return env[key]
   }
 }
