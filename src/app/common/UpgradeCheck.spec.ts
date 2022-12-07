f696f63d8dc625ce3a73716390c6529d3d7a89d1
Philipp Rudloff <philipp.rudloff@konghq.com>
2022-12-07 09:29:22 +0100

    chore(deps): removes semver-compare
    Removes semver-compare in favor of using the semver provided compare function. We had both packages installed and only used semver-compare. The latter has last been updated 8 years ago and its source code is no longer available.
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/app/common/UpgradeCheck.spec.ts
+++ src/app/common/UpgradeCheck.spec.ts
@@ -4,11 +4,16 @@ import { flushPromises, mount } from '@vue/test-utils'
 import UpgradeCheck from './UpgradeCheck.vue'
 import { store } from '@/store/store'
 
+function renderComponent() {
+  return mount(UpgradeCheck)
+}
+
 describe('UpgradeCheck.vue', () => {
   test('renders snapshot', async () => {
+    store.state.config.version = '1.2.0'
     store.state.config.tagline = import.meta.env.VITE_NAMESPACE
 
-    const wrapper = mount(UpgradeCheck)
+    const wrapper = renderComponent()
 
     await flushPromises()
     expect(wrapper.html()).toContain('Update')
