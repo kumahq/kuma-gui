0684b37eb7adeb5f7d6adc43170bc39d52029ed8
John Cowen <john.cowen@konghq.com>
2022-12-15 12:24:08 +0000

    Mock out an older version to fire an upgrade
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/common/UpgradeCheck.spec.ts
+++ src/app/common/UpgradeCheck.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, test } from '@jest/globals'
 import { flushPromises, mount } from '@vue/test-utils'
 
 import UpgradeCheck from './UpgradeCheck.vue'
+import container from '@/services/container'
 
 function renderComponent() {
   return mount(UpgradeCheck)
@@ -9,6 +10,15 @@ function renderComponent() {
 
 describe('UpgradeCheck.vue', () => {
   test('renders snapshot', async () => {
+    const env = container.get('env')
+    container.set('env', {
+      var: (key: string) => {
+        if (key === 'KUMA_VERSION') {
+          return '1.2.0'
+        }
+        return env.var(key)
+      },
+    })
     const wrapper = renderComponent()
 
     await flushPromises()
