5fd0877eade98466643c93767cae68b157924237
John Cowen <john.cowen@konghq.com>
2023-01-24 12:08:10 +0000

    Add some test utils, container reset and easy version overwrite func
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/services/development.ts
+++ src/services/development.ts
@@ -2,7 +2,7 @@ import { TOKENS } from './production'
 import { service, injected } from './utils'
 import CookiedEnv from '@/services/env/CookiedEnv'
 
-export { service, constant, get, container, injected, createInjections } from './utils'
+export { service, constant, get, set, container, injected, createInjections } from './utils'
 export { TOKENS } from './production'
 
 TOKENS.Env = service(CookiedEnv, { description: 'Env' })
