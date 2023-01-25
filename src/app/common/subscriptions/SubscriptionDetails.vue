aa7fd84545ffef0434b56edaf23d03d409c3a0c3
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-01-25 16:36:23 +0100

    chore: split words using map
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/app/common/subscriptions/SubscriptionDetails.vue
+++ src/app/common/subscriptions/SubscriptionDetails.vue
@@ -7,22 +7,22 @@
 
       <ul>
         <li v-if="details.globalInstanceId">
-          <strong>Global Instance ID:</strong>&nbsp;
-          <span class="mono">{{ details.globalInstanceId }}</span>
+          <strong>Global Instance ID:</strong>
+          <span>{{ details.globalInstanceId }}</span>
         </li>
 
         <li v-if="details.controlPlaneInstanceId">
-          <strong>Control Plane Instance ID:</strong>&nbsp;
-          <span class="mono">{{ details.controlPlaneInstanceId }}</span>
+          <strong>Control Plane Instance ID:</strong>
+          <span>{{ details.controlPlaneInstanceId }}</span>
         </li>
 
         <li v-if="details.connectTime">
-          <strong>Last Connected:</strong>&nbsp;
+          <strong>Last Connected:</strong>
           {{ humanReadableDate(details.connectTime) }}
         </li>
 
         <li v-if="details.disconnectTime">
-          <strong>Last Disconnected:</strong>&nbsp;
+          <strong>Last Disconnected:</strong>
           {{ humanReadableDate(details.disconnectTime) }}
         </li>
       </ul>
@@ -43,8 +43,8 @@
               v-for="(k, v) in item"
               :key="v"
             >
-              <strong>{{ v }}:</strong>&nbsp;
-              <span class="mono">{{ formatError(formatValue(k)) }}</span>
+              <strong>{{ v }}:</strong>
+              <span>{{ formatError(formatValue(k)) }}</span>
             </li>
           </ul>
         </li>
@@ -73,6 +73,12 @@ import { KAlert, KIcon } from '@kong/kongponents'
 
 import { humanReadableDate } from '@/utilities/helpers'
 
+const map: Record<string, string> = {
+  responsesSent: 'Responses Sent',
+  responsesAcknowledged: 'Responses Acknowledged',
+  responsesRejected: 'Responses Rejected',
+}
+
 const props = defineProps({
   details: {
     type: Object,
@@ -86,13 +92,29 @@ const props = defineProps({
 })
 
 const detailsIterator = computed<Record<string, Record<string, any>>>(() => {
+  let details
   if (props.isDiscoverySubscription) {
     const { lastUpdateTime, total, ...restDetails } = props.details.status
 
-    return restDetails
+    details = restDetails
   }
 
-  return props.details.status?.stat
+  if (props.details.status?.stat) {
+    details = props.details.status?.stat
+  }
+
+  for (const detailProperty in details) {
+    const detail: any = details[detailProperty]
+
+    for (const prop in detail) {
+      if (prop in map) {
+        detail[map[prop]] = detail[prop]
+        delete detail[prop]
+      }
+    }
+  }
+
+  return details
 })
 
 function formatValue(value: string): string {
