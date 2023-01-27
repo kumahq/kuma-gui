b29f345093216730e15faf8603d2c955edebff9b
John Cowen <john.cowen@konghq.com>
2023-01-27 13:06:00 +0000

    Use service container for components
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,15 @@
+import { service, createInjections } from '@/services/utils'
 import KumaLogo from '@/app/common/KumaLogo.vue'
 import GithubButton from 'vue-github-button'
 
+export const TOKENS = {
+  KumaLogo: service(() => KumaLogo, { description: 'KumaLogo' }),
+  GithubButton: service(() => GithubButton, { description: 'GithubButton' }),
+}
 export const [
   useKumaLogo,
   useGithubButton,
-] = [
-  () => KumaLogo,
-  () => GithubButton,
-]
+] = createInjections(
+  TOKENS.KumaLogo,
+  TOKENS.GithubButton,
+)
