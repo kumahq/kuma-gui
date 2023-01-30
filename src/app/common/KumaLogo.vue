4165b1919fce4396484687e303a0d06f457aba9a
John Cowen <john.cowen@konghq.com>
2023-01-30 09:33:46 +0000

    Add some blank lines between SFC Vue elements
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/common/KumaLogo.vue
+++ src/app/common/KumaLogo.vue
@@ -2,12 +2,14 @@
 import { useEnv } from '@/utilities'
 const env = useEnv()
 </script>
+
 <template>
   <img
     src="@/assets/images/product-logo.png"
     :alt="`${env('KUMA_PRODUCT_NAME')} Logo`"
   >
 </template>
+
 <style lang="scss" scoped>
 img {
   max-height: 36px;
