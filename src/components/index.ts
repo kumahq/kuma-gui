e5f18a088dcb612b0a5e81d448a4ab9985466cba
John Cowen <john.cowen@konghq.com>
2023-01-31 11:55:16 +0000

    chore: Move main overview charts into their own component
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,19 @@
 import { service, createInjections } from '@/services/utils'
 import KumaLogo from '@/app/common/KumaLogo.vue'
+import OverviewCharts from '@/app/main-overview/components/OverviewCharts.vue'
 import GithubButton from 'vue-github-button'
 
 export const TOKENS = {
   KumaLogo: service(() => KumaLogo, { description: 'KumaLogo' }),
   GithubButton: service(() => GithubButton, { description: 'GithubButton' }),
+  OverviewCharts: service(() => OverviewCharts, { description: 'OverviewCharts' }),
 }
 export const [
   useKumaLogo,
   useGithubButton,
+  useOverviewCharts,
 ] = createInjections(
   TOKENS.KumaLogo,
   TOKENS.GithubButton,
+  TOKENS.OverviewCharts,
 )
