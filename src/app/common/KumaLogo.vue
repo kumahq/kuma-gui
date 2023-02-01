f32e408a27a08b7480d1c810e7fded95a353154c
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-02-01 09:48:39 +0100

    chore: fixes inconsistent SFC block order
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/app/common/KumaLogo.vue
+++ src/app/common/KumaLogo.vue
@@ -1,14 +1,14 @@
-<script lang="ts" setup>
-import { useEnv } from '@/utilities'
-const env = useEnv()
-</script>
-
 <template>
   <img
     src="@/assets/images/product-logo.png"
     :alt="`${env('KUMA_PRODUCT_NAME')} Logo`"
   >
 </template>
+
+<script lang="ts" setup>
+import { useEnv } from '@/utilities'
+const env = useEnv()
+</script>
 
 <style lang="scss" scoped>
 img {
