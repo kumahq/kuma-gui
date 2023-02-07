f93518026bb88b5403e4f046081d2764163fe3db
John Cowen <john.cowen@konghq.com>
2023-02-07 12:43:15 +0000

    chore: re-arrange test mocking
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/api/setupMockServer.ts
+++ src/api/setupMockServer.ts
@@ -1,12 +1,11 @@
 import { setupServer, SetupServer } from 'msw/node'
-
-import { setupHandlers } from './mocks'
+import type { RestHandler } from 'msw'
 
 /**
  * Sets up a mock server for the use in tests.
  *
  * **IMPORTANT**: Do not import this file in the regular application. Since it imports `msw/node`, this will cause the application to break because it will try to import (require, actually) Node built-ins which aren’t available in browser environments.
  */
-export function setupMockServer(url: string = ''): SetupServer {
-  return setupServer(...setupHandlers(url))
+export function setupMockServer(handlers: RestHandler[]): SetupServer {
+  return setupServer(...handlers)
 }
