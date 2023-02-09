d1c84127025f426a73c0628bd8d660c08ed35ceb
John Cowen <johncowen@users.noreply.github.com>
2023-02-09 11:34:40 +0000

    fix(api): Fixes an issue where KUMA_API_BASE_PATH was not being respected (#639)
    Kuma GUI reads a config value to understand where to find the API endpoints, generally these are just `/`, but they are user configurable using a `KUMA_API_SERVER_GUI_ROOT_URL` environment variable (https://github.com/kumahq/kuma/blob/be5122db4e3377164b3e2e12c641112b08009ce6/pkg/config/api-server/config.go#L30).
    
    If a user configures this value to be anything other than the default, this setting was not being respected in the GUI.
    
    There is a related bug here where `KUMA_API_SERVER_GUI_BASE_PATH` could was also partly not being respected within the GUI code, although when chasing this down, ignoring this value in the GUI code seems to have no clear affect. Therefore whilst this is theoretically wrong, there is no immediately apparent bug.
    
    It all boils down to the difference between `??` and `||`, for once `||` is the correct choice. Unit tests to this and anything computed added.
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/services/env/Env.ts
+++ src/services/env/Env.ts
@@ -24,7 +24,7 @@ export default class Env {
     let _env: EnvInternal = envArgs
     const env = (str: keyof EnvInternal, d: string = '') => this.var(str, _env?.[str] ?? d)
 
-    const config = readPathConfigFromDom()
+    const config = this.getConfig()
     const version = semver(env('KUMA_VERSION', config.version))
 
     const productName = encodeURIComponent(env('KUMA_PRODUCT_NAME'))
@@ -38,13 +38,34 @@ export default class Env {
       // TODO(jc): not totally sure we need to use a regex here, maybe just split and join if not
       KUMA_DOCS_URL: `${env('KUMA_DOCS_URL')}/${version.patch === '0.0.0' ? 'dev' : version.patch.replace(/\.\d+$/, '.x')}`,
       KUMA_VERSION: version.pre,
-      KUMA_API_URL: env('KUMA_API_URL') ?? config.apiUrl,
-      KUMA_BASE_PATH: env('KUMA_BASE_PATH') ?? config.baseGuiPath,
+      KUMA_API_URL: env('KUMA_API_URL') || config.apiUrl,
+      KUMA_BASE_PATH: env('KUMA_BASE_PATH') || config.baseGuiPath,
     }
   }
 
   var(key: keyof EnvVars, d: string = '') {
     return this.env?.[key] ?? d
+  }
+
+  /**
+   * Reads the path config object from a JSON string found in a special script
+   * tag that’s populated during server-side rendering of the Vue application’s
+   * index.html file.
+   */
+  protected getConfig(): PathConfig {
+    const pathConfigNode = document.querySelector('#kuma-config')
+
+    if (pathConfigNode instanceof HTMLScriptElement) {
+      try {
+        return JSON.parse(pathConfigNode.innerText.trim())
+      } catch {
+        // Handled by falling back to a default value.
+      }
+    }
+
+    // Falls back to a sensible default when encountering a malformed JSON payload
+    // or non-replaced template.
+    return PATH_CONFIG_DEFAULT
   }
 }
 
@@ -57,25 +78,4 @@ export function semver(version: string): { major: string, minor: string, patch: 
     patch: `${major}.${minor}.${patch}`,
     pre: `${major}.${minor}.${patch}${pre !== undefined ? `-${pre}` : ''}`,
   }
-}
-
-/**
- * Reads the path config object from a JSON string found in a special script
- * tag that’s populated during server-side rendering of the Vue application’s
- * index.html file.
- */
-function readPathConfigFromDom(): PathConfig {
-  const pathConfigNode = document.querySelector('#kuma-config')
-
-  if (pathConfigNode instanceof HTMLScriptElement) {
-    try {
-      return JSON.parse(pathConfigNode.innerText.trim())
-    } catch {
-      // Handled by falling back to a default value.
-    }
-  }
-
-  // Falls back to a sensible default when encountering a malformed JSON payload
-  // or non-replaced template.
-  return PATH_CONFIG_DEFAULT
 }
