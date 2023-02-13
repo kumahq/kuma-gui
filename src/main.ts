3b34a9dbdeda5c7432842657fe369766de41993a
John Cowen <johncowen@users.noreply.github.com>
2023-02-13 14:51:17 +0000

    chore: Makes App injectable, moves injections to composition root/main (#644)
    This commit re-arranges initializeVue slightly to no longer use get(TOKENS.*) and use parameter/constructor injection instead. This is partly to further hide our service container from the actual application code, partly to move the injections to the very root of the application (main.ts) and partly to allow us to inject App if we ever need to make use of that. We continue to do the majority of our application wiring in @/services and import that into main so we can get the 'application' and call it.
    
    I also added "main": "index.ts" to the package.json meaning kuma-gui can now be used as a real dependency/sub-package.
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,5 @@
-import { createApp } from 'vue'
-import { RouteRecordRaw } from 'vue-router'
-
-import { createRouter } from './router/router'
-import { EnvVars } from '@/services/env/Env'
 import { TOKENS, get } from '@/services'
 import App from './app/App.vue'
-import type { ClientConfigInterface } from '@/store/modules/config/config.types'
-
-/**
- * Initializes and mounts the Vue application.
- *
- * This is a good place to run operations that should ideally be initiated or completed before the Vue application instance exists.
- */
-
-async function initializeVue(
-  env: (key: keyof EnvVars) => string,
-  routes: readonly RouteRecordRaw[],
-  logger: {setup: (config: ClientConfigInterface) => void},
-) {
-  const store = get(TOKENS.store)
-  const kumaApi = get(TOKENS.api)
-
-  document.title = `${env('KUMA_PRODUCT_NAME')} Manager`
-  // during development setBaseUrl also optionally installs MSW mocking via
-  // MockKumaApi
-  kumaApi.setBaseUrl(env('KUMA_API_URL'))
-
-  if (import.meta.env.PROD) {
-    (async () => {
-      const config = await kumaApi.getConfig()
-      logger.setup(config)
-    })()
-  }
-
-  const app = createApp(App)
-
-  app.use(store, get(TOKENS.storeKey))
-
-  await Promise.all([
-    // Fetches basic resources before setting up the router and mounting the
-    // application. This is mainly needed to properly redirect users to the
-    // onboarding flow in the appropriate scenarios.
-    store.dispatch('bootstrap'),
-    // Loads available policy types in order to populate the necessary information used for titling/breadcrumbing and policy lookups in the app.
-    store.dispatch('fetchPolicyTypes'),
-  ])
-
-  const router = await createRouter(routes, env('KUMA_BASE_PATH'))
-
-  app.use(router)
-
-  app.mount('#app')
-}
-
-initializeVue(get(TOKENS.env), get(TOKENS.routes), get(TOKENS.logger))
+(async () => {
+  await get(TOKENS.app)(App)
+})()
