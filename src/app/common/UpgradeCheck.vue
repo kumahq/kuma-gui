9142cc2482671b6a67dc4d613ff75b4a20c9020a
John Cowen <john.cowen@konghq.com>
2023-03-02 12:38:59 +0000

    return early for none matching versions (those that aren't 3 dotted)
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/common/UpgradeCheck.vue
+++ src/app/common/UpgradeCheck.vue
@@ -40,7 +40,8 @@ const env = useEnv()
 const latestVersion = ref('')
 const showNotice = ref(false)
 
-checkVersion()
+checkVersion(env('KUMA_VERSION'))
+
 // mostly taken from semver-compare
 const compare = (a: string, b: string) => {
   const pa = a.split('.')
@@ -48,44 +49,36 @@ const compare = (a: string, b: string) => {
   for (let i = 0; i < 3; i++) {
     const na = Number(pa[i])
     const nb = Number(pb[i])
-    if (i === 0) {
-      // if 'major' isn't a number then prefer the alternate
-      if (isNaN(na)) {
-        return 1
-      } else if (isNaN(nb)) {
-        return 0
-      }
-    }
     if (na > nb) return 1
     if (nb > na) return -1
-    if (!isNaN(na) && isNaN(nb)) return 1
-    if (isNaN(na) && !isNaN(nb)) return -1
   }
   return 0
 }
 
-async function checkVersion(): Promise<void> {
+async function checkVersion(currentVersion: string): Promise<void> {
+  if (!currentVersion.match('^[0-9]+.[0-9]+.[0-9]+$')) {
+    return
+  }
+
   try {
     latestVersion.value = await kumaApi.getLatestVersion()
   } catch (error) {
-    showNotice.value = false
-
     console.error(error)
-  } finally {
-    if (latestVersion.value !== '') {
-      // compare the latest version to the currently running version
-      // but only if we were able to set the latest version in the first place.
-      const comparison = compare(latestVersion.value, env('KUMA_VERSION'))
-      showNotice.value = comparison === 1
-    } else {
-      const timespan = 3 // months
-      const today = new Date()
-      const refDate = new Date('2020-06-03 12:00:00')
-      const later = new Date(refDate.getFullYear(), refDate.getMonth() + timespan, refDate.getDate())
+    return
+  }
+  if (latestVersion.value !== '') {
+    // compare the latest version to the currently running version
+    // but only if we were able to set the latest version in the first place.
+    const comparison = compare(latestVersion.value, currentVersion)
+    showNotice.value = comparison === 1
+  } else {
+    const timespan = 3 // months
+    const today = new Date()
+    const refDate = new Date('2020-06-03 12:00:00')
+    const later = new Date(refDate.getFullYear(), refDate.getMonth() + timespan, refDate.getDate())
 
-      // compare dates and handle the notice accordingly
-      showNotice.value = today.getTime() >= later.getTime()
-    }
+    // compare dates and handle the notice accordingly
+    showNotice.value = today.getTime() >= later.getTime()
   }
 }
 </script>
