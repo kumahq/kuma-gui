d5b3f5673fae49f695986191cf2fffdeb9bab2ee
John Cowen <john.cowen@konghq.com>
2023-03-07 11:36:10 +0000

    Remove the old MockApi Class
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/services/kuma-api/MockKumaApi.ts
+++ src/services/kuma-api/MockKumaApi.ts
@@ -3,20 +3,6 @@ import { setupHandlers, Mocks } from '@/api/mocks'
 import { setupMockWorker } from '@/api/setupMockWorker'
 import Env from '@/services/env/Env'
 
-export default class MockKumaApi extends KumaApi {
-  mocks: Mocks
-  constructor(env: Env, mocks: Mocks) {
-    super(env)
-    this.mocks = mocks
-  }
-
-  setBaseUrl(baseUrl: string): void {
-    super.setBaseUrl(baseUrl)
-    if (this.env.var('KUMA_MOCK_API_ENABLED') === 'true') {
-      setupMockWorker(setupHandlers(this.env.var('KUMA_API_URL'), this.mocks))
-    }
-  }
-}
 export const mockApi = function (parent: typeof KumaApi) {
   const name = `Mock${parent.name}`
   const mocked = {
