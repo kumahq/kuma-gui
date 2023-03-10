4ca91c7bcefb19fc514653d04f5b3121c3b1bd7d
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-03-10 10:10:48 +0100

    feat: determines breadcrumb title dynamically
    Adds a feature to the breadcrumbs component for programmatically determining an entry’s title. This can be used to resolve a resource’s name from, for example, a given ID.
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/shims-vue-router.d.ts
+++ src/shims-vue-router.d.ts
@@ -18,6 +18,13 @@ declare module 'vue-router' {
     breadcrumbTitleParam?: string
 
     /**
+     * Allows routes to define their breadcrumb title programmatically based on current route and store data.
+     *
+     * Takes precedence over the `breadcrumbTitleParam` mechanism.
+     */
+    getBreadcrumbTitle?: (route: RouteLocationNormalizedLoaded, store: Store<State>) => string
+
+    /**
      * Whether a route is part of the onboarding pages.
      */
     onboardingProcess?: boolean
