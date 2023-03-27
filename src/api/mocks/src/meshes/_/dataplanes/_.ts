9a7fbce4a82d817fade33f2a2889f0953b8267c6
John Cowen <john.cowen@konghq.com>
2023-03-27 13:23:24 +0100

    chore: Adds initial gateway mocks
    Signed-off-by: John Cowen <john.cowen@konghq.com>

---
+++ src/api/mocks/src/meshes/_/dataplanes/_.ts
@@ -0,0 +1,52 @@
+import type { EndpointDependencies, MockResponder } from '@/api/mocks/index'
+export default ({ fake }: EndpointDependencies): MockResponder => (req) => {
+  const params = req.params
+  const isGateway = params.name.includes('gateway')
+  const name = `${fake.hacker.noun()}`
+  const zone = `${fake.hacker.noun()}`
+  const isMultizone = true && fake.datatype.boolean()
+  return {
+    headers: {
+    },
+    body: {
+      type: 'Dataplane',
+      mesh: params.mesh,
+      name: params.name,
+      creationTime: '2021-02-17T08:33:36.442044+01:00',
+      modificationTime: '2021-02-17T08:33:36.442044+01:00',
+      networking: {
+        address: fake.internet.ip(),
+        ...(isGateway && {
+          gateway: {
+            tags: {
+              'kuma.io/service': name,
+              ...(isMultizone && {
+                'kuma.io/zone': zone,
+              }),
+            },
+            type: 'BUILTIN',
+          },
+        }),
+        inbound: [
+          {
+            port: fake.internet.port(),
+            servicePort: fake.internet.port(),
+            serviceAddress: fake.internet.ip(),
+            tags: {
+              'kuma.io/protocol': fake.kuma.protocol(),
+              'kuma.io/service': name,
+            },
+          },
+        ],
+        outbound: [
+          {
+            port: fake.internet.port(),
+            tags: {
+              'kuma.io/service': fake.hacker.noun(),
+            },
+          },
+        ],
+      },
+    },
+  }
+}
