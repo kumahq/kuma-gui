b6acef5971dfdebe13368e6b1c70a7323a20897d
John Cowen <john.cowen@konghq.com>
2023-04-18 14:43:50 +0100

    Adds decorates and some handy types
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -37,23 +37,64 @@ type DependencyDefinition = {
   arguments?: Array<RequiredToken | OptionalToken>
   shared?: boolean
   labels?: Token[]
+  decorates?: Token
 }
 export type ServiceDefinition = [Token, DependencyDefinition]
+export type ServiceConfigurator = <T extends Record<string, Token>>(app: T) => ServiceDefinition[]
 export type Alias<T extends (...args: never[]) => unknown> = (...rest: Parameters<T>) => ReturnType<T>
-
+export type Decorator<T extends TokenValue> = TokenType<T>
+export type ReturnDecorated<T extends TokenValue> = () => TokenType<T>
 export const container = createContainer()
 
 //
 export const merge = (...definitions: Array<ServiceDefinition[]>): ServiceDefinition[] => {
   return [...new Map([...definitions.flat()]).entries()]
 }
-export const build = (...entries: Array<ServiceDefinition[]>): ServiceDefinition[] => {
-  const merged = merge(...entries)
+const decorate = (entries: ServiceDefinition[]) => {
+  const map = new Map(entries)
+  entries.forEach(([_token, definition]) => {
+    if (typeof definition.decorates !== 'undefined') {
+      const decoratedToken = definition.decorates
+      const decoratedDefinition = map.get(decoratedToken)
+      if (typeof decoratedDefinition !== 'undefined') {
+        // set up a structure that allows us to only get the decorated service
+        // when we want to. i.e. we might want to call/get the service then execute more code, or
+        // we might want to call/get the service last after executing code
+        const { labels, ...decorated } = decoratedDefinition
+        type DecoratedToken = typeof decoratedToken
+        const TOKEN = token<TokenType<DecoratedToken>>('inner')
+        const WRAPPER_TOKEN = token<ReturnDecorated<DecoratedToken>>('inner.wrapper')
+        map.set(TOKEN, {
+          ...decorated,
+        })
+        map.set(WRAPPER_TOKEN, {
+          service: (): ReturnDecorated<DecoratedToken> => () => get(TOKEN),
+        })
+
+        map.set(decoratedToken,
+          {
+            ...definition,
+            arguments: [
+              WRAPPER_TOKEN,
+              // adding further arguments is currently unsupported
+              // ...(definition.arguments || []),
+            ],
+            labels,
+          },
+        )
+      }
+      definition.decorates = undefined
+    }
+  })
+  return [...map.entries()]
+}
+export const get = <T extends TokenValue>(token: T): TokenType<T> => container.get(token)
+export const build = (...entries: Array<ServiceDefinition[]>): typeof get => {
+  const merged = decorate(merge(...entries))
   merged.forEach(item => service(...item))
-  return merged
+  return get
 }
 
-export const get = <T extends TokenValue>(token: T): TokenType<T> => container.get(token)
 export const createInjections = <T extends TokenValue[]>(
   ...tokens: T
 ): InjectionHooks<T> =>
