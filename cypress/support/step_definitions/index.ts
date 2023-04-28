2212f55a290ea8cff6c02e24e17d57216ac7cb34
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-04-28 10:19:33 +0200

    test(e2e): adds length assertion to step
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- cypress/support/step_definitions/index.ts
+++ cypress/support/step_definitions/index.ts
@@ -91,7 +91,9 @@ Then(/^the "(.*)" element[s]? exist[s]? ([0-9]*) time[s]?$/, async (selector: st
 Then(/^the "(.*)" element(s)? contain[s]?$/, async (selector: string, multiple = '', table: DataTable) => {
   const rows = table.rows()
   if (multiple === 's') {
-    $(selector).each((el, i) => {
+    const tableHeads = $(selector).should('have.length', rows.length)
+
+    tableHeads.each((el, i) => {
       const item = rows[i]
       if (item) {
         cy.wrap(el).contains(item[0])
