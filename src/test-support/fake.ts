b7ea5e327bc0b864931564f69c69dd71eab18629
John Cowen <john.cowen@konghq.com>
2023-04-28 16:30:11 +0100

    Set the paging to use 0
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/test-support/fake.ts
+++ src/test-support/fake.ts
@@ -18,7 +18,8 @@ const pager: Pager = (_total: string | number, req: RestRequest, self) => {
   const total = parseInt(`${_total}`)
   const query = req.url.searchParams
   const size = parseInt(query.get('size') || '10')
-  const offset = parseInt(query.get('offset') || '1')
+  const offset = parseInt(query.get('offset') || '0')
+
   const remaining = total - offset
   const pageTotal = Math.min(size, remaining)
   const next = remaining <= size ? null : `${baseUrl}${self}?offset=${offset + size}`
