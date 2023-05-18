308e3680c24cb28ebbcf7fdc84889ba360231a68
John Cowen <john.cowen@konghq.com>
2023-05-18 10:27:52 +0100

    fix: Using a decorator for cookied env rather than overwriting
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/services/env/CookiedEnv.ts
+++ src/services/env/CookiedEnv.ts
@@ -1,18 +1,19 @@
-import Env from './Env'
-export default class CookiedEnv extends Env {
-  var(...rest: Parameters<Env['var']>) {
-    const cookies = document.cookie.split(';')
-      .map((item) => item.trim())
-      .filter((item) => item !== '')
-      .reduce((prev, item) => {
-        const [key, value] = item.split('=')
-        prev[key] = value
-        return prev
-      }, {} as Record<string, string>)
-    const key = rest[0]
-    if (key in cookies) {
-      return cookies[key]
-    }
-    return super.var(...rest)
+import type { EnvVars } from './Env'
+export default (
+  env: (str: keyof EnvVars, d?: string) => string,
+  cookie: string = document.cookie,
+) => (...rest: Parameters<typeof env>) => {
+  const cookies = cookie.split(';')
+    .map((item) => item.trim())
+    .filter((item) => item !== '')
+    .reduce((prev, item) => {
+      const [key, value] = item.split('=')
+      prev[key] = value
+      return prev
+    }, {} as Record<string, string>)
+  const key = rest[0]
+  if (key in cookies) {
+    return cookies[key]
   }
+  return env(...rest)
 }
