3814142dfd9c012e7349329a7fa76aff0c4be455
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-05-19 17:10:33 +0200

    chore(i18n): moves global messages into dedicated file
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/locales/en-us/index.ts
+++ src/locales/en-us/index.ts
@@ -1,8 +1,10 @@
+import global from './global/index.yaml'
 import http from './http/index.yaml'
 import meshes from '@/app/meshes/locales/en-us/index.yaml'
 import zones from '@/app/zones/locales/en-us/index.yaml'
 
 export default {
+  ...global,
   ...http,
   ...meshes,
   ...zones,
