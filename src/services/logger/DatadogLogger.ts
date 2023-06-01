93fd98170d4d640b7d64892ae94dae970aa38b3f
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-06-01 15:32:43 +0200

    refactor: uses the correct types for DatadogLogger
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/services/logger/DatadogLogger.ts
+++ src/services/logger/DatadogLogger.ts
@@ -1,22 +1,19 @@
 import { datadogLogs } from '@datadog/browser-logs'
 
-import type { EnvVars } from '@/services/env/Env'
+import type Env from '@/services/env/Env'
 import type { ClientConfigInterface } from '@/store/modules/config/config.types'
 
-type Env = (
-  key: keyof Pick<EnvVars,
-  'KUMA_PRODUCT_NAME'
-  >
-) => string
 export default class DatadogLogger {
-  env: Env
-  constructor(env: Env) {
+  env: Env['var']
+
+  constructor(env: Env['var']) {
     this.env = env
   }
 
   setup(config: ClientConfigInterface) {
     if (config.reports.enabled) {
       datadogLogs.init({
+        // This token is called “kuma-gui” in Datadog
         clientToken: 'pub94a0029259f79f29a5d881a06d1e9653',
         site: 'datadoghq.com',
         forwardErrorsToLogs: true,
