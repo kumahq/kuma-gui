889a8d74f4eacf8e32f5ae69d358a7fc7bdf0ca3
John Cowen <john.cowen@konghq.com>
2023-06-01 18:33:31 +0100

    chore: Removes a check for empty states from all tests
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- cypress/support/step_definitions/visit.ts
+++ cypress/support/step_definitions/visit.ts
@@ -12,8 +12,6 @@ When('I visit the {string} URL', function (path: string) {
   cy.visit(`${config.BASE_URL}${path}`)
   // currently use this to denote "the page has initially rendered"
   cy.get('.app-main-content').should('be.visible')
-  cy.wait(1000)
-  cy.get('.empty-state-wrapper').should('not.exist')
 })
 When('I load the {string} URL', function (path: string) {
   cy.viewport(1366, 768)
