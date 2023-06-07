1f8b1ec0e745648c2d6793d051befb7e421f00c3
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-06-07 14:12:48 +0200

    refactor: simplifies status badge class names
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/app/common/StatusBadge.vue
+++ src/app/common/StatusBadge.vue
@@ -3,30 +3,23 @@
     class="status"
     :class="{
       'status--with-title': !props.shouldHideTitle,
-      [`status--${statusObject.appearance}`]: true,
+      [`status--${props.status}`]: true,
     }"
     data-testid="status-badge"
   >
     <span :class="{ 'visually-hidden': props.shouldHideTitle }">
-      {{ statusObject.title }}
+      {{ i18n.t(`http.api.value.${props.status}`) }}
     </span>
   </span>
 </template>
 
 <script lang="ts" setup>
-import { computed, PropType } from 'vue'
+import { PropType } from 'vue'
 
 import { StatusKeyword } from '@/types/index.d'
 import { useI18n } from '@/utilities'
 
 const i18n = useI18n()
-
-const STATUS: Record<StatusKeyword, string> = {
-  not_available: 'not-available',
-  partially_degraded: 'warning',
-  offline: 'danger',
-  online: 'success',
-}
 
 const props = defineProps({
   status: {
@@ -40,11 +33,6 @@ const props = defineProps({
     default: false,
   },
 })
-
-const statusObject = computed(() => ({
-  title: i18n.t(`http.api.value.${props.status}`),
-  appearance: STATUS[props.status],
-}))
 </script>
 
 <style lang="scss" scoped>
@@ -64,19 +52,19 @@ const statusObject = computed(() => ({
   margin-right: var(--spacing-xs);
 }
 
-.status--not-available {
+.status--not_available {
   color: var(--gray-400);
 }
 
-.status--success {
+.status--online {
   color: var(--green-500);
 }
 
-.status--warning {
+.status--partially_degraded {
   color: var(--yellow-500);
 }
 
-.status--danger {
+.status--offline {
   color:  var(--red-600);
 }
 </style>
