7974745b0e855b6491e3903998abd74cc0c1897c
John Cowen <john.cowen@konghq.com>
2023-07-06 12:29:59 +0100

    Use a Set in SharedPool for usage count
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/application/services/data-source/SharedPool.ts
+++ src/app/application/services/data-source/SharedPool.ts
@@ -2,7 +2,7 @@ type Init<K, T> = (key: K) => T | undefined
 type Del<K, T> = (key: K, item: T) => void
 export default class SharedPool<K, T> {
   pool: Map<K, T> = new Map()
-  usage: Map<T, symbol[]> = new Map()
+  usage: Map<T, Set<symbol>> = new Map()
   init: Init<K, T>
   del: (key: K, item: T) => void
   constructor(init: Init<K, T>, del: Del<K, T>) {
@@ -12,29 +12,30 @@ export default class SharedPool<K, T> {
 
   // getter, not init
   acquire(key: K, ref: symbol): T {
+    // there is no way pool/usage.get(item) can be undefined due to using has hence
+    // we use ! to avoid typescript
     if (!this.pool.has(key)) {
       this.pool.set(key, this.init(key) as T)
     }
-    const item = this.pool.get(key) as T
-    if (typeof this.usage.get(item) === 'undefined') {
-      this.usage.set(item, [])
+    const item = this.pool.get(key)!
+    if (!this.usage.has(item)) {
+      this.usage.set(item, new Set<symbol>())
     }
-    const refs = this.usage.get(item) as symbol[]
-    refs.push(ref)
+    const refs = this.usage.get(item)!
+    refs.add(ref)
     return item
   }
 
   // deleter
   release(key: K, ref: symbol) {
+    // there is no way pool/usage.get(item) can be undefined due to using has hence
+    // we use ! to avoid typescript
     if (this.pool.has(key)) {
-      const item = this.pool.get(key) as T
+      const item = this.pool.get(key)!
       if (this.usage.has(item)) {
-        const refs = this.usage.get(item) as symbol[]
-        const pos = refs.findIndex((item) => item === ref)
-        if (pos !== -1) {
-          refs.splice(pos, 1)
-        }
-        if (refs.length === 0) {
+        const refs = this.usage.get(item)!
+        refs.delete(ref)
+        if (refs.size === 0) {
           this.pool.delete(key)
           this.usage.delete(item)
           this.del(key, item)
