fd6dbabbcbe4c58813598d768bf4df8d449739df
John Cowen <john.cowen@konghq.com>
2023-07-06 14:03:26 +0100

    usage > references
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/application/services/data-source/SharedPool.ts
+++ src/app/application/services/data-source/SharedPool.ts
@@ -3,7 +3,7 @@ type Destroyer<K, T> = (key: K, item: T) => void
 
 type Entry<T> = {
   value: T,
-  usage: Set<symbol>
+  references: Set<symbol>
 }
 export default class SharedPool<K, T> {
   pool: Map<K, Entry<T>> = new Map()
@@ -14,28 +14,28 @@ export default class SharedPool<K, T> {
 
   // getter, not init
   acquire(key: K, ref: symbol): T {
-    // there is no way pool/usage.get(item) can be undefined due to using has hence
-    // we use ! to avoid typescript
     if (!this.pool.has(key)) {
-      const usage = {
+      const references = {
         value: this.create(key),
-        usage: new Set<symbol>(),
+        references: new Set<symbol>(),
       }
-      this.pool.set(key, usage)
+      this.pool.set(key, references)
     }
+    // there is no way pool/usage.get(item) can be undefined due to using has
+    // above hence we use ! to avoid typescript
     const item = this.pool.get(key)!
-    item.usage.add(ref)
+    item.references.add(ref)
     return item.value
   }
 
   // deleter
   release(key: K, ref: symbol) {
-    // there is no way pool/usage.get(item) can be undefined due to using has hence
-    // we use ! to avoid typescript
     if (this.pool.has(key)) {
+    // there is no way pool/usage.get(item) can be undefined due to using has
+    // above hence we use ! to avoid typescript
       const item = this.pool.get(key)!
-      item.usage.delete(ref)
-      if (item.usage.size === 0) {
+      item.references.delete(ref)
+      if (item.references.size === 0) {
         this.pool.delete(key)
         this.destroy(key, item.value)
       }
