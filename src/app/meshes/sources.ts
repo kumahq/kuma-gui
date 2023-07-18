a796cbeefd08d1bd3984995523d7e786aab2ec26
John Cowen <john.cowen@konghq.com>
2023-07-18 10:30:11 +0100

    chore: Uses DataSource/AppCollection for Mesh listing
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/meshes/sources.ts
+++ src/app/meshes/sources.ts
@@ -1,18 +1,51 @@
 import { DataSourceResponse } from '@/app/application/services/data-source/DataSourcePool'
 import type KumaApi from '@/services/kuma-api/KumaApi'
 import type {
+  PaginatedApiListResponse as CollectionResponse,
+} from '@/types/api.d'
+import type {
+  Mesh,
   MeshInsight,
 } from '@/types/index.d'
 
 type MeshParams = {
   mesh: string
 }
+type PaginationParams = {
+  size: number
+  page: number
+}
+
+type Closeable = {close: () => void}
+
+export type MeshSource = DataSourceResponse<Mesh>
+export type MeshCollection = CollectionResponse<Mesh>
+export type MeshCollectionSource = DataSourceResponse<MeshCollection>
+
 export type MeshInsightSource = DataSourceResponse<MeshInsight>
+
 export const sources = (api: KumaApi) => {
   return {
-    '/:mesh/insights': async (params: MeshParams, source: {close: () => void}) => {
+    '/:mesh/meshes': async (params: MeshParams & PaginationParams, source: Closeable) => {
       source.close()
-      return api.getMeshInsights({ name: params.mesh })
+      const offset = params.size * params.page
+      return api.getAllMeshes({
+        size: params.size,
+        offset,
+      })
     },
+    '/:mesh/mesh': (params: MeshParams, source: Closeable) => {
+      source.close()
+      return api.getMesh({ name: params.mesh })
+    },
+    '/:mesh/insights': async (params: MeshParams, source: Closeable) => {
+      source.close()
+      if (params.mesh === '*') {
+        return api.getAllMeshInsights()
+      } else {
+        return api.getMeshInsights({ name: params.mesh })
+      }
+    },
+
   }
 }
