004eb0d382456ade8012bb9c21b2da851c92efc8
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-07-19 14:45:05 +0200

    feat: expand app collection (#1203)
    * chore: marks `total` as optional prop
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    * chore: aid syntax highlighting by wrapping `as string` in template
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    * refactor: moves cell attributes function into script block
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    * feat: adds custom toolbar styles
    
    Adds custom toolbar styles so that toolbar entries are automatically placed and spaced in the right position above the table.
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    * feat: adds `rowValue` to AppCollection slow
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    * chore: move cell attrs function back into the template
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    * chore: renames FetcherProps to FetcherParams
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    * chore: annotates cell-attrs parameters
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    * refactor: rework slot splitting
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    * chore: uses :deep
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    ---------
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/app/application/components/app-collection/AppCollection.vue
+++ src/app/application/components/app-collection/AppCollection.vue
@@ -5,7 +5,7 @@
     :pagination-total-items="props.total"
     :initial-fetcher-params="{ page: props.pageNumber, pageSize: props.pageSize }"
     :fetcher-cache-key="String(cacheKey)"
-    :fetcher="({page, pageSize, query}: FetcherProps) => {
+    :fetcher="({ page, pageSize, query }: FetcherParams) => {
       emit('change', {
         page: page,
         size: pageSize,
@@ -13,13 +13,9 @@
       })
       return {data: items}
     }"
-    :cell-attrs="({ headerKey }: any) => {
-      return {
-        class: {
-          [`${headerKey}-column`]: true,
-        },
-      }
-    }"
+    :cell-attrs="({ headerKey }: CellAttrParams) => ({
+      class: `${headerKey}-column`
+    })"
     empty-state-icon-size="96"
     disable-sorting
     hide-pagination-when-optional
@@ -27,22 +23,41 @@
   >
     <template
       v-for="key in Object.keys(slots)"
-      #[`${key}`]="{ row }"
+      :key="key"
+      #[key]="{ row, rowValue }"
     >
-      <slot
-        :name="key"
-        :row="row"
-      />
+      <template
+        v-if="key === 'toolbar'"
+      >
+        <div class="app-collection-toolbar">
+          <slot name="toolbar" />
+        </div>
+      </template>
+
+      <template v-else>
+        <slot
+          :name="key"
+          :row="row"
+          :row-value="rowValue"
+        />
+      </template>
     </template>
   </KTable>
 </template>
+
 <script lang="ts" setup>
 import {
   KTable,
 } from '@kong/kongponents'
 import { useSlots, ref, watch } from 'vue'
 
-type FetcherProps = {
+type CellAttrParams = {
+  headerKey: string
+  row: any
+  rowIndex: number
+  colIndex: number
+}
+type FetcherParams = {
   page: number,
   pageSize: number,
   query: string
@@ -54,7 +69,7 @@ type ChangeValue = {
 }
 
 const props = withDefaults(defineProps<{
-  total: number,
+  total?: number,
   pageNumber: number,
   pageSize: number,
   items: unknown[] | undefined,
@@ -71,6 +86,7 @@ const slots = useSlots()
 
 const items = ref<unknown[] | undefined>(props.items)
 const cacheKey = ref<number>(0)
+
 watch(() => props.items, () => {
   cacheKey.value++
   items.value = props.items
@@ -86,9 +102,18 @@ const click = (e: MouseEvent) => {
 }
 </script>
 <style type="scss" scoped>
-.app-collection :is(td) > :is(a) {
+.app-collection :deep(td:first-of-type > a) {
   color: inherit;
   font-weight: var(--font-weight-semi-bold);
   text-decoration: none;
+}
+
+.app-collection-toolbar {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: var(--spacing-md);
+  padding: var(--spacing-sm) var(--spacing-sm) 0 var(--spacing-sm);
 }
 </style>
