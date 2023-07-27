495f2264c10fc8e18bcd50617d57aa95b07ea928
John Cowen <johncowen@users.noreply.github.com>
2023-07-27 10:12:58 +0100

    chore: Remove dead code (#1247)
    We've rebuilt quite a lot of the application lately, and therefore there is quite a bit of dead code to delete.
    
    This PR deletes some obvious "we don't need these now" files. There might be some other things we don't need still but we can iteratively delete those bits over time.
    
    ---------
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/utilities/kumaDpServerUrl.ts
+++
@@ -1,9 +0,0 @@
-export function kumaDpServerUrl(): string {
-  const dpServerUrl = import.meta.env.VITE_KUMA_DP_SERVER_URL + '/'
-
-  if (import.meta.env.PROD) {
-    return dpServerUrl.replace('localhost', window.location.hostname)
-  } else {
-    return dpServerUrl
-  }
-}
