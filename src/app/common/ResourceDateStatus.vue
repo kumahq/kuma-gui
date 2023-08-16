40e510d22c8e5f0ffbb8d73b84cd941f3ed221ab
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-08-16 14:56:40 +0200

    feat: implements new mesh detail view design (#1342)
    * feat: implements new mesh detail view design
    
    Implements the new mesh detail view design.
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    * chore: renames getBackendData function
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    * chore: annotates return type
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    ---------
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

---
+++ src/app/common/ResourceDateStatus.vue
@@ -0,0 +1,35 @@
+<template>
+  <span class="date-status">
+    {{ t('common.detail.created') }}: {{ createdDate }}
+
+    <KIcon icon="arrowRight" />
+
+    {{ t('common.detail.modified') }}: {{ modifiedDate }}
+  </span>
+</template>
+
+<script lang="ts" setup>
+import { KIcon } from '@kong/kongponents'
+import { computed } from 'vue'
+
+import { useI18n } from '@/utilities'
+
+const { t, formatIsoDate } = useI18n()
+
+const props = defineProps<{
+  creationTime: string
+  modificationTime: string
+}>()
+
+const createdDate = computed(() => formatIsoDate(props.creationTime))
+const modifiedDate = computed(() => formatIsoDate(props.modificationTime))
+</script>
+
+<style lang="scss" scoped>
+.date-status {
+  display: inline-flex;
+  align-items: flex-end;
+  gap: var(--spacing-sm);
+  color: var(--black-300);
+}
+</style>
--- src/test-support/mocks/src/meshes/_.ts
+++ src/test-support/mocks/src/meshes/_.ts
@@ -1,40 +1,105 @@
 import type { EndpointDependencies, MockResponder } from '@/test-support'
-export default (_deps: EndpointDependencies): MockResponder => (req) => {
-  const params = req.params
+export default ({ fake }: EndpointDependencies): MockResponder => (req) => {
+  const name = req.params.mesh
+
   return {
     headers: {},
     body: {
-      name: params.mesh,
+      name,
       type: 'Mesh',
       creationTime: '2020-06-19T12:18:02.097986-04:00',
       modificationTime: '2020-06-19T12:18:02.097986-04:00',
-      mtls: {
-        enabledBackend: 'ca-1',
-        backends: [
-          {
-            name: 'ca-1',
-            type: 'provided',
-            dpCert: {
-              rotation: {
-                expiration: '1d',
+      ...(fake.datatype.boolean() && {
+        mtls: {
+          enabledBackend: 'ca-1',
+          backends: [
+            {
+              name: 'ca-1',
+              type: 'provided',
+              dpCert: {
+                rotation: {
+                  expiration: '1d',
+                },
+              },
+              conf: {
+                cert: {
+                  secret: 'name-of-secret',
+                },
+                key: {
+                  secret: 'name-of-secret',
+                },
               },
             },
-            conf: {
-              cert: {
-                secret: 'name-of-secret',
-              },
-              key: {
-                secret: 'name-of-secret',
+            {
+              name: 'ca-2',
+              type: 'BUILTIN',
+            },
+          ],
+        },
+      }),
+      ...(fake.datatype.boolean() && {
+        logging: {
+          backends: [
+            {
+              name: 'logstash',
+              type: 'tcp',
+              conf: {
+                address: '127.0.0.1:5000',
               },
             },
-          },
-          {
-            name: 'ca-2',
-            type: 'BUILTIN',
-          },
-        ],
-      },
-
+            {
+              name: 'file',
+              type: 'file',
+              conf: {
+                path: '/tmp/service.log',
+              },
+            },
+          ],
+        },
+      }),
+      ...(fake.datatype.boolean() && {
+        metrics: {
+          enabledBackend: 'prometheus-1',
+          backends: [
+            {
+              name: 'prometheus-1',
+              type: 'prometheus',
+              conf: {
+                path: '/non-standard-path',
+                port: 1234,
+              },
+            },
+            {
+              name: 'prometheus-2',
+              type: 'prometheus',
+              conf: {
+                path: '/non-standard-path',
+                port: 1235,
+              },
+            },
+          ],
+        },
+      }),
+      ...(fake.datatype.boolean() && {
+        tracing: {
+          backends: [
+            {
+              name: 'zipkin-us',
+              type: 'zipkin',
+              conf: {
+                url: 'http://zipkin.us:8080/v1/spans',
+              },
+            },
+            {
+              name: 'zipkin-eu',
+              type: 'zipkin',
+              conf: {
+                url: 'http://zipkin.eu:8080/v1/spans',
+              },
+            },
+          ],
+        },
+      }),
     },
   }
 }
