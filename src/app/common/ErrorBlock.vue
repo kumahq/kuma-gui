f5bae634fbff1b606fa222fc92aaea097b08ac51
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-09-20 18:02:48 +0200

    chore: tweaks error block appearance (#1492)
    Changes the badge list to no longer be absolutely position in order to avoid it overlapping other content.
    
    Puts icon and error title in one row.
    
    Removes `details` element when showing invalid parameters.
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/app/common/ErrorBlock.vue
+++ src/app/common/ErrorBlock.vue
@@ -5,42 +5,69 @@
   >
     <KEmptyState cta-is-hidden>
       <template #title>
-        <WarningIcon
-          v-if="props.icon === 'warning'"
-          class="mb-3"
-          :size="KUI_ICON_SIZE_50"
-        />
+        <div class="error-block-header">
+          <div class="error-block-title">
+            <WarningIcon
+              v-if="props.icon === 'warning'"
+              :size="KUI_ICON_SIZE_50"
+            />
 
-        <KIcon
-          v-else
-          class="mb-3"
-          :icon="props.icon"
-          :size="KUI_ICON_SIZE_50"
-        />
+            <KIcon
+              v-else
+              :icon="props.icon"
+              :size="KUI_ICON_SIZE_50"
+            />
 
-        <slot>
-          <p>{{ error instanceof ApiError ? error.detail : t('common.error_state.title') }}</p>
-        </slot>
+            <slot>
+              <p>{{ error instanceof ApiError ? error.detail : t('common.error_state.title') }}</p>
+            </slot>
+          </div>
+
+          <span
+            v-if="(error instanceof ApiError)"
+            class="badge-list"
+          >
+            <KBadge
+              :appearance="props.badgeAppearance"
+              data-testid="error-status"
+            >
+              {{ error.status }}
+            </KBadge>
+
+            <KBadge
+              v-if="error.type"
+              appearance="neutral"
+              data-testid="error-type"
+            >
+              type: {{ error.type }}
+            </KBadge>
+
+            <KBadge
+              v-if="error.instance"
+              appearance="neutral"
+              data-testid="error-trace"
+            >
+              trace: <TextWithCopyButton :text="error.instance" />
+            </KBadge>
+          </span>
+        </div>
       </template>
 
       <template #message>
-        <slot
-          v-if="$slots.message"
-          name="message"
-        />
+        <div class="error-block-message">
+          <slot
+            v-if="$slots.message"
+            name="message"
+          />
 
-        <p v-else>
-          {{ error.message }}
-        </p>
+          <p v-else>
+            {{ error.message }}
+          </p>
 
-        <details
-          v-if="invalidParameters.length > 0"
-          class="error-block-details"
-          data-testid="error-invalid-parameters"
-        >
-          <summary>{{ t('common.error_state.details') }}</summary>
-
-          <ul>
+          <ul
+            v-if="invalidParameters.length > 0"
+            data-testid="error-invalid-parameters"
+          >
             <li
               v-for="(parameter, index) in invalidParameters"
               :key="index"
@@ -48,37 +75,9 @@
               {{ t('common.error_state.field') }} <b><code>{{ parameter.field }}</code></b>: {{ parameter.reason }}
             </li>
           </ul>
-        </details>
+        </div>
       </template>
     </KEmptyState>
-
-    <div
-      v-if="(error instanceof ApiError)"
-      class="badge-list"
-    >
-      <KBadge
-        :appearance="props.badgeAppearance"
-        data-testid="error-status"
-      >
-        {{ error.status }}
-      </KBadge>
-
-      <KBadge
-        v-if="error.type"
-        appearance="neutral"
-        data-testid="error-type"
-      >
-        type: {{ error.type }}
-      </KBadge>
-
-      <KBadge
-        v-if="error.instance"
-        appearance="neutral"
-        data-testid="error-trace"
-      >
-        trace: <TextWithCopyButton :text="error.instance" />
-      </KBadge>
-    </div>
   </div>
 </template>
 
@@ -117,22 +116,37 @@ const invalidParameters = computed(() => props.error instanceof ApiError ? props
 </script>
 
 <style lang="scss" scoped>
-.error-block {
-  position: relative;
+.error-block-header {
+  max-width: 50%;
+  margin-right: auto;
+  margin-left: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: $kui-space-40;
 }
 
-.error-block-details {
+.error-block-title {
+  display: flex;
+  align-items: baseline;
+  gap: $kui-space-40;
+  text-align: left;
+}
+
+.error-block-message {
   text-align: left;
 }
 
 .badge-list {
-  position: absolute;
-  top: 16px;
-  right: 16px;
   display: flex;
+  gap: $kui-space-40;
+  flex-wrap: wrap;
 }
+</style>
 
-.badge-list > * + * {
-  margin-left: $kui-space-40;
+<style lang="scss">
+.error-block-title p {
+  margin-top: 0;
 }
 </style>
