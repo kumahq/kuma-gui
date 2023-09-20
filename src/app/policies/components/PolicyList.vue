6253b0b96e234080169701284681206a9d1a9235
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-09-20 15:04:59 +0200

    chore(policies): allows policy type-specific descriptions (#1505)
    * chore: fix incorrect prop definition
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    * chore(policies): allows policy type-specific descriptions
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    * chore: renames option to defaultMessage
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    ---------
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/app/policies/components/PolicyList.vue
+++ src/app/policies/components/PolicyList.vue
@@ -46,7 +46,7 @@
                   </PolicyTypeTag>
                 </h3>
 
-                <p>{{ t('policies.collection.description') }}</p>
+                <p>{{ t(`policies.type.${props.currentPolicyType.name}.description`, undefined, { defaultMessage: t('policies.collection.description') }) }}</p>
               </div>
 
               <div class="description-actions">
