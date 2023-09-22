f56a7173dcf423c2787309715c9c836325eda997
John Cowen <john.cowen@konghq.com>
2023-09-22 14:11:33 +0100

    http body can be deep
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/test-support/client.ts
+++ src/test-support/client.ts
@@ -15,29 +15,39 @@ type Listener = {
   request: Request
   resolve: (bool: boolean) => void
 }
+
 const equals = (request: Request, item: HistoryEntry) => {
   return Object.entries(request).every(
     ([key, value]) => {
       switch (key) {
         case 'url':
-          return item[key].pathname === String(value)
+          return item.url.pathname === String(value)
         case 'method':
-          return item.request[key] === String(value)
-        case 'body':
-          return Object.entries(request[key]).every(([prop, value]) => {
-            return item.request[key][prop] === String(value)
-          })
+          return item.request.method === String(value)
+        case 'body': {
+          return (function loop(request: Record<string, unknown>, item: Record<string, unknown>): boolean {
+            return Object.entries(request).every(([prop]) => {
+              const req = request[prop] ?? {}
+              const entry = item[prop] ?? {}
+              if (req.constructor === Object && Object.keys(req).length > 0) {
+                return loop(req as Record<string, unknown>, entry as Record<string, unknown>)
+              }
+              return item[prop] === (item[prop] !== null ? String(request[prop]) : request[prop])
+            })
+          })(request.body, item.request.body)
+        }
         case 'searchParams':
-          return Object.entries(request[key]).every(([key, value]) => {
-            const actual = item.url.searchParams.getAll(key)
+          return Object.entries(request[key]).every(([name, param]) => {
+            const actual = item.url.searchParams.getAll(name)
             // convert input to an array
-            const expected = Array.isArray(value) ? value : [value]
+            const expected = Array.isArray(param) ? param : [param]
             return expected.every((item) => {
               return actual.includes(String(item)) === true
             })
           })
+        default:
+          return false
       }
-      return false
     },
   )
 }
