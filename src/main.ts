1433755216ec7fd042469678eb9140c83b12b259
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-10-11 11:10:33 +0200

    fix: stylesheet order being inconsistent between serving and build (#1606)
    Moves the main stylesheet entry point from the index.html file into the main.ts file to guarantee the same stylesheet order between serving the files during development and building for production.
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+// Importing styles here enforces a consistent stylesheet order between the Vite development server and the production build. See https://github.com/vitejs/vite/issues/4890.
+import './assets/styles/main.scss'
+
 import { services as controlPlanes } from '@/app/control-planes'
 import { services as diagnostics } from '@/app/diagnostics'
 import { services as onboarding } from '@/app/onboarding'
