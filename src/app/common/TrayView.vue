94e0bfe5b95c29d9fa539f91bde83548ca07f9ce
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-10-20 12:47:29 +0200

    chore(trayview): switches to scoped styles
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/app/common/TrayView.vue
+++ src/app/common/TrayView.vue
@@ -1,5 +1,6 @@
 <template>
   <KSlideout
+    class="tray"
     close-button-alignment="end"
     :has-overlay="false"
     is-visible
@@ -41,23 +42,23 @@ const emit = defineEmits<{
 .tray-title {
   margin-top: 0;
 }
-</style>
 
-<style lang="scss">
-.k-slideout {
+.tray {
+  // TODO: Remove or replace these once we switch to Kongponents v9 which will deprecate these variables.
   // Overrides KSlideout’s override.
   --KCardPaddingX: #{$kui-space-80} !important;
   --KCardPaddingY: #{$kui-space-80} !important;
+
+  :deep(.panel),
+  :deep(.panel-background),
+  :deep(.panel-background-transparent) {
+    // TODO: Remove this in favor of setting KSlideout’s `props.offsetTop` to `var(--AppHeaderHeight)` once Kongponents v9 is available (which has https://github.com/Kong/kongponents/pull/1769).
+    // Overrides KSlideout’s top offset. `props.offsetTop` doesn’t accept plain CSS values.
+    top: var(--AppHeaderHeight) !important;
+  }
 }
 
-.k-slideout .panel,
-.k-slideout .panel-background,
-.k-slideout .panel-background-transparent {
-  // Overrides KSlideout’s top offset. `props.offsetTop` doesn’t accept plain CSS values.
-  top: var(--AppHeaderHeight) !important;
-}
-
-.k-slideout-header-content {
+.tray :deep(.k-slideout-header-content) {
   padding-right: $kui-space-80;
   padding-left: $kui-space-80;
 }
