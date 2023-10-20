91fa17b825f7fbc41da108477bb747b53b88d3ba
John Cowen <johncowen@users.noreply.github.com>
2023-10-20 11:05:43 +0100

    chore(tests): Removes last in-application reference to VITE_ vars (#1636)
    Removes last runtime-code reference to VITE_ var (we only use this one in tests).
    
    There's one more I found but thats in the vite build config, so not only is that fine because its in VITE but also we don't have our env etc available there, nor should we.
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/services/env/CliEnv.ts
+++ src/services/env/CliEnv.ts
@@ -1,6 +1,6 @@
 import Env, { getPathConfigDefault } from './Env'
 export default class CliEnv extends Env {
   protected getConfig() {
-    return getPathConfigDefault(import.meta.env.VITE_KUMA_API_SERVER_URL)
+    return getPathConfigDefault()
   }
 }
