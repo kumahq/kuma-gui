c5d30091685d64b871fe5e9e622e27306cb4049f
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-11-02 15:05:41 +0100

    refactor(policies): move policy details into view (#1707)
    * refactor(policies): removes unnecessary loading of policy types
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    * refactor(policies): integrates policy details into view
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    ---------
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/app/policies/views/PolicyDetailView.vue
+++ src/app/policies/views/PolicyDetailView.vue
@@ -8,88 +8,93 @@
       policyPath: '',
     }"
   >
-    <DataSource
-      v-slot="{ data: policyTypesData, error: policyTypesError }: PolicyTypeCollectionSource"
-      :src="`/*/policy-types`"
+    <AppView
+      :breadcrumbs="[
+        {
+          to: {
+            name: 'mesh-detail-view',
+            params: {
+              mesh: route.params.mesh,
+            },
+          },
+          text: route.params.mesh,
+        },
+        {
+          to: {
+            name: 'policy-list-view',
+            params: {
+              mesh: route.params.mesh,
+              policyPath: route.params.policyPath,
+            },
+          },
+          text: t('policies.routes.item.breadcrumbs'),
+        },
+      ]"
     >
-      <ErrorBlock
-        v-if="policyTypesError"
-        :error="policyTypesError"
-      />
-
-      <LoadingBlock v-else-if="policyTypesData === undefined" />
-
-      <EmptyBlock v-else-if="policyTypesData.policies.length === 0" />
-
-      <template v-else>
-        <template
-          v-for="currentPolicyType in [policyTypesData.policies.find((item) => item.path === route.params.policyPath) ?? policyTypesData.policies[0]]"
-          :key="currentPolicyType.name"
-        >
-          <AppView
-            :breadcrumbs="[
-              {
-                to: {
-                  name: 'mesh-detail-view',
-                  params: {
-                    mesh: route.params.mesh,
-                  },
-                },
-                text: route.params.mesh,
-              },
-              {
-                to: {
-                  name: 'policy-list-view',
-                  params: {
-                    mesh: route.params.mesh,
-                    policyPath: route.params.policyPath,
-                  },
-                },
-                text: t('policies.routes.item.breadcrumbs'),
-              },
-            ]"
-          >
-            <template #title>
-              <h1>
-                <TextWithCopyButton :text="route.params.policy">
-                  <RouteTitle
-                    :title="t('policies.routes.item.title', { name: route.params.policy })"
-                    :render="true"
-                  />
-                </TextWithCopyButton>
-              </h1>
-            </template>
-
-            <DataSource
-              v-slot="{ data, error }: PolicySource"
-              :src="`/meshes/${route.params.mesh}/policy-path/${currentPolicyType.path}/policy/${route.params.policy}`"
-            >
-              <ErrorBlock
-                v-if="error"
-                :error="error"
-              />
-
-              <LoadingBlock v-else-if="data === undefined" />
-
-              <PolicyDetails
-                v-else
-                :policy="data"
-                :path="currentPolicyType.path"
-                data-testid="detail-view-details"
-              />
-            </DataSource>
-          </AppView>
-        </template>
+      <template #title>
+        <h1>
+          <TextWithCopyButton :text="route.params.policy">
+            <RouteTitle
+              :title="t('policies.routes.item.title', { name: route.params.policy })"
+              :render="true"
+            />
+          </TextWithCopyButton>
+        </h1>
       </template>
-    </DataSource>
+
+      <DataSource
+        v-slot="{ data, error }: PolicySource"
+        :src="`/meshes/${route.params.mesh}/policy-path/${route.params.policyPath}/policy/${route.params.policy}`"
+      >
+        <ErrorBlock
+          v-if="error"
+          :error="error"
+        />
+
+        <LoadingBlock v-else-if="data === undefined" />
+
+        <div
+          v-else
+          class="stack"
+          data-testid="detail-view-details"
+        >
+          <KCard>
+            <template #body>
+              <h2>{{ t('policies.detail.affected_dpps') }}</h2>
+
+              <PolicyConnections
+                class="mt-4"
+                :mesh="route.params.mesh"
+                :policy-name="route.params.policy"
+                :policy-path="route.params.policyPath"
+              />
+            </template>
+          </KCard>
+
+          <ResourceCodeBlock
+            id="code-block-policy"
+            :resource="data"
+            :resource-fetcher="(params) => kumaApi.getSinglePolicyEntity({
+              name: route.params.policy,
+              mesh: route.params.mesh,
+              path: route.params.policyPath,
+            }, params)"
+            is-searchable
+          />
+        </div>
+      </DataSource>
+    </AppView>
   </RouteView>
 </template>
 
 <script lang="ts" setup>
-import PolicyDetails from '../components/PolicyDetails.vue'
-import type { PolicySource, PolicyTypeCollectionSource } from '../sources'
-import EmptyBlock from '@/app/common/EmptyBlock.vue'
+import PolicyConnections from '../components/PolicyConnections.vue'
+import type { PolicySource } from '../sources'
 import ErrorBlock from '@/app/common/ErrorBlock.vue'
 import LoadingBlock from '@/app/common/LoadingBlock.vue'
+import ResourceCodeBlock from '@/app/common/ResourceCodeBlock.vue'
 import TextWithCopyButton from '@/app/common/TextWithCopyButton.vue'
+import { useKumaApi } from '@/utilities'
+
+const kumaApi = useKumaApi()
 </script>
