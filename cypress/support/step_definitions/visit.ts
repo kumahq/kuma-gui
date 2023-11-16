9459e790559c48a8fc0604d88d25ddd2070a8ffa
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-11-16 17:34:56 +0100

    chore: assert against data-testid-root (#1777)
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- cypress/support/step_definitions/visit.ts
+++ cypress/support/step_definitions/visit.ts
@@ -30,7 +30,7 @@ When('I visit the {string} URL', function (path: string) {
     })
   })
   // currently use this to denote "the page has initially rendered"
-  cy.get('.app-main-content').should('be.visible')
+  cy.get('[data-testid-root="mesh-app"]').should('be.visible')
 })
 When('I load the {string} URL', function (path: string) {
   cy.visit(`${path}`)
