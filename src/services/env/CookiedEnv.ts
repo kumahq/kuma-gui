538e924ff65da8c7baead649a2f93d32543e252e
John Cowen <john.cowen@konghq.com>
2024-01-04 17:33:38 +0000

    Add something so we can deeplink to feature flags
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/services/env/CookiedEnv.ts
+++ src/services/env/CookiedEnv.ts
@@ -1,16 +1,36 @@
 import type { EnvVars } from './Env'
-export default (
-  env: (str: keyof EnvVars, d?: string) => string,
-  doc: { cookie: string } = document,
-) => (...rest: Parameters<typeof env>) => {
-  const cookies = doc.cookie.split(';')
+
+const strToEnv = (str: string): [string, string][] => {
+  return str.split(';')
     .map((item) => item.trim())
     .filter((item) => item !== '')
-    .reduce((prev, item) => {
-      const [key, value] = item.split('=')
-      prev[key] = value
-      return prev
-    }, {} as Record<string, string>)
+    .map((item) => {
+      const [key, ...value] = item.split('=')
+      return [key, value.join('=')] as [string, string]
+    })
+    .filter(([key, _value]) => key.startsWith('KUMA_'))
+}
+
+export default (
+  env: (str: keyof EnvVars, d?: string) => string,
+  doc: {
+    cookie: string
+    location: {
+      hash: string
+    }
+  } = document,
+) => (...rest: Parameters<typeof env>) => {
+  if (doc.location.hash.length > 0) {
+    const hashEnv = strToEnv(document.location.hash.substring(1))
+    doc.location.hash = ''
+    if (hashEnv.length > 0) {
+      doc.cookie = hashEnv.map(([key, value]) => `${key}=${value}`).join(';')
+    }
+  }
+  const cookies = strToEnv(doc.cookie).reduce((prev, [key, value]) => {
+    prev[key] = value
+    return prev
+  }, {} as Record<string, string>)
   const key = rest[0]
   if (key in cookies) {
     return cookies[key]
