242ab3197b76b1cfe6007653c0d5a4c940947ad0
Philipp Rudloff <philipp.rudloff@konghq.com>
2024-01-17 09:52:44 +0100

    fix(dataplanes): conf code block being displayed as one line
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/app/data-planes/components/RuleEntryList.vue
+++ src/app/data-planes/components/RuleEntryList.vue
@@ -180,7 +180,7 @@ function getCellAttributes({ headerKey }: any): Record<string, string> {
   }
 }
 
-.policy-type-table :deep(td *) {
+.policy-type-table :deep(td > *) {
   white-space: normal;
   word-break: break-word;
 }
