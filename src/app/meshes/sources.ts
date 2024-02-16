01594209213e2ad69e381ca01e6c45d5e466a844
John Cowen <john.cowen@konghq.com>
2024-02-16 16:09:48 +0000

    refact: moves remaining source definitions to use defineSource
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/meshes/sources.ts
+++ src/app/meshes/sources.ts
@@ -1,16 +1,8 @@
 import { Mesh, MeshInsight } from './data'
+import { defineSources } from '../application/services/data-source'
 import type { DataSourceResponse } from '@/app/application'
 import type KumaApi from '@/services/kuma-api/KumaApi'
 import type { PaginatedApiListResponse as CollectionResponse } from '@/types/api.d'
-
-type DetailParams = {
-  name: string
-}
-
-type PaginationParams = {
-  size: number
-  page: number
-}
 
 export type MeshSource = DataSourceResponse<Mesh>
 
@@ -19,29 +11,29 @@ export type MeshInsightCollection = CollectionResponse<MeshInsight>
 export type MeshInsightCollectionSource = DataSourceResponse<MeshInsightCollection>
 
 export const sources = (api: KumaApi) => {
-  return {
-    '/meshes/:name': async (params: DetailParams) => {
+  return defineSources({
+    '/meshes/:name': async (params) => {
       const { name } = params
 
       return Mesh.fromObject(await api.getMesh({ name }))
     },
 
-    '/meshes/:name/as/kubernetes': (params: DetailParams) => {
+    '/meshes/:name/as/kubernetes': (params) => {
       const { name } = params
       return api.getMesh({ name }, { format: 'kubernetes' })
     },
 
-    '/mesh-insights': async (params: PaginationParams) => {
+    '/mesh-insights': async (params) => {
       const { size } = params
       const offset = params.size * (params.page - 1)
 
       return MeshInsight.fromCollection(await api.getAllMeshInsights({ size, offset }))
     },
 
-    '/mesh-insights/:name': async (params: DetailParams) => {
+    '/mesh-insights/:name': async (params) => {
       const { name } = params
 
       return MeshInsight.fromObject(await api.getMeshInsights({ name }))
     },
-  }
+  })
 }
