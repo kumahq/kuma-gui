a74da3727108581c75d1b6cd9225146437652c59
Philipp Rudloff <philipp.rudloff@konghq.com>
2024-02-16 11:36:49 +0100

    ci: tweak parallelization logic
    Update main.yml workflow to match kong-mesh-gui more closely.
    
    Fix an issue with the Cypress artifact upload failing on account of existing artifact file names.
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

---
+++ scripts/getPartitionedTestFiles.cjs
@@ -0,0 +1,34 @@
+const { sync: globSync } = require('glob')
+
+/**
+ * @param {number} length
+ * @param {string} prefix
+ * @returns {string[]}
+ */
+function getPartitionedTestFiles(length, prefix = '') {
+  const files = globSync(`${prefix}features/**/*.feature`)
+
+  shuffleArray(files)
+
+  /** @type {string[][]} */ const groups = Array.from({ length }, () => [])
+
+  for (let i = 0; i < files.length; i++) {
+    groups[i % length].push(files[i].replace(prefix, ''))
+  }
+
+  return groups.map((group) => group.join(','))
+}
+
+/**
+ * Source: https://stackoverflow.com/a/12646864
+ *
+ * @param {any[]} array
+ */
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]
+  }
+}
+
+module.exports = getPartitionedTestFiles
