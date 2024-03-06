36a46ca65294275328c06950caf40a618c32e4ba
John Cowen <john.cowen@konghq.com>
2024-03-06 14:17:25 +0000

    chore: adds XAction component
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/x/index.ts
+++ src/app/x/index.ts
@@ -1,3 +1,4 @@
+import XAction from './components/x-action/XAction.vue'
 import XTeleportSlot from './components/x-teleport/XTeleportSlot.vue'
 import XTeleportTemplate from './components/x-teleport/XTeleportTemplate.vue'
 import type { ServiceDefinition } from '@/services/utils'
@@ -5,12 +6,21 @@ import { token } from '@/services/utils'
 
 type Token = ReturnType<typeof token>
 
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents {
+    XAction: typeof XAction
+    XTeleportTemplate: typeof XTeleportTemplate
+    XTeleportSlot: typeof XTeleportSlot
+  }
+}
+
 export const services = (app: Record<string, Token>): ServiceDefinition[] => {
   return [
     [token('x.vue.components'),
       {
         service: () => {
           return [
+            ['XAction', XAction],
             ['XTeleportTemplate', XTeleportTemplate],
             ['XTeleportSlot', XTeleportSlot],
           ]
