98a0a6046739fbe212bd416aa6f3c192419564e0
John Cowen <johncowen@users.noreply.github.com>
2024-03-13 13:22:20 +0000

    chore(policies): refactor policies and fix zone links (#2250)
    Closes https://github.com/kumahq/kuma-gui/issues/2162
    
    This PR was mainly a refactor of the PolicyList component to split it into nested routes. But whilst doing this I spotted several tiny bugs that I've fixed up here also, at least one of those bugs will require a fix on 2.6.x using the older version of the code.
    
    A good deal of the green here is taking that code that was in PolicyList and splitting it into two separate routes.
    
    ---------
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/policies/views/PolicyListView.vue
+++ src/app/policies/views/PolicyListView.vue
@@ -5,7 +5,7 @@
   >
     <RouteView
       v-if="me"
-      v-slot="{ route, t }"
+      v-slot="{ route, t, can }"
       name="policy-list-view"
       :params="{
         page: 1,
@@ -15,95 +15,238 @@
         policy: '',
       }"
     >
-      <AppView>
-        <template #title>
-          <h2>
-            <RouteTitle
-              :title="t('policies.routes.items.title')"
-            />
-          </h2>
+      <DataCollection
+        :predicate="(policyType) => typeof policyType !== 'undefined' && policyType.path === route.params.policyPath"
+        :find="true"
+        :items="props.policyTypes ?? [undefined]"
+      >
+        <template #empty>
+          <EmptyBlock>
+            <template #message>
+              {{ t('policies.routes.items.empty') }}
+            </template>
+          </EmptyBlock>
         </template>
+        <template #default="{ items: types }">
+          <template
+            v-for="policyType in [types[0]!]"
+            :key="policyType"
+          >
+            <AppView>
+              <div class="stack">
+                <KCard>
+                  <header>
+                    <div>
+                      <KBadge
+                        v-if="policyType.isExperimental"
+                        appearance="warning"
+                      >
+                        {{ t('policies.collection.beta') }}
+                      </KBadge>
 
-        <!--
-          Load in all the potential policy types.
-          This means we can find an valid 'selected' type.
-          It’s also used for the list of policy types.
-        -->
-        <DataSource
-          v-slot="{ data: policyTypesData, error: policyTypesError }: PolicyTypeCollectionSource"
-          :src="`/*/policy-types`"
-        >
-          <ErrorBlock
-            v-if="policyTypesError"
-            :error="policyTypesError"
-          />
+                      <KBadge
+                        v-if="policyType.isInbound"
+                        appearance="neutral"
+                      >
+                        {{ t('policies.collection.inbound') }}
+                      </KBadge>
 
-          <LoadingBlock v-else-if="policyTypesData === undefined" />
+                      <KBadge
+                        v-if="policyType.isOutbound"
+                        appearance="neutral"
+                      >
+                        {{ t('policies.collection.outbound') }}
+                      </KBadge>
 
-          <EmptyBlock v-else-if="policyTypesData.policies.length === 0" />
+                      <DocumentationLink
+                        :href="t('policies.href.docs', { name: policyType.name })"
+                        data-testid="policy-documentation-link"
+                      >
+                        <span class="visually-hidden">{{ t('common.documentation') }}</span>
+                      </DocumentationLink>
+                    </div>
+                    <h3>
+                      <PolicyTypeTag
+                        :policy-type="policyType.name"
+                      >
+                        {{ t('policies.collection.title', { name: policyType.name }) }}
+                      </PolicyTypeTag>
+                    </h3>
+                  </header>
+                  <!-- eslint-disable-next-line vue/no-v-html -->
+                  <div v-html="t(`policies.type.${policyType.name}.description`, undefined, { defaultMessage: t('policies.collection.description') })" />
+                </KCard>
 
-          <template v-else>
-            <DataSource
-              v-slot="{ data, error }: PolicyCollectionSource"
-              :src="`/meshes/${route.params.mesh}/policy-path/${route.params.policyPath}?page=${route.params.page}&size=${route.params.size}`"
-            >
-              <DataSource
-                v-slot="{ data: meshInsight }: MeshInsightSource"
-                :src="`/mesh-insights/${route.params.mesh}`"
-              >
-                <PolicyList
-                  :page-number="route.params.page"
-                  :page-size="route.params.size"
-                  :mesh="route.params.mesh"
-                  :current-policy-type="policyTypesData.policies.find((policyType) => policyType.path === route.params.policyPath) ?? policyTypesData.policies[0]"
-                  :policy-types="policyTypesData.policies"
-                  :mesh-insight="meshInsight"
-                  :policy-collection="data"
-                  :policy-error="error"
-                  :is-selected-row="(row) => row.name === route.params.policy"
-                  @change="route.update"
-                />
-
-                <RouterView
-                  v-if="route.params.policy"
-                  v-slot="child"
-                >
-                  <SummaryView
-                    @close="route.replace({
-                      name: 'policy-list-view',
-                      params: {
-                        mesh: route.params.mesh,
-                        policyPath: route.params.policyPath,
-                      },
-                      query: {
-                        page: route.params.page,
-                        size: route.params.size,
-                      },
-                    })"
+                <KCard>
+                  <DataLoader
+                    v-slot="{ data }: PolicyCollectionSource"
+                    :src="`/meshes/${route.params.mesh}/policy-path/${route.params.policyPath}?page=${route.params.page}&size=${route.params.size}`"
+                    :loader="false"
                   >
-                    <component
-                      :is="child.Component"
-                      :policy="data?.items.find((item) => item.name === route.params.policy)"
-                      :policy-type="policyTypesData.policies.find((policyType) => policyType.path === route.params.policyPath)"
-                    />
-                  </SummaryView>
-                </RouterView>
-              </DataSource>
-            </DataSource>
+                    <AppCollection
+                      class="policy-collection"
+                      data-testid="policy-collection"
+                      :empty-state-message="t('common.emptyState.message', { type: `${policyType.name} policies` })"
+                      :empty-state-cta-to="t('policies.href.docs', { name: policyType.name })"
+                      :empty-state-cta-text="t('common.documentation')"
+                      :headers="[
+                        { label: 'Name', key: 'name' },
+                        ...(can('use zones') && policyType.isTargetRefBased || policyType.name === 'MeshGateway' ? [{ label: 'Zone', key: 'zone' }] : []),
+                        ...(policyType.isTargetRefBased ? [{ label: 'Target ref', key: 'targetRef' }] : []),
+                        { label: 'Details', key: 'details', hideLabel: true },
+                      ]"
+                      :page-number="route.params.page"
+                      :page-size="route.params.size"
+                      :total="data?.total"
+                      :items="data?.items"
+                      :is-selected-row="(row) => row.name === route.params.policy"
+                      @change="route.update"
+                    >
+                      <template #name="{ row }">
+                        <RouterLink
+                          :to="{
+                            name: 'policy-summary-view',
+                            params: {
+                              mesh: row.mesh,
+                              policyPath: policyType.path,
+                              policy: row.name,
+                            },
+                            query: {
+                              page: route.params.page,
+                              size: route.params.size,
+                            },
+                          }"
+                        >
+                          {{ row.name }}
+                        </RouterLink>
+                      </template>
+
+                      <template #targetRef="{ row }">
+                        <template v-if="policyType.isTargetRefBased && typeof row.spec?.targetRef !== 'undefined'">
+                          <KBadge appearance="neutral">
+                            {{ row.spec.targetRef.kind }}<span v-if="row.spec.targetRef.name">:<b>{{ row.spec.targetRef.name }}</b></span>
+                          </KBadge>
+                        </template>
+
+                        <template v-else>
+                          {{ t('common.detail.none') }}
+                        </template>
+                      </template>
+
+                      <template #zone="{ row }">
+                        <template v-if="row.labels && row.labels['kuma.io/origin'] === 'zone' && row.labels['kuma.io/zone']">
+                          <RouterLink
+                            :to="{
+                              name: 'zone-cp-detail-view',
+                              params: {
+                                zone: row.labels['kuma.io/zone'],
+                              },
+                            }"
+                          >
+                            {{ row.labels['kuma.io/zone'] }}
+                          </RouterLink>
+                        </template>
+
+                        <template v-else>
+                          {{ t('common.detail.none') }}
+                        </template>
+                      </template>
+
+                      <template #details="{ row }">
+                        <RouterLink
+                          class="details-link"
+                          data-testid="details-link"
+                          :to="{
+                            name: 'policy-detail-view',
+                            params: {
+                              mesh: row.mesh,
+                              policyPath: policyType.path,
+                              policy: row.name,
+                            },
+                          }"
+                        >
+                          {{ t('common.collection.details_link') }}
+
+                          <ArrowRightIcon
+                            display="inline-block"
+                            decorative
+                            :size="KUI_ICON_SIZE_30"
+                          />
+                        </RouterLink>
+                      </template>
+                    </AppCollection>
+                    <RouterView
+                      v-if="route.params.policy"
+                      v-slot="{ Component }"
+                    >
+                      <SummaryView
+                        @close="route.replace({
+                          name: 'policy-list-view',
+                          params: {
+                            mesh: route.params.mesh,
+                            policyPath: route.params.policyPath,
+                          },
+                          query: {
+                            page: route.params.page,
+                            size: route.params.size,
+                          },
+                        })"
+                      >
+                        <component
+                          :is="Component"
+                          :policy="data?.items.find((item) => item.name === route.params.policy)"
+                          :policy-type="policyType"
+                        />
+                      </SummaryView>
+                    </RouterView>
+                  </DataLoader>
+                </KCard>
+              </div>
+            </AppView>
           </template>
-        </DataSource>
-      </AppView>
+        </template>
+      </DataCollection>
     </RouteView>
   </DataSource>
 </template>
 
 <script lang="ts" setup>
-import PolicyList from '../components/PolicyList.vue'
+import { KUI_ICON_SIZE_30 } from '@kong/design-tokens'
+import { ArrowRightIcon } from '@kong/icons'
+
+import type { PolicyType } from '../data'
+import AppCollection from '@/app/application/components/app-collection/AppCollection.vue'
+import DocumentationLink from '@/app/common/DocumentationLink.vue'
 import EmptyBlock from '@/app/common/EmptyBlock.vue'
-import ErrorBlock from '@/app/common/ErrorBlock.vue'
-import LoadingBlock from '@/app/common/LoadingBlock.vue'
+import PolicyTypeTag from '@/app/common/PolicyTypeTag.vue'
 import SummaryView from '@/app/common/SummaryView.vue'
 import type { MeSource } from '@/app/me/sources'
-import type { MeshInsightSource } from '@/app/meshes/sources'
-import type { PolicyCollectionSource, PolicyTypeCollectionSource } from '@/app/policies/sources'
+import type { PolicyCollectionSource } from '@/app/policies/sources'
+const props = defineProps<{
+  policyTypes?: PolicyType[]
+}>()
 </script>
+<style lang="scss" scoped>
+header {
+  display: flow-root;
+}
+header > * {
+  margin-bottom: $kui-space-40;
+}
+header > div {
+  float: right;
+  display: flex;
+  gap: $kui-space-40;
+  align-items: flex-start;
+}
+header > h3 {
+  /* TODO: remove :not(:first-child)s */
+  margin-top: 0;
+  float: left;
+}
+.details-link {
+  display: inline-flex;
+  align-items: center;
+  gap: $kui-space-20;
+}
+</style>
---
+++ src/app/policies/views/PolicyTypeListView.vue
@@ -0,0 +1,154 @@
+<template>
+  <DataSource
+    v-slot="{ data: me }: MeSource"
+    src="/me"
+  >
+    <RouteView
+      v-if="me"
+      v-slot="{ route, t }"
+      name="policy-list-view"
+      :params="{
+        page: 1,
+        size: me.pageSize,
+        mesh: '',
+        policyPath: '',
+        policy: '',
+      }"
+    >
+      <AppView>
+        <template #title>
+          <h2>
+            <RouteTitle
+              :title="t('policies.routes.types.title')"
+            />
+          </h2>
+        </template>
+        <DataSource
+          v-slot="{ data: meshInsight }: MeshInsightSource"
+          :src="`/mesh-insights/${route.params.mesh}`"
+        >
+          <DataSource
+            v-slot="{ data, error }: PolicyTypeCollectionSource"
+            :src="`/*/policy-types`"
+          >
+            <div
+              class="policy-list-content"
+            >
+              <KCard
+                class="policy-type-list"
+                data-testid="policy-type-list"
+              >
+                <!-- block on policy types but not meshInsight -->
+                <DataLoader
+                  :data="[data]"
+                  :errors="[error]"
+                >
+                  <template
+                    v-for="legacy in [typeof meshInsight?.policies === 'undefined' ? data!.policies : data!.policies.filter(item => {
+                      // legacy policies are those that aren't targetRef, aren't MeshGateway and are also in use
+                      return !item.isTargetRefBased && item.name !== 'MeshGateway' && (meshInsight.policies?.[item.name]?.total ?? 0) > 0
+                    })]"
+                    :key="legacy"
+                  >
+                    <DataCollection
+                      v-slot="{ items }"
+                      :predicate="typeof meshInsight?.policies === 'undefined' ? undefined : (item) => legacy.length > 0 || item.isTargetRefBased || item.name === 'MeshGateway'"
+                      :items="data!.policies"
+                    >
+                      <template
+                        v-for="current in [items.find(policyType => policyType.path === route.params.policyPath)]"
+                        :key="current"
+                      >
+                        <div
+                          v-for="policyType in items"
+                          :key="policyType.path"
+                          class="policy-type-link-wrapper"
+                          :class="{
+                            'policy-type-link-wrapper--is-active': current && current.path === policyType.path,
+                          }"
+                        >
+                          <RouterLink
+                            class="policy-type-link"
+                            :to="{
+                              name: 'policy-list-view',
+                              params: {
+                                mesh: route.params.mesh,
+                                policyPath: policyType.path,
+                              },
+                            }"
+                            :data-testid="`policy-type-link-${policyType.name}`"
+                          >
+                            {{ policyType.name }}
+                          </RouterLink>
+
+                          <div class="policy-count">
+                            {{ meshInsight?.policies?.[policyType.name]?.total ?? 0 }}
+                          </div>
+                        </div>
+                      </template>
+                    </DataCollection>
+                  </template>
+                </DataLoader>
+              </KCard>
+              <div class="policy-list">
+                <RouterView v-slot="{ Component }">
+                  <component
+                    :is="Component"
+                    :policy-types="data?.policies"
+                  />
+                </RouterView>
+              </div>
+            </div>
+          </DataSource>
+        </DataSource>
+      </AppView>
+    </RouteView>
+  </DataSource>
+</template>
+
+<script lang="ts" setup>
+import type { MeSource } from '@/app/me/sources'
+import type { MeshInsightSource } from '@/app/meshes/sources'
+import type { PolicyTypeCollectionSource } from '@/app/policies/sources'
+</script>
+<style lang="scss" scoped>
+.policy-list-content {
+  display: flex;
+  gap: $kui-space-80;
+}
+
+.policy-type-list {
+  align-self: flex-start;
+  max-width: 500px;
+}
+
+.policy-list {
+  flex-grow: 1;
+}
+.policy-type-link-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: $kui-space-60;
+}
+
+.policy-type-link-wrapper--is-active {
+  background-color: $kui-color-background-primary-weakest;
+}
+
+.policy-type-link-wrapper:not(.policy-type-link-wrapper--is-active) {
+  color: $kui-color-text-neutral;
+}
+
+.policy-type-link {
+  color: currentColor;
+  flex-grow: 1;
+  padding: $kui-space-40 $kui-space-60;
+}
+
+.policy-count {
+  text-align: right;
+  padding-right: $kui-space-60;
+}
+
+</style>
