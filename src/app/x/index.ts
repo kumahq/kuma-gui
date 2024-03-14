d9406f0e371fbf8ec1ef24e04fb3d1235f850b93
John Cowen <john.cowen@konghq.com>
2024-03-14 18:14:47 +0000

    chore(zones): adds modal layer
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/x/index.ts
+++ src/app/x/index.ts
@@ -1,3 +1,4 @@
+import XDisclosure from './components/x-disclosure/XDisclosure.vue'
 import XTeleportSlot from './components/x-teleport/XTeleportSlot.vue'
 import XTeleportTemplate from './components/x-teleport/XTeleportTemplate.vue'
 import type { ServiceDefinition } from '@/services/utils'
@@ -5,6 +6,13 @@ import { token } from '@/services/utils'
 
 type Token = ReturnType<typeof token>
 
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents {
+    XTeleportTemplate: typeof XTeleportTemplate
+    XTeleportSlot: typeof XTeleportSlot
+    XDisclosure: typeof XDisclosure
+  }
+}
 export const services = (app: Record<string, Token>): ServiceDefinition[] => {
   return [
     [token('x.vue.components'),
@@ -13,6 +21,7 @@ export const services = (app: Record<string, Token>): ServiceDefinition[] => {
           return [
             ['XTeleportTemplate', XTeleportTemplate],
             ['XTeleportSlot', XTeleportSlot],
+            ['XDisclosure', XDisclosure],
           ]
         },
         labels: [
