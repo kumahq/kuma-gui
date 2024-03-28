ae3991639774943e515f97a44bddef43e1f0823e
Philipp Rudloff <philipp.rudloff@konghq.com>
2024-03-28 16:00:28 +0100

    chore: condense template in RuleFilter
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/app/gateways/components/RuleFilter.vue
+++ src/app/gateways/components/RuleFilter.vue
@@ -1,94 +1,56 @@
 <template>
   <div class="filter">
-    <div>
-      <KBadge appearance="neutral">
-        {{ props.filter.type }}
-      </KBadge>
-    </div>
+    <KBadge appearance="neutral">
+      {{ t(`http.api.property.${props.filter.type}`) }}
+    </KBadge>
 
     <div>
       <template v-if="props.filter.type === 'RequestHeaderModifier'">
         <div class="list">
-          <template v-if="props.filter.requestHeaderModifier.add">
-            <div class="list">
-              <KBadge>add</KBadge>
+          <template
+            v-for="(value, key) in props.filter.requestHeaderModifier"
+            :key="key"
+          >
+            <template v-if="value">
+              <div class="list">
+                <KBadge>{{ t(`http.api.property.${key}`) }}</KBadge>
 
-              <span
-                v-for="({ name, value }, index) in props.filter.requestHeaderModifier.add"
-                :key="index"
-              >
-                {{ name }}:{{ value }}
-              </span>
-            </div>
-          </template>
-
-          <template v-if="props.filter.requestHeaderModifier.set">
-            <div class="list">
-              <KBadge>set</KBadge>
-
-              <span
-                v-for="({ name, value }, index) in props.filter.requestHeaderModifier.set"
-                :key="index"
-              >
-                {{ name }}:{{ value }}
-              </span>
-            </div>
-          </template>
-
-          <template v-if="props.filter.requestHeaderModifier.remove">
-            <div class="list">
-              <KBadge>remove</KBadge>
-
-              <span
-                v-for="(name, index) in props.filter.requestHeaderModifier.remove"
-                :key="index"
-              >
-                {{ name }}
-              </span>
-            </div>
+                <span
+                  v-for="(entry, index) in value"
+                  :key="index"
+                >
+                  {{ typeof entry === 'string' ? entry : `${entry.name}:${entry.value}` }}
+                </span>
+              </div>
+            </template>
           </template>
         </div>
       </template>
 
       <template v-else-if="props.filter.type === 'ResponseHeaderModifier'">
         <div class="list">
-          <template v-if="props.filter.responseHeaderModifier.add">
-            <div class="list">
-              <KBadge>add</KBadge>
+          <template
+            v-for="(value, key) in props.filter.responseHeaderModifier"
+            :key="key"
+          >
+            <template v-if="value">
+              <div class="list">
+                <KBadge>{{ t(`http.api.property.${key}`) }}</KBadge>
 
-              <span
-                v-for="({ name, value }, index) in props.filter.responseHeaderModifier.add"
-                :key="index"
-              >
-                {{ name }}:{{ value }}
-              </span>
-            </div>
-          </template>
+                <span
+                  v-for="(entry, index) in value"
+                  :key="index"
+                >
+                  <template v-if="typeof entry === 'string'">
+                    {{ entry }}
+                  </template>
 
-          <template v-if="props.filter.responseHeaderModifier.set">
-            <div class="list">
-              <KBadge>set</KBadge>
-
-              <span
-                v-for="({ name, value }, index) in props.filter.responseHeaderModifier.set"
-                :key="index"
-              >
-                {{ name }}:{{ value }}
-              </span>
-            </div>
-          </template>
-
-          <template v-if="props.filter.responseHeaderModifier.remove">
-            <div class="list">
-              <KBadge>remove</KBadge>
-
-              <span
-                v-for="(name, index) in props.filter.responseHeaderModifier.remove"
-                :key="index"
-              >
-                {{ name }}
-              </span>
-            </div>
+                  <template v-else>
+                    {{ entry.name }}:{{ entry.value }}
+                  </template>
+                </span>
+              </div>
+            </template>
           </template>
         </div>
       </template>
@@ -103,79 +65,49 @@
       </template>
 
       <template v-else-if="props.filter.type === 'RequestRedirect'">
-        <dl class="list">
-          <div v-if="props.filter.requestRedirect.schema">
-            <span class="text-neutral">
-              {{ t('http.api.property.schema') }}:
-            </span>
-            {{ props.filter.requestRedirect.schema }}
-          </div>
+        <div class="list">
+          <div
+            v-for="(value, key) in props.filter.requestRedirect"
+            :key="key"
+          >
+            <template v-if="value">
+              <span class="text-neutral">
+                {{ t(`http.api.property.${key}`) }}:
+              </span>
 
-          <div v-if="props.filter.requestRedirect.hostname">
-            <span class="text-neutral">
-              {{ t('http.api.property.hostname') }}:
-            </span>
-            {{ props.filter.requestRedirect.hostname }}
-          </div>
+              <template v-if="typeof value === 'object'">
+                {{ value.type === 'ReplaceFullPath' ? value.replaceFullPath : value.replacePrefixMatch }}
+              </template>
 
-          <div v-if="props.filter.requestRedirect.port">
-            <span class="text-neutral">
-              {{ t('http.api.property.port') }}:
-            </span>
-            {{ props.filter.requestRedirect.port }}
-          </div>
-
-          <div v-if="props.filter.requestRedirect.path">
-            <span class="text-neutral">
-              {{ t(`http.api.property.${props.filter.requestRedirect.path.type}`) }}:
-            </span>
-
-            <template v-if="props.filter.requestRedirect.path.type === 'ReplaceFullPath'">
-              {{ props.filter.requestRedirect.path.replaceFullPath }}
-            </template>
-
-            <template v-else>
-              {{ props.filter.requestRedirect.path.replacePrefixMatch }}
+              <template v-else>
+                {{ value }}
+              </template>
             </template>
           </div>
-
-          <div v-if="props.filter.requestRedirect.statusCode">
-            <span class="text-neutral">
-              {{ t('http.api.property.statusCode') }}:
-            </span>
-            {{ props.filter.requestRedirect.statusCode }}
-          </div>
-        </dl>
+        </div>
       </template>
 
       <template v-else>
-        <dl class="list">
-          <div v-if="props.filter.urlRewrite.hostToBackendHostname">
-            <span class="text-neutral">
-              {{ t('http.api.property.hostToBackendHostname') }}:
-            </span>
-            {{ props.filter.urlRewrite.hostToBackendHostname }}
-          </div>
+        <div class="list">
+          <div
+            v-for="(value, key) in props.filter.urlRewrite"
+            :key="key"
+          >
+            <template v-if="value">
+              <span class="text-neutral">
+                {{ t(`http.api.property.${key}`) }}:
+              </span>
 
-          <div v-if="props.filter.urlRewrite.hostname">
-            <span class="text-neutral">
-              {{ t('http.api.property.hostname') }}:
-            </span>
-            {{ props.filter.urlRewrite.hostname }}
-          </div>
+              <template v-if="typeof value === 'object'">
+                {{ value.type === 'ReplaceFullPath' ? value.replaceFullPath : value.replacePrefixMatch }}
+              </template>
 
-          <div v-if="props.filter.urlRewrite.path">
-            <span class="text-neutral">
-              {{ t(`http.api.property.${props.filter.urlRewrite.path.type}`) }}:
-            </span>
-            <template v-if="props.filter.urlRewrite.path.type === 'ReplaceFullPath'">
-              {{ props.filter.urlRewrite.path.replaceFullPath }}
-            </template>
-            <template v-else>
-              {{ props.filter.urlRewrite.path.replacePrefixMatch }}
+              <template v-else>
+                {{ value }}
+              </template>
             </template>
           </div>
-        </dl>
+        </div>
       </template>
     </div>
   </div>
