a056b0c03aa21e728f7a543e820653ade4c14284
John Cowen <john.cowen@konghq.com>
2024-04-03 11:18:47 +0100

    temporarily add proxy icon
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/x/components/x-icon/XIcon.vue
+++ src/app/x/components/x-icon/XIcon.vue
@@ -6,12 +6,12 @@
     <component
       :is="icons[props.name]"
       :aria-described-by="slots.default ? id : undefined"
+      :tabindex="slots.default ? 0 : undefined"
       :decorative="!!slots.default"
-      tabindex="0"
       :color="props.color ? props.color : `var(--${props.name}IconColor, 'currentColor')`"
       :size="props.size"
       display="inline-block"
-      class="x-icon-icon"
+      :class="`x-icon-icon x-icon-${props.name}-icon`"
     />
 
     <template #content>
@@ -29,7 +29,6 @@
 <script lang="ts" setup>
 import { KUI_ICON_SIZE_30 } from '@kong/design-tokens'
 import {
-  MeshIcon,
   DeployIcon,
   WarningIcon,
 } from '@kong/icons'
@@ -40,7 +39,7 @@ import { uniqueId } from '@/app/application'
 import AnonymousComponent from '@/app/application/components/anonymous-component/AnonymousComponent.vue'
 
 const icons = {
-  standard: MeshIcon,
+  standard: 'span',
   builtin: DeployIcon,
   delegated: DeployIcon,
   warning: WarningIcon,
@@ -62,10 +61,33 @@ const props = withDefaults(defineProps<{
 <style lang="scss" scoped>
 .x-icon-icon {
   --warningIconColor: #ffa600;
-  --standardIconColor: #250d50;
   --delegatedIconColor: #4ca58e;
   --builtinIconColor: #c25398;
 }
+.x-icon-standard-icon {
+  --icon-before: url('@/assets/images/proxy.svg');
+  --icon-before-color: #250d50;
+}
+.x-icon-standard-icon {
+  &::before {
+    position: relative;
+    top: 2px;
+    mask-repeat: no-repeat;
+    -webkit-mask-repeat: no-repeat;
+    mask-position: center;
+    -webkit-mask-position: center;
+    content: '';
+    background-color: var(--icon-before-color, currentColor);
+    display: inline-flex;
+    margin-top: 2px;
+
+    mask-image: var(--icon-before);
+    -webkit-mask-image: var(--icon-before);
+    width: 16px;
+    height: 16px;
+  }
+}
+
 </style>
 <style lang="scss">
 [id^='-x-icon-tooltip'] ul {
