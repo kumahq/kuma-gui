f8e488a8d08dd377f6d0efe89ffe38924a7b7498
Philipp Rudloff <philipp.rudloff@konghq.com>
2024-04-04 12:29:59 +0200

    refactor: replace CopyButton with KCopy
    Replace the CopyButton in TextWithCopyButton with KCopy as we don’t have more specific needs here.
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/app/common/TextWithCopyButton.vue
+++ src/app/common/TextWithCopyButton.vue
@@ -2,20 +2,14 @@
   <div class="copy-button-wrapper">
     <span class="text"><slot>{{ props.text }}</slot></span>
 
-    <CopyButton
+    <KCopy
+      format="hidden"
       :text="props.text"
-      :copy-text="i18n.t('common.copyText')"
-      :tooltip-success-text="i18n.t('common.copySuccessText')"
     />
   </div>
 </template>
 
 <script lang="ts" setup>
-import CopyButton from './CopyButton.vue'
-import { useI18n } from '@/utilities'
-
-const i18n = useI18n()
-
 const props = defineProps<{
   text: string
 }>()
