fb426328bba6226bc6ddca8f49531426155077c8
Philipp Rudloff <philipp.rudloff@konghq.com>
2024-04-25 17:00:32 +0200

    chore(mk: update install makefiles (release-2.4) (#2500)
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- mk/install.mk
+++ mk/install.mk
@@ -4,15 +4,7 @@ install:
 
 .PHONY: install/sync
 install/sync:
-	make install
-
-.PHONY: clean-install
-clean-install:
 	yarn install --frozen-lockfile
-
-.PHONY: clean-install/sync
-clean-install/sync:
-	make clean-install
 
 .PHONY: clean
 clean: ## Delete all node_modules directories
