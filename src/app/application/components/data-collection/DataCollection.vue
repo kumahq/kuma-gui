9d94c4ca263efce16bbcb3297e4250a34c051047
John Cowen <john.cowen@konghq.com>
2024-04-30 12:14:14 +0100

    chore: add DataCollection#item, EmptyBlock:type
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/application/components/data-collection/DataCollection.vue
+++ src/app/application/components/data-collection/DataCollection.vue
@@ -1,55 +1,86 @@
 <template>
-  <slot
-    v-if="items.length === 0"
-    name="empty"
-    :items="items"
+  <template
+    v-if="slots.item"
   >
-    <EmptyBlock v-if="props.empty" />
-  </slot>
-  <slot
+    <template
+      v-for="item in [props.items.find(props.predicate)]"
+      :key="item"
+    >
+      <slot
+        v-if="item"
+        name="item"
+        :item="item as T"
+      />
+      <slot
+        v-else
+        name="empty"
+        :items="items"
+      >
+        <EmptyBlock
+          v-if="props.empty"
+          :type="props.type"
+        />
+      </slot>
+    </template>
+  </template>
+  <template
     v-else
-    name="default"
-    :items="paginated"
-  />
-
-  <slot
-    v-if="props.page !== 0 && items.length > 0"
-    name="pagination"
-    :items="paginated"
   >
-    <KPagination
-      :total-count="items.length"
-      :current-page="props.page"
-      :initial-page-size="props.pageSize"
-      :page-sizes="[15, 30, 50, 75, 100]"
-      @page-change="({ page }: PaginationChangeEvent) => {
-        emit('change', {
-          page,
-          pageSize: props.pageSize,
-        })
-      }"
+    <slot
+      v-if="items.length === 0"
+      name="empty"
+      :items="items"
+    >
+      <EmptyBlock
+        v-if="props.empty"
+        :type="props.type"
+      />
+    </slot>
+    <slot
+      v-else
+      name="default"
+      :items="paginated"
     />
-  </slot>
+
+    <slot
+      v-if="props.page !== 0 && items.length > 0"
+      name="pagination"
+      :items="paginated"
+    >
+      <KPagination
+        :total-count="items.length"
+        :current-page="props.page"
+        :initial-page-size="props.pageSize"
+        :page-sizes="[15, 30, 50, 75, 100]"
+        @page-change="({ page }: PaginationChangeEvent) => {
+          emit('change', {
+            page,
+            pageSize: props.pageSize,
+          })
+        }"
+      />
+    </slot>
+  </template>
 </template>
 <script lang="ts" generic="T" setup>
-import { computed } from 'vue'
+import { computed, useSlots } from 'vue'
 
 import EmptyBlock from '@/app/common/EmptyBlock.vue'
 type PaginationChangeEvent = {
   page: number
 }
 const props = withDefaults(defineProps<{
-  // type: string
+  type?: string
   paginationType?: 'server' | 'client'
   page?: number
   pageSize?: number
   items: T[]
   predicate?: (item: T) => boolean
   comparator?: ((a: T, b: T) => number) | undefined
-  find?: boolean
+  find?: boolean // deprecated: please use the `item` slot
   empty?: boolean
 }>(), {
-  // type: '',
+  type: '',
   paginationType: 'server',
   page: 0,
   pageSize: 50,
@@ -62,7 +93,11 @@ const emit = defineEmits<{
   (e: 'change', value: {page: number, pageSize: number}): void
   (e: 'error', error: Error): void
 }>()
+const slots = useSlots()
 const items = computed(() => {
+  if (slots.item) {
+    return props.items
+  }
   if (props.find) {
     const found = props.items.find(props.predicate)
     return typeof found === 'undefined' ? [] : [found]
