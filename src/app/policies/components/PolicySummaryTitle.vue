4ac791c4ef89b84401d6b4b57fb0483cf3fdd1d3
Moritz Fleck <24444046+schogges@users.noreply.github.com>
2024-05-19 22:47:10 +0200

    feat(policies/components): create PolicySummary, -Title
    Signed-off-by: Moritz Fleck <24444046+schogges@users.noreply.github.com>

---
+++ src/app/policies/components/PolicySummaryTitle.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>
+    <RouterLink
+      :to="props.linkTo"
+    >
+      <RouteTitle
+        :title="props.title"
+      />
+    </RouterLink>
+  </h2>
+</template>
+
+<script lang="ts" setup>
+import type { RouterLinkProps } from 'vue-router'
+
+const props = defineProps<{
+  title: string
+  linkTo: RouterLinkProps['to']
+}>()
+</script>
+
+<style scoped>
+h2 {
+  --icon-before: url('@/assets/images/icon-circles-ext.svg?inline') !important;
+}
+</style>
