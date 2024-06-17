ff9f4ebc324ed9f1ebf1499eb6c44b78b392b412
John Cowen <john.cowen@konghq.com>
2024-06-17 11:18:48 +0100

    feature(meshes): rework mesh detail page
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/meshes/views/MeshDetailView.vue
+++ src/app/meshes/views/MeshDetailView.vue
@@ -12,6 +12,12 @@
     />
 
     <AppView>
+      <template
+        v-if="!props.mesh.mtlsBackend"
+        #notifications
+      >
+        <div v-html="t('meshes.routes.item.mtls-warning')" />
+      </template>
       <div
         class="stack"
       >
@@ -21,6 +27,46 @@
             name: route.params.mesh,
           })"
         >
+          <KCard>
+            <div class="date-status-wrapper">
+              Created: {{ t('common.formats.datetime', {value: Date.parse(mesh.creationTime)}) }}
+            </div>
+            <div class="columns">
+              <template
+                v-for="policy in ['MeshMetric', 'MeshAccessLog', 'MeshTrace']"
+                :key="policy"
+              >
+                <template
+                  v-for="enabled in [Object.entries(data?.policies ?? {}).find(([key, value]) => key === policy)]"
+                  :key="enabled"
+                >
+                  <DefinitionCard>
+                    <template #title>
+                      <XAction
+                        :to="{
+                          name: 'policy-list-view',
+                          params: {
+                            mesh: route.params.mesh,
+                            policyPath: `${policy.toLowerCase()}s`,
+                          },
+                        }"
+                      >
+                        {{ policy }}
+                      </XAction>
+                    </template>
+
+                    <template #body>
+                      <KBadge
+                        appearance="neutral"
+                      >
+                        {{ enabled ? t('meshes.detail.enabled') : t('meshes.detail.disabled') }}
+                      </KBadge>
+                    </template>
+                  </DefinitionCard>
+                </template>
+              </template>
+            </div>
+          </KCard>
           <MeshStatus
             :mesh="props.mesh"
             :mesh-insight="data"
@@ -45,13 +91,6 @@
             }"
           />
         </ResourceCodeBlock>
-
-        <div class="date-status-wrapper">
-          <ResourceDateStatus
-            :creation-time="mesh.creationTime"
-            :modification-time="mesh.modificationTime"
-          />
-        </div>
       </div>
     </AppView>
   </RouteView>
@@ -61,7 +100,7 @@
 import type { Mesh } from '../data'
 import { sources } from '../sources'
 import ResourceCodeBlock from '@/app/common/code-block/ResourceCodeBlock.vue'
-import ResourceDateStatus from '@/app/common/ResourceDateStatus.vue'
+import DefinitionCard from '@/app/common/DefinitionCard.vue'
 import { useMeshStatus } from '@/app/meshes/'
 
 const props = defineProps<{
@@ -76,5 +115,6 @@ const MeshStatus = useMeshStatus()
 .date-status-wrapper {
   display: flex;
   justify-content: flex-end;
+  margin-bottom: 1em
 }
 </style>
