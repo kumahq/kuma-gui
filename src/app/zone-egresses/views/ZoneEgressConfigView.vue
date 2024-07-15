22965102c44a3bbbbdaccec38efa8a4ad4087ea0
John Cowen <johncowen@users.noreply.github.com>
2024-07-15 14:21:30 +0100

    refactor: Change remaining DataSource/Error/Loading's to use DataLoader (#2749)
    Moves the majority of remaining instances of DataSource/ErrorBlock/LoadingBlock to use DataLoaders (which is more concise)
    
    Unfortunately one of those PRs where there isn't much change, but there is lots of nesting changes so everything is red/green. If you look at the changeset you'll see that it's mostly the same changes again and again and again.
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/zone-egresses/views/ZoneEgressConfigView.vue
+++ src/app/zone-egresses/views/ZoneEgressConfigView.vue
@@ -1,6 +1,6 @@
 <template>
   <RouteView
-    v-slot="{ route, t }"
+    v-slot="{ route, t, uri }"
     name="zone-egress-config-view"
     :params="{
       zoneEgress: '',
@@ -15,50 +15,45 @@
     />
     <AppView>
       <KCard>
-        <DataSource
-          v-slot="{ data, error }: ZoneEgressSource"
-          :src="`/zone-egresses/${route.params.zoneEgress}`"
+        <DataLoader
+          v-slot="{ data }"
+          :src="uri(sources, `/zone-egresses/:name`, {
+            name: route.params.zoneEgress,
+          })"
         >
-          <ErrorBlock
-            v-if="error !== undefined"
-            :error="error"
-          />
-
-          <LoadingBlock v-else-if="data === undefined" />
-
-          <template v-else>
-            <ResourceCodeBlock
-              v-slot="{ copy, copying }"
-              :resource="data.config"
-              is-searchable
-              :query="route.params.codeSearch"
-              :is-filter-mode="route.params.codeFilter"
-              :is-reg-exp-mode="route.params.codeRegExp"
-              @query-change="route.update({ codeSearch: $event })"
-              @filter-mode-change="route.update({ codeFilter: $event })"
-              @reg-exp-mode-change="route.update({ codeRegExp: $event })"
-            >
-              <DataSource
-                v-if="copying"
-                :src="`/zone-egresses/${route.params.zoneEgress}/as/kubernetes?no-store`"
-                @change="(data) => {
-                  copy((resolve) => resolve(data))
-                }"
-                @error="(e) => {
-                  copy((_resolve, reject) => reject(e))
-                }"
-              />
-            </ResourceCodeBlock>
-          </template>
-        </DataSource>
+          <ResourceCodeBlock
+            v-slot="{ copy, copying }"
+            :resource="data.config"
+            is-searchable
+            :query="route.params.codeSearch"
+            :is-filter-mode="route.params.codeFilter"
+            :is-reg-exp-mode="route.params.codeRegExp"
+            @query-change="route.update({ codeSearch: $event })"
+            @filter-mode-change="route.update({ codeFilter: $event })"
+            @reg-exp-mode-change="route.update({ codeRegExp: $event })"
+          >
+            <DataSource
+              v-if="copying"
+              :src="uri(sources, `/zone-egresses/:name/as/kubernetes`, {
+                name: route.params.zoneEgress,
+              }, {
+                cacheControl: 'no-store',
+              })"
+              @change="(data) => {
+                copy((resolve) => resolve(data))
+              }"
+              @error="(e) => {
+                copy((_resolve, reject) => reject(e))
+              }"
+            />
+          </ResourceCodeBlock>
+        </DataLoader>
       </KCard>
     </AppView>
   </RouteView>
 </template>
 
 <script lang="ts" setup>
-import { ZoneEgressSource } from '../sources'
+import { sources } from '../sources'
 import ResourceCodeBlock from '@/app/common/code-block/ResourceCodeBlock.vue'
-import ErrorBlock from '@/app/common/ErrorBlock.vue'
-import LoadingBlock from '@/app/common/LoadingBlock.vue'
 </script>
--- src/app/zone-egresses/views/ZoneEgressListView.vue
+++ src/app/zone-egresses/views/ZoneEgressListView.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- TODO: Update page & size once the list endpoint is being filtered by zone -->
   <RouteView
-    v-slot="{ route, t, me }"
+    v-slot="{ route, t, me, uri }"
     name="zone-egress-list-view"
     :params="{
       /* page: 1, */
@@ -16,125 +16,126 @@
     />
     <AppView>
       <!-- TODO: Update page & size once the list endpoint is being filtered by zone -->
-      <DataSource
-        v-slot="{ data, error }: ZoneEgressOverviewCollectionSource"
-        :src="`/zone-cps/${route.params.zone || '*'}/egresses?page=${'1'}&size=${'100'}`"
-      >
-        <KCard>
-          <ErrorBlock
-            v-if="error !== undefined"
-            :error="error"
-          />
-
-          <!-- TODO: Update page & size once the list endpoint is being filtered by zone -->
-          <AppCollection
-            v-else
-            class="zone-egress-collection"
-            data-testid="zone-egress-collection"
-            :headers="[
-              { ...me.get('headers.name'), label: 'Name', key: 'name' },
-              { ...me.get('headers.socketAddress'), label: 'Address', key: 'socketAddress' },
-              { ...me.get('headers.status'), label: 'Status', key: 'status' },
-              { ...me.get('headers.actions'), label: 'Actions', key: 'actions', hideLabel: true },
-            ]"
-            :page-number="1"
-            :page-size="100"
-            :total="data?.total"
-            :items="data?.items"
-            :error="error"
-            :empty-state-message="t('common.emptyState.message', { type: 'Zone Egresses' })"
-            :empty-state-cta-to="t('zone-egresses.href.docs')"
-            :empty-state-cta-text="t('common.documentation')"
-            :is-selected-row="(row) => row.name === route.params.zoneEgress"
-            @change="route.update"
-            @resize="me.set"
-          >
-            <template #name="{ row: item }">
-              <XAction
-                data-action
-                :to="{
-                  name: 'zone-egress-summary-view',
-                  params: {
-                    zone: route.params.zone,
-                    zoneEgress: item.id,
-                  },
-                  query: {
-                    // TODO: Update page & size once the list endpoint is being filtered by zone
-                    page: 1,
-                    size: 100,
-                  },
-                }"
-              >
-                {{ item.name }}
-              </XAction>
-            </template>
-
-            <template #socketAddress="{ row: item }">
-              <TextWithCopyButton
-                v-if="item.zoneEgress.socketAddress.length > 0"
-                :text="item.zoneEgress.socketAddress"
-              />
-              <template v-else>
-                {{ t('common.collection.none') }}
-              </template>
-            </template>
-
-            <template #status="{ row: item }">
-              <StatusBadge
-                :status="item.state"
-              />
-            </template>
-
-            <template #actions="{ row: item }">
-              <XActionGroup>
-                <XAction
-                  :to="{
-                    name: 'zone-egress-detail-view',
-                    params: {
-                      zoneEgress: item.id,
-                    },
-                  }"
-                >
-                  {{ t('common.collection.actions.view') }}
-                </XAction>
-              </XActionGroup>
-            </template>
-          </AppCollection>
-        </KCard>
-
-        <RouterView
-          v-if="route.params.zoneEgress"
-          v-slot="child"
+      <KCard>
+        <DataLoader
+          :src="uri(sources, `/zone-cps/:name/egresses`, {
+            name: route.params.zone || '*',
+          }, {
+            page: 1,
+            size: 100,
+          })"
         >
-          <SummaryView
-            @close="route.replace({
-              name: 'zone-egress-list-view',
-              params: {
-                zone: route.params.zone,
-              },
-              query: {
-                // TODO: Update page & size once the list endpoint is being filtered by zone
-                page: 1,
-                size: 100,
-              },
-            })"
+          <template
+            #loadable="{ data }"
           >
-            <component
-              :is="child.Component"
-              v-if="typeof data !== 'undefined'"
-              :items="data.items"
-            />
-          </SummaryView>
-        </RouterView>
-      </DataSource>
+            <DataCollection
+              type="zone-egresses"
+              :items="data?.items ?? [undefined]"
+            >
+              <!-- TODO: Update page & size once the list endpoint is being filtered by zone -->
+              <AppCollection
+                class="zone-egress-collection"
+                data-testid="zone-egress-collection"
+                :headers="[
+                  { ...me.get('headers.name'), label: 'Name', key: 'name' },
+                  { ...me.get('headers.socketAddress'), label: 'Address', key: 'socketAddress' },
+                  { ...me.get('headers.status'), label: 'Status', key: 'status' },
+                  { ...me.get('headers.actions'), label: 'Actions', key: 'actions', hideLabel: true },
+                ]"
+                :page-number="1"
+                :page-size="100"
+                :total="data?.total"
+                :items="data?.items"
+                :is-selected-row="(row) => row.name === route.params.zoneEgress"
+                @change="route.update"
+                @resize="me.set"
+              >
+                <template #name="{ row: item }">
+                  <XAction
+                    data-action
+                    :to="{
+                      name: 'zone-egress-summary-view',
+                      params: {
+                        zone: route.params.zone,
+                        zoneEgress: item.id,
+                      },
+                      query: {
+                        // TODO: Update page & size once the list endpoint is being filtered by zone
+                        page: 1,
+                        size: 100,
+                      },
+                    }"
+                  >
+                    {{ item.name }}
+                  </XAction>
+                </template>
+
+                <template #socketAddress="{ row: item }">
+                  <TextWithCopyButton
+                    v-if="item.zoneEgress.socketAddress.length > 0"
+                    :text="item.zoneEgress.socketAddress"
+                  />
+                  <template v-else>
+                    {{ t('common.collection.none') }}
+                  </template>
+                </template>
+
+                <template #status="{ row: item }">
+                  <StatusBadge
+                    :status="item.state"
+                  />
+                </template>
+
+                <template #actions="{ row: item }">
+                  <XActionGroup>
+                    <XAction
+                      :to="{
+                        name: 'zone-egress-detail-view',
+                        params: {
+                          zoneEgress: item.id,
+                        },
+                      }"
+                    >
+                      {{ t('common.collection.actions.view') }}
+                    </XAction>
+                  </XActionGroup>
+                </template>
+              </AppCollection>
+              <RouterView
+                v-slot="{ Component }"
+              >
+                <SummaryView
+                  v-if="route.child()"
+                  @close="route.replace({
+                    name: 'zone-egress-list-view',
+                    params: {
+                      zone: route.params.zone,
+                    },
+                    query: {
+                      // TODO: Update page & size once the list endpoint is being filtered by zone
+                      page: 1,
+                      size: 100,
+                    },
+                  })"
+                >
+                  <component
+                    :is="Component"
+                    v-if="typeof data !== 'undefined'"
+                    :items="data.items"
+                  />
+                </SummaryView>
+              </RouterView>
+            </DataCollection>
+          </template>
+        </DataLoader>
+      </KCard>
     </AppView>
   </RouteView>
 </template>
 
 <script lang="ts" setup>
-import type { ZoneEgressOverviewCollectionSource } from '../sources'
+import { sources } from '../sources'
 import AppCollection from '@/app/application/components/app-collection/AppCollection.vue'
-import ErrorBlock from '@/app/common/ErrorBlock.vue'
 import StatusBadge from '@/app/common/StatusBadge.vue'
 import SummaryView from '@/app/common/SummaryView.vue'
 import TextWithCopyButton from '@/app/common/TextWithCopyButton.vue'
