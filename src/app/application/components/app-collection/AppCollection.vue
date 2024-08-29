f4c6722563fc5abd1074f541b961ef988d4a4c05
John Cowen <john.cowen@konghq.com>
2024-08-29 15:17:56 +0100

    chore: Refactor AppCollection to not include non-table things
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/application/components/app-collection/AppCollection.vue
+++ src/app/application/components/app-collection/AppCollection.vue
@@ -1,36 +1,19 @@
 <template>
   <KTable
-    :key="kTableMountKey"
     data-testid="app-collection"
     class="app-collection"
-    :has-error="(typeof props.error !== 'undefined')"
-    :pagination-total-items="props.total"
-    :initial-fetcher-params="{ page: props.pageNumber, pageSize: props.pageSize }"
     :headers="props.headers"
     :fetcher-cache-key="String(cacheKey)"
-    :fetcher="({ page, pageSize, query: _query }: FetcherParams) => {
-      const value: ChangeValue = {}
-      if(lastPageNumber !== page) {
-        value.page = page;
-      }
-      if(lastPageSize !== pageSize) {
-        value.size = pageSize;
-      }
-      lastPageNumber = page
-      lastPageSize = pageSize
-      if(Object.keys(value).length > 0) {
-        emit('change', value)
-      }
+    :fetcher="() => {
       return { data: props.items }
     }"
     :cell-attrs="({ headerKey }: CellAttrParams) => ({
       class: `${headerKey}-column`,
     })"
     :row-attrs="getRowAttributes"
-    disable-sorting
-    :disable-pagination="props.pageNumber === 0"
-    hide-pagination-when-optional
-    resize-columns
+    :disable-sorting="true"
+    :disable-pagination="true"
+    :resize-columns="true"
     :table-preferences="{
       columnWidths: props.headers.reduce<Record<string, number>>((prev, value) => {
         if(typeof value.width !== 'undefined') {
@@ -43,44 +26,6 @@
     @row:click="click"
     @update:table-preferences="resize"
   >
-    <template
-      v-if="props.items?.length === 0"
-      #empty-state
-    >
-      <EmptyBlock>
-        <template #title>
-          {{ props.emptyStateTitle ?? t('common.emptyState.title') }}
-        </template>
-
-        <template v-if="props.emptyStateMessage">
-          {{ props.emptyStateMessage }}
-        </template>
-
-        <template
-          v-if="props.emptyStateCtaTo"
-          #action
-        >
-          <XAction
-            v-if="typeof props.emptyStateCtaTo === 'string'"
-            action="docs"
-            :href="props.emptyStateCtaTo"
-          >
-            {{ props.emptyStateCtaText }}
-          </XAction>
-
-          <KButton
-            v-else
-            appearance="primary"
-            :to="props.emptyStateCtaTo"
-          >
-            <AddIcon />
-
-            {{ props.emptyStateCtaText }}
-          </KButton>
-        </template>
-      </EmptyBlock>
-    </template>
-
     <template
       v-for="key in Object.keys(slots)"
       :key="key"
@@ -107,65 +52,32 @@
 </template>
 
 <script lang="ts" setup generic="Row extends {}">
-import { AddIcon } from '@kong/icons'
-import { KButton, KTable } from '@kong/kongponents'
+import { KTable } from '@kong/kongponents'
 import { useSlots, ref, watch, Ref } from 'vue'
-import { RouteLocationRaw } from 'vue-router'
 
-import { useI18n } from '@/app/application'
-import EmptyBlock from '@/app/common/EmptyBlock.vue'
 import type { TableHeader as KTableHeader, TablePreferences } from '@kong/kongponents'
-
 type CellAttrParams = {
   headerKey: string
   row: Row
   rowIndex: number
   colIndex: number
 }
-type FetcherParams = {
-  page: number
-  pageSize: number
-  query: string
-}
-type ChangeValue = {
-  page?: number
-  size?: number
-}
 type ResizeValue = {
   headers: Record<string, { width: number }>
 }
-
-const { t } = useI18n()
 
 type TableHeader = KTableHeader & {
   width?: number
 }
 const props = withDefaults(defineProps<{
-  isSelectedRow?: ((row: Row) => boolean) | null
-  total?: number
-  pageNumber?: number
-  pageSize?: number
+  isSelectedRow?: ((row: Row) => boolean)
   items: Row[] | undefined
   headers: TableHeader[]
-  error?: Error | undefined
-  emptyStateTitle?: string
-  emptyStateMessage?: string
-  emptyStateCtaTo?: string | RouteLocationRaw
-  emptyStateCtaText?: string
 }>(), {
-  isSelectedRow: null,
-  total: 0,
-  pageNumber: 0,
-  pageSize: 30,
-  error: undefined,
-  emptyStateTitle: undefined,
-  emptyStateMessage: undefined,
-  emptyStateCtaTo: undefined,
-  emptyStateCtaText: undefined,
+  isSelectedRow: undefined,
 })
 
 const emit = defineEmits<{
-  (e: 'change', value: ChangeValue): void
   (e: 'resize', value: ResizeValue): void
 }>()
 
@@ -173,23 +85,6 @@ const slots = useSlots()
 
 const items = ref(props.items) as Ref<typeof props.items>
 const cacheKey = ref<number>(0)
-/**
- * Used as a means to instruct KTable to re-mount.
- *
- * This is a hack around the fact that there is no other way to tell KTable if
- * the current page of a paginated view has changed. KTable assumes it’s the
- * *sole* owner/maintainer of pagination-related state, but that’s a design
- * flaw that doesn’t account for browser history navigation (e.g. pressing back
- * to go from page 2 to page 1).
- *
- * Is triggered via a watcher whenever `props.pageNumber` changes while the
- * last page number that was emitted by KTable (via calls to its `fetcher`
- * prop) is different (i.e. the page number has changed independently of a
- * KTable mechanism).
- */
-const kTableMountKey = ref(0)
-const lastPageNumber = ref(props.pageNumber)
-const lastPageSize = ref(props.pageSize)
 
 const resize = (args: TablePreferences) => {
   const headers = Object.entries(args.columnWidths ?? {}).reduce<Record<string, { width: number }>>((prev, [key, value]) => {
@@ -211,17 +106,6 @@ watch(() => props.items, (newItems, oldItems) => {
   }
 })
 
-watch(() => props.pageNumber, function () {
-  if (props.pageNumber !== lastPageNumber.value) {
-    kTableMountKey.value++
-  }
-})
-watch(() => props.headers, function (val, prev) {
-  if (JSON.stringify(val) !== JSON.stringify(prev)) {
-    kTableMountKey.value++
-  }
-})
-
 function getRowAttributes(row: Row): Record<string, string> {
   if (!row) {
     return {}
@@ -229,7 +113,7 @@ function getRowAttributes(row: Row): Record<string, string> {
 
   const attributes: Record<string, string> = {}
 
-  if (props.isSelectedRow !== null && props.isSelectedRow(row)) {
+  if (typeof props.isSelectedRow !== 'undefined' && props.isSelectedRow(row)) {
     attributes.class = 'is-selected'
   }
 
