d5bc3146f9e8e93c39701ed7ff4d925b930c181c
John Cowen <john.cowen@konghq.com>
2024-08-29 09:54:25 +0100

    Remove form
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/x/components/x-prompt/XPrompt.vue
+++ src/app/x/components/x-prompt/XPrompt.vue
@@ -1,27 +1,23 @@
 <template>
-  <form
-    @submit.prevent="() => emit('submit')"
+  <KPrompt
+    :action-button-text="props.action"
+    :confirmation-text="props.expected.length > 0 ? props.expected : undefined"
+    :visible="true"
+    :type="props.type"
+    @cancel="() => emit('cancel')"
+    @proceed="() => emit('submit')"
   >
-    <KPrompt
-      ref="$ref"
-      :action-button-text="props.action"
-      :confirmation-text="props.expected.length > 0 ? props.expected : undefined"
-      :visible="true"
-      :type="props.type"
-      @cancel="() => emit('cancel')"
+    <template
+      #title
     >
-      <template
-        #title
-      >
-        <slot name="title" />
-      </template>
+      <slot name="title" />
+    </template>
 
-      <slot name="default" />
-    </KPrompt>
-  </form>
+    <slot name="default" />
+  </KPrompt>
 </template>
 <script lang="ts" setup>
-import { onMounted, ref, provide } from 'vue'
+import { provide } from 'vue'
 const emit = defineEmits<{
   (e: 'cancel'): void
   (e: 'submit'): void
@@ -35,16 +31,6 @@ const props = withDefaults(defineProps<{
   expected: '',
 })
 provide('x-prompt', {})
-const $ref = ref<{$el: HTMLDivElement } | null>(null)
-const rewrite = () => {
-  const $el = $ref.value!.$el // $ref is statically always there
-
-  const $confirm = $el.querySelector('button[disabled]')
-  if ($confirm) {
-    $confirm.setAttribute('type', 'submit')
-  }
-}
-onMounted(rewrite)
 </script>
 <style lang="scss">
 .error {
