0b85e097e7264be6e699850dd759c51f0f9edfdf
John Cowen <johncowen@users.noreply.github.com>
2024-09-19 15:14:38 +0100

    fix(mmzs): remove Service listing from MeshMultiZoneDetail (#2973)
    We decided to completely remove the list of MeshServices for a MeshMultiZoneService in the MeshMultiZoneService detail view.
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/services/views/MeshMultiZoneServiceDetailView.vue
+++ src/app/services/views/MeshMultiZoneServiceDetailView.vue
@@ -4,14 +4,11 @@
     :params="{
       mesh: '',
       service: '',
-      page: 1,
-      size: 50,
-      s: '',
       codeSearch: '',
       codeFilter: false,
       codeRegExp: false,
     }"
-    v-slot="{ t, me, can }"
+    v-slot="{ route }"
   >
     <AppView>
       <div
@@ -63,75 +60,28 @@
         </KCard>
 
         <div>
-          <h3>
-            Mesh Services
-          </h3>
-
-          <KCard
-            class="mt-4"
+          <ResourceCodeBlock
+            :resource="props.data.config"
+            is-searchable
+            :query="route.params.codeSearch"
+            :is-filter-mode="route.params.codeFilter"
+            :is-reg-exp-mode="route.params.codeRegExp"
+            @query-change="route.update({ codeSearch: $event })"
+            @filter-mode-change="route.update({ codeFilter: $event })"
+            @reg-exp-mode-change="route.update({ codeRegExp: $event })"
+            v-slot="{ copy, copying }"
           >
-            <AppCollection
-              data-testid="mesh-service-collection"
-              :headers="[
-                { ...me.get('headers.name'), label: 'Name', key: 'name' },
-                ...(can('use zones') ? [{ ...me.get('headers.zone'), label: 'Zone', key: 'zone' }] : []),
-                { ...me.get('headers.namespace'), label: 'Namespace', key: 'namespace' },
-                { ...me.get('headers.actions'), label: 'Actions', key: 'actions', hideLabel: true },
-              ]"
-              :items="data.status.meshServices"
-              :total="data.status.meshServices.length"
-              @resize="me.set"
-            >
-              <template #name="{ row: item }">
-                <XAction
-                  data-action
-                  class="name-link"
-                  :to="{
-                    name: 'mesh-service-detail-view',
-                    params: {
-                      mesh: item.mesh,
-                      service: item.name,
-                    },
-                  }"
-                >
-                  {{ item.name }}
-                </XAction>
-              </template>
-
-              <template #zone="{ row }">
-                <XAction
-                  :to="{
-                    name: 'zone-cp-detail-view',
-                    params: {
-                      zone: row.zone,
-                    },
-                  }"
-                >
-                  {{ row.zone }}
-                </XAction>
-              </template>
-
-              <template #namespace="{ row: item }">
-                {{ item.namespace }}
-              </template>
-
-              <template #actions="{ row: item }">
-                <XActionGroup>
-                  <XAction
-                    :to="{
-                      name: 'mesh-service-detail-view',
-                      params: {
-                        mesh: item.mesh,
-                        service: item.name,
-                      },
-                    }"
-                  >
-                    {{ t('common.collection.actions.view') }}
-                  </XAction>
-                </XActionGroup>
-              </template>
-            </AppCollection>
-          </KCard>
+            <DataSource
+              v-if="copying"
+              :src="`/meshes/${props.data.mesh}/mesh-multi-zone-service/${props.data.id}/as/kubernetes?no-store`"
+              @change="(data) => {
+                copy((resolve) => resolve(data))
+              }"
+              @error="(e) => {
+                copy((_resolve, reject) => reject(e))
+              }"
+            />
+          </ResourceCodeBlock>
         </div>
       </div>
     </AppView>
@@ -140,7 +90,7 @@
 
 <script lang="ts" setup>
 import type { MeshMultiZoneService } from '../data'
-import AppCollection from '@/app/application/components/app-collection/AppCollection.vue'
+import ResourceCodeBlock from '@/app/common/code-block/ResourceCodeBlock.vue'
 import DefinitionCard from '@/app/common/DefinitionCard.vue'
 
 const props = defineProps<{
