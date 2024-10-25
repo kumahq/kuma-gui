701c9ffddf584d3f47d90f7a9d657bbdb6c858a6
John Cowen <johncowen@users.noreply.github.com>
2024-10-25 11:11:25 +0100

    chore(services): remove mesh-service feature flag (#3090)
    During the development of our MeshService support we added a feature
    flag (defaulting to `true`) in case we needed to turn it off in a
    central place for any reason.
    
    We no longer need this ability as we now always want MeshService support
    enabled.
    
    This PR removes any usage of the feature flag plus the feature flag
    itself.
    
    (Make sure you use the GH `Hide whitespace` toggle whilst viewing the
    diff, otherwise the change look much bigger than they actually are)
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/services/features.ts
+++ src/app/services/features.ts
@@ -1,16 +1,8 @@
 import type { Features } from '@/app/application'
 import { runInDebug } from '@/app/application'
-import type Env from '@/app/application/services/env/Env'
 import { Mesh } from '@/app/meshes/data'
-export const features = (env: Env['var']): Features => {
+export const features = (): Features => {
   return {
-    'use meshservice': () => {
-      try {
-        return !!JSON.parse(env('KUMA_MESHSERVICE_ENABLED', 'true'))
-      } catch (e) {
-        return true
-      }
-    },
     'use service-insights': (_can, mesh: Mesh) => {
       runInDebug(() => {
         if (typeof mesh === 'undefined') {
