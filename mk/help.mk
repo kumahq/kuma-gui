c062e97f607e4414725739b6f8fd00a289bbb93e
John Cowen <johncowen@users.noreply.github.com>
2024-10-29 09:45:08 +0000

    chore(mk): move accessible targets to the "make main file" i.e. Makefile (#3017)
    Changes our approach to Makefile targets to make them more customisable
    upstream
    
    ---
    
    The biggest problem here is that Make make its troublesome to simply
    override make targets.
    
    Previously we would 'override' targets by not including the file that
    had the target we had to override, and then including a different file
    with our customisations in it for the relevant target. The thing is you
    would also need to override (or just copy) all targets in a file, rather
    than just the one you wanted to override.
    
    This approach names all consumable make targets prefixed with a `.` i.e.
    `.install` vs `install`. We than add references to all these in our base
    `Makefile` (essentially `make`s `main` file). The `Makefile` is specific
    to each project so we can add what we want in there and it doesn't
    affect anything downstream. This also includes adding different
    documentation if need be.
    
    For example, say in one make file we have:
    
    ```make
    install: .install ## Docs for this here
    ```
    
    And we need to add some sort of auth to that we can use 
    ```make
    .install/auth:
      $(AUTH) $(MAKE) .install
    install: .install/auth ## Different docs for this here
    ```
    
    Both of the above examples will run our installation scripts without
    them being duplicated, but one is decorated with some sort of
    authentication before doing so.
    
    Moving forwards we should add all "common" user scripts in this file
    using the same approach. We should avoid mixing the documented aliases
    in with the rest of the `./mk` files, and avoid mixing long form scripts
    into the `Makefile` itself. I've added a small comment at the top of the
    `Makefile` to this effect.
    
    Lastly, I've only moved previously documented targets into this form as
    these are the commonly used ones. We can move others later on if
    necessary/desirable.
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- mk/help.mk
+++ mk/help.mk
@@ -1,5 +1,6 @@
-.PHONY: help
-help: ## Display this help screen
+.PHONY: .help
+.help: ## Display this help screen
+	@echo "The following targets can be used by running \`make <target>\`"; echo "---"
 	@# Display top-level targets since they are the ones most developers will need.
 	@grep -h -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort -k1 | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 	@# Now show hierarchical targets in separate sections.
