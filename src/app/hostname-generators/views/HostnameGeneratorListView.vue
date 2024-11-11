ee8b55caa3be48d93fb9239aed78a8130184899f
schogges <moritz.fleck@konghq.com>
2024-11-11 15:02:21 +0100

    refactor(HostnameGeneratorListView): use XCopyButton instead of TextCopyButton
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- src/app/hostname-generators/views/HostnameGeneratorListView.vue
+++ src/app/hostname-generators/views/HostnameGeneratorListView.vue
@@ -51,7 +51,7 @@
                   @resize="me.set"
                 >
                   <template #name="{ row: item }">
-                    <TextWithCopyButton
+                    <XCopyButton
                       :text="item.name"
                     >
                       <XAction
@@ -69,7 +69,7 @@
                       >
                         {{ item.name }}
                       </XAction>
-                    </TextWithCopyButton>
+                    </XCopyButton>
                   </template>
 
                   <template #actions="{ row: item }">
@@ -122,5 +122,5 @@
 import { sources } from '../sources'
 import AppCollection from '@/app/application/components/app-collection/AppCollection.vue'
 import SummaryView from '@/app/common/SummaryView.vue'
-import TextWithCopyButton from '@/app/common/TextWithCopyButton.vue'
+import XCopyButton from '@/app/x/components/x-copy-button/XCopyButton.vue'
 </script>
