1322b23d7452bbf4b042a1587cb8056f85d22927
John Cowen <john.cowen@konghq.com>
2024-11-26 10:15:45 +0000

    feature: enable delayed teleporting
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- packages/kuma-gui/src/app/x/components/x-teleport/XTeleportSlot.vue
+++ packages/kuma-gui/src/app/x/components/x-teleport/XTeleportSlot.vue
@@ -4,8 +4,11 @@
   />
 </template>
 <script lang="ts" setup>
+import { onMounted } from 'vue'
 const props = defineProps<{
   name: string
 }>()
-
+onMounted(() => {
+  window.dispatchEvent(new CustomEvent<{ name: string }>('x-teleport-slot:mounted', { detail: { name: props.name } }))
+})
 </script>
--- packages/kuma-gui/src/app/x/components/x-teleport/XTeleportTemplate.vue
+++ packages/kuma-gui/src/app/x/components/x-teleport/XTeleportTemplate.vue
@@ -7,19 +7,30 @@
   </Teleport>
 </template>
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, onBeforeUnmount, ref } from 'vue'
 const props = defineProps<{
   to: {
     name: string
   }
 }>()
-
+const controller = new AbortController()
 const ready = ref<boolean>(false)
+
+const isCustomEvent = (e: Event | CustomEvent): e is CustomEvent => 'detail' in e
+
 onMounted(() => {
   if (document.querySelector(`[data-x-teleport-id='${props.to.name}']`) !== null) {
     ready.value = true
   } else {
-    throw new Error(`The '[data-x-teleport-id='${props.to.name}']' element could not be found to teleport to`)
+    window.addEventListener('x-teleport-slot:mounted', (e: Event) => {
+      if (isCustomEvent(e) && e.detail?.name === props.to.name) {
+        ready.value = true
+      }
+    }, { signal: controller.signal })
   }
+})
+
+onBeforeUnmount(() => {
+  controller.abort()
 })
 </script>
