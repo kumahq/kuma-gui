69db10841820100fcf7a1f0cc8265345fedd4a6a
schogges <moritz.fleck@konghq.com>
2024-12-09 16:22:45 +0100

    feat(gateways/builtin-gateway-list-view): add summary drawer with config
    Signed-off-by: schogges <moritz.fleck@konghq.com>

---
+++ packages/kuma-gui/src/app/gateways/views/BuiltinGatewaySummaryView.vue
@@ -0,0 +1,124 @@
+<template>
+  <RouteView
+    :name="props.routeName"
+    :params="{
+      mesh: '',
+      gateway: '',
+      codeSearch: '',
+      codeFilter: false,
+      codeRegExp: false,
+    }"
+    v-slot="{ route, t }"
+  >
+    <DataCollection
+      :items="props.items"
+      :predicate="item => item.id === route.params.gateway"
+      :find="true"
+    >
+      <template #empty>
+        <XEmptyState>
+          <template #title>
+            <h2>
+              {{ t('common.collection.summary.empty_title', { type: 'Gateway' }) }}
+            </h2>
+          </template>
+          <p>
+            {{ t('common.collection.summary.empty_message', { type: 'Gateway' }) }}
+          </p>
+        </XEmptyState>
+      </template>
+      <template
+        #default="{ items: proxies }"
+      >
+        <template
+          v-for="item in [proxies[0]]"
+          :key="item.id"
+        >
+          <AppView>
+            <template #title>
+              <h2>
+                <XAction
+                  :to="{
+                    name: 'builtin-gateway-detail-view',
+                    params: {
+                      mesh: item.mesh,
+                      gateway: item.id,
+                    },
+                  }"
+                >
+                  <RouteTitle
+                    :title="t('builtin-gateways.routes.item.title', { name: item.name })"
+                  />
+                </XAction>
+              </h2>
+            </template>
+
+            <div
+              class="stack-with-borders"
+            >
+              <DefinitionCard
+                v-if="item.namespace.length > 0"
+                layout="horizontal"
+              >
+                <template #title>
+                  {{ t('gateways.routes.item.namespace') }}
+                </template>
+
+                <template #body>
+                  {{ item.namespace }}
+                </template>
+              </DefinitionCard>
+            </div>
+            <div>
+              <h3>
+                {{ t('gateways.routes.item.config') }}
+              </h3>
+
+              <div class="mt-4">
+                <ResourceCodeBlock
+                  :resource="item.config"
+                  is-searchable
+                  :query="route.params.codeSearch"
+                  :is-filter-mode="route.params.codeFilter"
+                  :is-reg-exp-mode="route.params.codeRegExp"
+                  @query-change="route.update({ codeSearch: $event })"
+                  @filter-mode-change="route.update({ codeFilter: $event })"
+                  @reg-exp-mode-change="route.update({ codeRegExp: $event })"
+                  v-slot="{ copy, copying }"
+                >
+                  <DataSource
+                    v-if="copying"
+                    :src="`/meshes/${route.params.mesh}/mesh-gateways/${route.params.gateway}/as/kubernetes?no-store`"
+                    @change="(data) => {
+                      copy((resolve) => resolve(data))
+                    }"
+                    @error="(e) => {
+                      copy((_resolve, reject) => reject(e))
+                    }"
+                  />
+                </ResourceCodeBlock>
+              </div>
+            </div>
+          </AppView>
+        </template>
+      </template>
+    </DataCollection>
+  </RouteView>
+</template>
+
+<script lang="ts" setup>
+import type { MeshGateway } from '../data'
+import DataCollection from '@/app/application/components/data-collection/DataCollection.vue'
+import DefinitionCard from '@/app/common/DefinitionCard.vue'
+import ResourceCodeBlock from '@/app/x/components/x-code-block/ResourceCodeBlock.vue'
+
+const props = defineProps<{
+  items: MeshGateway[]
+  routeName: string
+}>()
+</script>
+<style scoped>
+h2 {
+  --icon-before: url('@/assets/images/gateway.svg?inline') !important;
+}
+</style>
