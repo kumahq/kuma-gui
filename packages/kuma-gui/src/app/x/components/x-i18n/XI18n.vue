1a5b4d57a5cedf083afd1cce6438fbd10c8460ad
John Cowen <john.cowen@konghq.com>
2024-12-10 11:21:20 +0000

    feat: adds XI18n component
    Signed-off-by: John Cowen <john.cowen@konghq.com>

---
+++ packages/kuma-gui/src/app/x/components/x-i18n/XI18n.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    v-html="_t(props.t, params)"
+  />
+  <template
+    v-for="(_slot, key) in slots"
+    :key="key"
+  >
+    <XTeleportTemplate
+      :to="{ name: `x-i18n-${id}-${key}`}"
+    >
+      <slot
+        :name="key"
+      />
+    </XTeleportTemplate>
+  </template>
+</template>
+<script lang="ts" setup>
+import { useSlots, computed } from 'vue'
+
+import { useI18n, uniqueId } from '@/app/application'
+const { t: _t } = useI18n()
+const props = defineProps<{
+  t: string
+}>()
+const slots = useSlots()
+const id = uniqueId('x-i18n')
+
+const params = computed(() => {
+  return Object.keys(slots).reduce<Record<string, string>>((prev, key) => {
+    prev[key] = `<span data-x-teleport-id="x-i18n-${id}-${key}"></span>`
+    return prev
+  }, {})
+})
+</script>
