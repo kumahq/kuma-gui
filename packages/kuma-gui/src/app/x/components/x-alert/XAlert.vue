2b8f0db3ba2e38f6a7934da6d4ea4cc63e2c60be
John Cowen <john.cowen@konghq.com>
2024-12-19 11:20:38 +0000

    fix: globally fix text color of buttons in alerts
    Signed-off-by: John Cowen <john.cowen@konghq.com>

---
+++ packages/kuma-gui/src/app/x/components/x-alert/XAlert.vue
@@ -0,0 +1,35 @@
+<template>
+  <KAlert
+    :appearance="props.variant"
+    :dismissible="props.dismissable"
+  >
+    <template
+      v-for="(_, key) in slots"
+      :key="key"
+      #[`${key}`]
+    >
+      <slot :name="key" />
+    </template>
+  </KAlert>
+</template>
+<script lang="ts" setup>
+import { KAlert } from '@kong/kongponents'
+import { useSlots } from 'vue'
+
+import type { AlertAppearance } from '@kong/kongponents'
+
+const slots = useSlots()
+
+const props = withDefaults(defineProps<{
+  variant?: AlertAppearance
+  dismissable?: boolean
+}>(), {
+  variant: 'warning',
+  dismissable: false,
+})
+</script>
+<style lang="scss" scoped>
+:deep(.k-button.primary) {
+  color: $kui-color-text-inverse !important;
+}
+</style>
