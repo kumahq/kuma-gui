9a7861b0609c81f3a13de0e1204fae22141ff841
schogges <moritz.fleck@konghq.com>
2025-01-20 16:18:30 +0100

    feat(x-layout): add size option unset
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/kuma-gui/src/app/zone-ingresses/views/ZoneIngressSummaryView.vue
+++ packages/kuma-gui/src/app/zone-ingresses/views/ZoneIngressSummaryView.vue
@@ -86,6 +86,7 @@
               <template v-if="route.params.format === 'structured'">
                 <XLayout
                   type="stack"
+                  size="unset"
                   class="stack-with-borders"
                   data-testid="structured-view"
                 >
