f8bf8f4ad5842c9c09aedbe2743b2d86937775fc
schogges <moritz.fleck@konghq.com>
2025-01-22 10:35:43 +0100

    feat(dataplane-summary-view): use dropdown to select config format
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/kuma-gui/src/app/data-planes/views/DataPlaneSummaryView.vue
+++ packages/kuma-gui/src/app/data-planes/views/DataPlaneSummaryView.vue
@@ -7,6 +7,7 @@
       codeSearch: '',
       codeFilter: false,
       codeRegExp: false,
+      format: 'structured',
     }"
     v-slot="{ route, t, uri, can }"
   >
@@ -53,120 +54,41 @@
               </h2>
             </template>
 
-            <XLayout
-              type="stack"
-            >
-              <div
-                class="stack-with-borders"
-              >
-                <DefinitionCard
-                  layout="horizontal"
+            <XLayout>
+              <header>
+                <XLayout
+                  type="separated"
+                  size="max"
                 >
-                  <template #title>
-                    {{ t('http.api.property.status') }}
-                  </template>
-
-                  <template #body>
-                    <XLayout
-                      type="separated"
-                    >
-                      <StatusBadge
-                        :status="item.status"
-                      />
-
-                      <DataCollection
-                        v-if="item.dataplaneType === 'standard'"
-                        :items="item.dataplane.networking.inbounds"
-                        :predicate="item => item.state !== 'Ready'"
-                        :empty="false"
-                        v-slot="{ items : inbounds }"
-                      >
-                        <XIcon
-                          name="info"
-                          :color="KUI_COLOR_TEXT_NEUTRAL"
-                        >
-                          <ul>
-                            <li
-                              v-for="inbound in inbounds"
-                              :key="`${inbound.service}:${inbound.port}`"
-                            >
-                              {{ t('data-planes.routes.item.unhealthy_inbound', { service: inbound.service, port: inbound.port }) }}
-                            </li>
-                          </ul>
-                        </XIcon>
-                      </DataCollection>
-                    </XLayout>
-                  </template>
-                </DefinitionCard>
-
-                <DefinitionCard
-                  layout="horizontal"
-                >
-                  <template #title>
-                    Type
-                  </template>
-
-                  <template #body>
-                    {{ t(`data-planes.type.${item.dataplaneType}`) }}
-                  </template>
-                </DefinitionCard>
-
-                <DefinitionCard
-                  v-if="item.namespace.length > 0"
-                  layout="horizontal"
-                >
-                  <template #title>
-                    {{ t('data-planes.routes.item.namespace') }}
-                  </template>
-
-                  <template #body>
-                    {{ item.namespace }}
-                  </template>
-                </DefinitionCard>
-
-                <DefinitionCard
-                  v-if="can('use zones') && item.zone"
-                  layout="horizontal"
-                >
-                  <template
-                    #title
-                  >
-                    Zone
-                  </template>
-                  <template
-                    #body
-                  >
-                    <XAction
-                      :to="{
-                        name: 'zone-cp-detail-view',
-                        params: {
-                          zone: item.zone,
-                        },
+                  <h3>
+                    {{ t('data-planes.routes.item.config') }}
+                  </h3>
+                  <div>
+                    <XSelect
+                      :label="t('data-planes.routes.item.format')"
+                      :selected="route.params.format"
+                      @change="(value) => {
+                        route.update({ format: value })
                       }"
                     >
-                      {{ item.zone }}
-                    </XAction>
-                  </template>
-                </DefinitionCard>
-                <DefinitionCard
-                  layout="horizontal"
-                >
-                  <template #title>
-                    {{ t('http.api.property.modificationTime') }}
-                  </template>
+                      <template
+                        v-for="value in ['structured', 'yaml']"
+                        :key="value"
+                        #[`${value}-option`]
+                      >
+                        {{ t(`data-planes.routes.item.formats.${value}`) }}
+                      </template>
+                    </XSelect>
+                  </div>
+                </XLayout>
+              </header>
+            </XLayout>
 
-                  <template #body>
-                    {{ t('common.formats.datetime', { value: Date.parse(item.modificationTime) }) }}
-                  </template>
-                </DefinitionCard>
-              </div>
-
+            <template v-if="route.params.format === 'structured'">
               <XLayout
-                v-if="item.dataplane.networking.gateway"
                 type="stack"
+                data-testid="structured-view"
               >
-                <h3>{{ t('data-planes.routes.item.gateway') }}</h3>
-
                 <div
                   class="stack-with-borders"
                 >
@@ -174,14 +96,39 @@
                     layout="horizontal"
                   >
                     <template #title>
-                      {{ t('http.api.property.tags') }}
+                      {{ t('http.api.property.status') }}
                     </template>
 
                     <template #body>
-                      <TagList
-                        alignment="right"
-                        :tags="item.dataplane.networking.gateway.tags"
-                      />
+                      <XLayout
+                        type="separated"
+                      >
+                        <StatusBadge
+                          :status="item.status"
+                        />
+
+                        <DataCollection
+                          v-if="item.dataplaneType === 'standard'"
+                          :items="item.dataplane.networking.inbounds"
+                          :predicate="item => item.state !== 'Ready'"
+                          :empty="false"
+                          v-slot="{ items : inbounds }"
+                        >
+                          <XIcon
+                            name="info"
+                            :color="KUI_COLOR_TEXT_NEUTRAL"
+                          >
+                            <ul>
+                              <li
+                                v-for="inbound in inbounds"
+                                :key="`${inbound.service}:${inbound.port}`"
+                              >
+                                {{ t('data-planes.routes.item.unhealthy_inbound', { service: inbound.service, port: inbound.port }) }}
+                              </li>
+                            </ul>
+                          </XIcon>
+                        </DataCollection>
+                      </XLayout>
                     </template>
                   </DefinitionCard>
 
@@ -189,55 +136,140 @@
                     layout="horizontal"
                   >
                     <template #title>
-                      {{ t('http.api.property.address') }}
+                      Type
                     </template>
 
                     <template #body>
-                      <XCopyButton
-                        :text="`${item.dataplane.networking.address}`"
-                      />
+                      {{ t(`data-planes.type.${item.dataplaneType}`) }}
+                    </template>
+                  </DefinitionCard>
+
+                  <DefinitionCard
+                    v-if="item.namespace.length > 0"
+                    layout="horizontal"
+                  >
+                    <template #title>
+                      {{ t('data-planes.routes.item.namespace') }}
+                    </template>
+
+                    <template #body>
+                      {{ item.namespace }}
+                    </template>
+                  </DefinitionCard>
+
+                  <DefinitionCard
+                    v-if="can('use zones') && item.zone"
+                    layout="horizontal"
+                  >
+                    <template
+                      #title
+                    >
+                      Zone
+                    </template>
+                    <template
+                      #body
+                    >
+                      <XAction
+                        :to="{
+                          name: 'zone-cp-detail-view',
+                          params: {
+                            zone: item.zone,
+                          },
+                        }"
+                      >
+                        {{ item.zone }}
+                      </XAction>
+                    </template>
+                  </DefinitionCard>
+                  <DefinitionCard
+                    layout="horizontal"
+                  >
+                    <template #title>
+                      {{ t('http.api.property.modificationTime') }}
+                    </template>
+
+                    <template #body>
+                      {{ t('common.formats.datetime', { value: Date.parse(item.modificationTime) }) }}
                     </template>
                   </DefinitionCard>
                 </div>
+
+                <XLayout
+                  v-if="item.dataplane.networking.gateway"
+                  type="stack"
+                >
+                  <h3>{{ t('data-planes.routes.item.gateway') }}</h3>
+
+                  <div
+                    class="stack-with-borders"
+                  >
+                    <DefinitionCard
+                      layout="horizontal"
+                    >
+                      <template #title>
+                        {{ t('http.api.property.tags') }}
+                      </template>
+
+                      <template #body>
+                        <TagList
+                          alignment="right"
+                          :tags="item.dataplane.networking.gateway.tags"
+                        />
+                      </template>
+                    </DefinitionCard>
+
+                    <DefinitionCard
+                      layout="horizontal"
+                    >
+                      <template #title>
+                        {{ t('http.api.property.address') }}
+                      </template>
+
+                      <template #body>
+                        <XCopyButton
+                          :text="`${item.dataplane.networking.address}`"
+                        />
+                      </template>
+                    </DefinitionCard>
+                  </div>
+                </XLayout>
               </XLayout>
-            </XLayout>
+            </template>
 
-            <XLayout
-              type="stack"
-            >
-              <h3>
-                {{ t('data-planes.routes.item.config') }}
-              </h3>
-
-              <ResourceCodeBlock
-                :resource="item.config"
-                language="yaml"
-                is-searchable
-                :query="route.params.codeSearch"
-                :is-filter-mode="route.params.codeFilter"
-                :is-reg-exp-mode="route.params.codeRegExp"
-                @query-change="route.update({ codeSearch: $event })"
-                @filter-mode-change="route.update({ codeFilter: $event })"
-                @reg-exp-mode-change="route.update({ codeRegExp: $event })"
-                v-slot="{ copy, copying }"
+            <template v-else>
+              <XLayout
+                type="stack"
               >
-                <DataSource
-                  v-if="copying"
-                  :src="uri(sources, `/meshes/:mesh/dataplanes/:name/as/kubernetes`, {
-                    mesh: route.params.mesh,
-                    name: route.params.dataPlane,
-                  }, {
-                    cacheControl: 'no-store',
-                  })"
-                  @change="(data) => {
-                    copy((resolve) => resolve(data))
-                  }"
-                  @error="(e) => {
-                    copy((_resolve, reject) => reject(e))
-                  }"
-                />
-              </ResourceCodeBlock>
-            </XLayout>
+                <ResourceCodeBlock
+                  :resource="item.config"
+                  language="yaml"
+                  is-searchable
+                  :query="route.params.codeSearch"
+                  :is-filter-mode="route.params.codeFilter"
+                  :is-reg-exp-mode="route.params.codeRegExp"
+                  @query-change="route.update({ codeSearch: $event })"
+                  @filter-mode-change="route.update({ codeFilter: $event })"
+                  @reg-exp-mode-change="route.update({ codeRegExp: $event })"
+                  v-slot="{ copy, copying }"
+                >
+                  <DataSource
+                    v-if="copying"
+                    :src="uri(sources, `/meshes/:mesh/dataplanes/:name/as/kubernetes`, {
+                      mesh: route.params.mesh,
+                      name: route.params.dataPlane,
+                    }, {
+                      cacheControl: 'no-store',
+                    })"
+                    @change="(data) => {
+                      copy((resolve) => resolve(data))
+                    }"
+                    @error="(e) => {
+                      copy((_resolve, reject) => reject(e))
+                    }"
+                  />
+                </ResourceCodeBlock>
+              </XLayout>
+            </template>
           </AppView>
         </template>
       </template>
