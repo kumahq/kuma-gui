b0aade1e870aebb497a89b031eedabe680e570a3
schogges <moritz.fleck@konghq.com>
2025-01-22 10:29:18 +0100

    feat(zone-egress-summary-view): use dropdown to select config format
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/kuma-gui/src/app/zone-egresses/views/ZoneEgressSummaryView.vue
+++ packages/kuma-gui/src/app/zone-egresses/views/ZoneEgressSummaryView.vue
@@ -6,6 +6,7 @@
       codeSearch: '',
       codeFilter: false,
       codeRegExp: false,
+      format: 'structured',
     }"
     v-slot="{ route, t }"
   >
@@ -53,88 +54,121 @@
               </h2>
             </template>
 
-            <div
-              class="stack-with-borders"
+            <XLayout
+              type="stack"
             >
-              <DefinitionCard
-                layout="horizontal"
-              >
-                <template #title>
-                  {{ t('http.api.property.status') }}
-                </template>
-
-                <template #body>
-                  <StatusBadge
-                    :status="item.state"
-                  />
-                </template>
-              </DefinitionCard>
-
-              <DefinitionCard
-                v-if="item.namespace.length > 0"
-                layout="horizontal"
-              >
-                <template #title>
-                  {{ t('data-planes.routes.item.namespace') }}
-                </template>
-
-                <template #body>
-                  {{ item.namespace }}
-                </template>
-              </DefinitionCard>
-
-              <DefinitionCard
-                layout="horizontal"
-              >
-                <template #title>
-                  {{ t('http.api.property.address') }}
-                </template>
-
-                <template #body>
-                  <template
-                    v-if="item.zoneEgress.socketAddress.length > 0"
-                  >
-                    <XCopyButton
-                      :text="item.zoneEgress.socketAddress"
-                    />
-                  </template>
-
-                  <template v-else>
-                    {{ t('common.detail.none') }}
-                  </template>
-                </template>
-              </DefinitionCard>
-            </div>
-            <div>
-              <h3>
-                {{ t('zone-ingresses.routes.item.config') }}
-              </h3>
-
-              <div class="mt-4">
-                <ResourceCodeBlock
-                  :resource="item.config"
-                  is-searchable
-                  :query="route.params.codeSearch"
-                  :is-filter-mode="route.params.codeFilter"
-                  :is-reg-exp-mode="route.params.codeRegExp"
-                  @query-change="route.update({ codeSearch: $event })"
-                  @filter-mode-change="route.update({ codeFilter: $event })"
-                  @reg-exp-mode-change="route.update({ codeRegExp: $event })"
-                  v-slot="{ copy, copying }"
+              <header>
+                <XLayout
+                  type="separated"
+                  size="max"
                 >
-                  <DataSource
-                    v-if="copying"
-                    :src="`/zone-egresses/${route.params.zoneEgress}/as/kubernetes?no-store`"
-                    @change="(data) => {
-                      copy((resolve) => resolve(data))
-                    }"
-                    @error="(e) => {
-                      copy((_resolve, reject) => reject(e))
-                    }"
-                  />
-                </ResourceCodeBlock>
-              </div>
-            </div>
+                  <h3>
+                    {{ t('zone-ingresses.routes.item.config') }}
+                  </h3>
+                  <div>
+                    <XSelect
+                      :label="t('zone-ingresses.routes.items.format')"
+                      :selected="route.params.format"
+                      @change="(value) => {
+                        route.update({ format: value })
+                      }"
+                    >
+                      <template
+                        v-for="value in ['structured', 'yaml']"
+                        :key="value"
+                        #[`${value}-option`]
+                      >
+                        {{ t(`zone-ingresses.routes.items.formats.${value}`) }}
+                      </template>
+                    </XSelect>
+                  </div>
+                </XLayout>
+              </header>
+              <template v-if="route.params.format === 'structured'">
+                <XLayout
+                  type="stack"
+                  class="stack-with-borders"
+                  data-testid="structured-view"
+                >
+                  <DefinitionCard
+                    layout="horizontal"
+                  >
+                    <template #title>
+                      {{ t('http.api.property.status') }}
+                    </template>
+                
+                    <template #body>
+                      <StatusBadge
+                        :status="item.state"
+                      />
+                    </template>
+                  </DefinitionCard>
+              
+                  <DefinitionCard
+                    v-if="item.namespace.length > 0"
+                    layout="horizontal"
+                  >
+                    <template #title>
+                      {{ t('data-planes.routes.item.namespace') }}
+                    </template>
+              
+                    <template #body>
+                      {{ item.namespace }}
+                    </template>
+                  </DefinitionCard>
+            
+                  <DefinitionCard
+                    layout="horizontal"
+                  >
+                    <template #title>
+                      {{ t('http.api.property.address') }}
+                    </template>
+            
+                    <template #body>
+                      <template
+                        v-if="item.zoneEgress.socketAddress.length > 0"
+                      >
+                        <XCopyButton
+                          :text="item.zoneEgress.socketAddress"
+                        />
+                      </template>
+            
+                      <template v-else>
+                        {{ t('common.detail.none') }}
+                      </template>
+                    </template>
+                  </DefinitionCard>
+                </XLayout>
+              </template>
+              <template v-else>
+                <div>
+                  <div class="mt-4">
+                    <ResourceCodeBlock
+                      :resource="item.config"
+                      is-searchable
+                      :query="route.params.codeSearch"
+                      :is-filter-mode="route.params.codeFilter"
+                      :is-reg-exp-mode="route.params.codeRegExp"
+                      @query-change="route.update({ codeSearch: $event })"
+                      @filter-mode-change="route.update({ codeFilter: $event })"
+                      @reg-exp-mode-change="route.update({ codeRegExp: $event })"
+                      v-slot="{ copy, copying }"
+                    >
+                      <DataSource
+                        v-if="copying"
+                        :src="`/zone-egresses/${route.params.zoneEgress}/as/kubernetes?no-store`"
+                        @change="(data) => {
+                          copy((resolve) => resolve(data))
+                        }"
+                        @error="(e) => {
+                          copy((_resolve, reject) => reject(e))
+                        }"
+                      />
+                    </ResourceCodeBlock>
+                  </div>
+                </div>
+              </template>
+            </XLayout>
           </AppView>
         </template>
       </template>
