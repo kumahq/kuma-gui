f9fd93b47670a2dfe4155352f05e801953ee468e
John Cowen <johncowen@users.noreply.github.com>
2025-02-25 10:52:23 +0000

    feat(zones): adds filter based pagination to zone proxies (#3584)
    Following https://github.com/kumahq/kuma/pull/12840 this PR adds support
    for proper paging of Zone Proxies via the GUI. Previousy we would just
    request a large number of Zone Proxies and filter for the ones we were
    interested in, with no paging (max of 100 proxies)
    
    The GUI now pages Zone Ingresses and Zone Egresses just like any other
    resource in the GUI
    
    Closes https://github.com/kumahq/kuma-gui/issues/3521
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- packages/kuma-gui/src/app/zone-ingresses/views/ZoneIngressListView.vue
+++ packages/kuma-gui/src/app/zone-ingresses/views/ZoneIngressListView.vue
@@ -1,10 +1,9 @@
 <template>
-  <!-- TODO: Update page & size once the list endpoint is being filtered by zone -->
   <RouteView
     name="zone-ingress-list-view"
     :params="{
-      /* page: 1, */
-      /* size: me.pageSize, */
+      page: 1,
+      size: Number,
       zone: '',
       proxy: '',
     }"
@@ -22,129 +21,124 @@
         default-path="common.i18n.ignore-error"
       />
       <XCard>
-        <!-- TODO: Update page & size once the list endpoint is being filtered by zone -->
         <DataLoader
           :src="uri(sources, `/zone-cps/:name/ingresses`, {
             name: route.params.zone,
           }, {
-            page: 1,
-            size: 100,
+            page: route.params.page,
+            size: route.params.size,
           })"
+          variant="list"
+          v-slot="{ data }"
         >
-          <template
-            #loadable="{ data: ingresses }"
+          <DataCollection
+            type="zone-ingresses"
+            :items="data.items"
+            :page="route.params.page"
+            :page-size="route.params.size"
+            :total="data.total"
+            @change="route.update"
           >
-            <DataCollection
-              type="zone-ingresses"
-              :items="ingresses?.items ?? [undefined]"
-              :total="ingresses?.total"
-              @change="route.update"
+            <AppCollection
+              class="zone-ingress-collection"
+              data-testid="zone-ingress-collection"
+              :headers="[
+                { ...me.get('headers.name'), label: 'Name', key: 'name' },
+                { ...me.get('headers.socketAddress'), label: 'Address', key: 'socketAddress' },
+                { ...me.get('headers.advertisedSocketAddress'), label: 'Advertised address', key: 'advertisedSocketAddress' },
+                { ...me.get('headers.status'), label: 'Status', key: 'status' },
+                { ...me.get('headers.actions'), label: 'Actions', key: 'actions', hideLabel: true },
+              ]"
+              :items="data.items"
+              :is-selected-row="(row) => row.name === route.params.proxy"
+              @resize="me.set"
             >
-              <!-- TODO: Update page & size once the list endpoint is being filtered by zone -->
-              <AppCollection
-                class="zone-ingress-collection"
-                data-testid="zone-ingress-collection"
-                :headers="[
-                  { ...me.get('headers.name'), label: 'Name', key: 'name' },
-                  { ...me.get('headers.socketAddress'), label: 'Address', key: 'socketAddress' },
-                  { ...me.get('headers.advertisedSocketAddress'), label: 'Advertised address', key: 'advertisedSocketAddress' },
-                  { ...me.get('headers.status'), label: 'Status', key: 'status' },
-                  { ...me.get('headers.actions'), label: 'Actions', key: 'actions', hideLabel: true },
-                ]"
-                :items="ingresses?.items"
-                :is-selected-row="(row) => row.name === route.params.proxy"
-                @resize="me.set"
-              >
-                <template #name="{ row: item }">
+              <template #name="{ row: item }">
+                <XAction
+                  data-action
+                  :to="{
+                    name: 'zone-ingress-summary-view',
+                    params: {
+                      zone: route.params.zone,
+                      proxy: item.id,
+                      proxyType: 'ingresses',
+                    },
+                    query: {
+                      page: route.params.page,
+                      size: route.params.size,
+                    },
+                  }"
+                >
+                  {{ item.name }}
+                </XAction>
+              </template>
+
+              <template #socketAddress="{ row: item }">
+                <XCopyButton
+                  v-if="item.zoneIngress.socketAddress.length > 0"
+                  :text="item.zoneIngress.socketAddress"
+                />
+                <template v-else>
+                  {{ t('common.collection.none') }}
+                </template>
+              </template>
+
+              <template #advertisedSocketAddress="{ row: item }">
+                <XCopyButton
+                  v-if="item.zoneIngress.advertisedSocketAddress.length > 0"
+                  :text="item.zoneIngress.advertisedSocketAddress"
+                />
+                <template v-else>
+                  {{ t('common.collection.none') }}
+                </template>
+              </template>
+
+              <template #status="{ row: item }">
+                <StatusBadge
+                  :status="item.state"
+                />
+              </template>
+
+              <template #actions="{ row: item }">
+                <XActionGroup>
                   <XAction
-                    data-action
                     :to="{
-                      name: 'zone-ingress-summary-view',
+                      name: 'zone-ingress-detail-view',
                       params: {
-                        zone: route.params.zone,
                         proxy: item.id,
                         proxyType: 'ingresses',
                       },
-                      query: {
-                        // TODO: Update page & size once the list endpoint is being filtered by zone
-                        page: 1,
-                        size: 100,
-                      },
                     }"
                   >
-                    {{ item.name }}
+                    {{ t('common.collection.actions.view') }}
                   </XAction>
-                </template>
+                </XActionGroup>
+              </template>
+            </AppCollection>
 
-                <template #socketAddress="{ row: item }">
-                  <XCopyButton
-                    v-if="item.zoneIngress.socketAddress.length > 0"
-                    :text="item.zoneIngress.socketAddress"
-                  />
-                  <template v-else>
-                    {{ t('common.collection.none') }}
-                  </template>
-                </template>
-
-                <template #advertisedSocketAddress="{ row: item }">
-                  <XCopyButton
-                    v-if="item.zoneIngress.advertisedSocketAddress.length > 0"
-                    :text="item.zoneIngress.advertisedSocketAddress"
-                  />
-                  <template v-else>
-                    {{ t('common.collection.none') }}
-                  </template>
-                </template>
-
-                <template #status="{ row: item }">
-                  <StatusBadge
-                    :status="item.state"
-                  />
-                </template>
-
-                <template #actions="{ row: item }">
-                  <XActionGroup>
-                    <XAction
-                      :to="{
-                        name: 'zone-ingress-detail-view',
-                        params: {
-                          proxy: item.id,
-                          proxyType: 'ingresses',
-                        },
-                      }"
-                    >
-                      {{ t('common.collection.actions.view') }}
-                    </XAction>
-                  </XActionGroup>
-                </template>
-              </AppCollection>
-
-              <RouterView
-                v-if="route.child()"
-                v-slot="{ Component }"
+            <RouterView
+              v-if="route.child()"
+              v-slot="{ Component }"
+            >
+              <SummaryView
+                @close="route.replace({
+                  name: 'zone-ingress-list-view',
+                  params: {
+                    zone: route.params.zone,
+                  },
+                  query: {
+                    page: route.params.page,
+                    size: route.params.size,
+                  },
+                })"
               >
-                <SummaryView
-                  @close="route.replace({
-                    name: 'zone-ingress-list-view',
-                    params: {
-                      zone: route.params.zone,
-                    },
-                    query: {
-                      // TODO: Update page & size once the list endpoint is being filtered by zone
-                      page: 1,
-                      size: 100,
-                    },
-                  })"
-                >
-                  <component
-                    :is="Component"
-                    v-if="typeof ingresses !== 'undefined'"
-                    :items="ingresses.items"
-                  />
-                </SummaryView>
-              </RouterView>
-            </DataCollection>
-          </template>
+                <component
+                  :is="Component"
+                  :items="data.items"
+                />
+              </SummaryView>
+            </RouterView>
+          </DataCollection>
         </DataLoader>
       </XCard>
     </AppView>
@@ -156,8 +150,4 @@ import { sources } from '../sources'
 import AppCollection from '@/app/application/components/app-collection/AppCollection.vue'
 import StatusBadge from '@/app/common/StatusBadge.vue'
 import SummaryView from '@/app/common/SummaryView.vue'
-import type { ZoneOverview } from '@/app/zones/data'
-const _props = defineProps<{
-  data: ZoneOverview
-}>()
 </script>
