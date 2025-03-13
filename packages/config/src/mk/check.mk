d251a9259b0757115394868ad666d3347b93b82c
John Cowen <john.cowen@konghq.com>
2025-03-13 18:32:57 +0000

    chore(docs): adds improved docs for installing nvm
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- packages/config/src/mk/check.mk
+++ packages/config/src/mk/check.mk
@@ -1,10 +1,18 @@
-NODE_VERSION?=v$(shell cat .nvmrc)
-NPM_VERSION?=$(shell cat $(NPM_WORKSPACE_ROOT)/package.json | jq -r '.engines.npm')
-
 .PHONY: check/node
+check/node: NPM_VERSION:=v$(shell cat $(NPM_WORKSPACE_ROOT)/package.json | jq -r '.engines.npm')
+check/node: NODE_VERSION:=v$(shell grep -v -e "^#" -e "^$$" $(NPM_WORKSPACE_ROOT)/.nvmrc)
 check/node:
 	@node -v | grep $(NODE_VERSION) &> /dev/null || ( \
-		echo "Make sure node-$(NODE_VERSION) is installed (see .nvmrc)"; \
+		echo "Make sure node-$(NODE_VERSION) is installed (please see .nvmrc for nvm installation)"; \
+		echo ""; \
+		echo "TLDR:"; \
+		echo "---"; \
+		echo "You can run \`nvm install\` if you have nvm installed, otherwise to install nvm run"; \
+		echo "\`curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash\`"; \
+		echo "Once it's installed, run \`nvm install\`"; \
+		echo "---"; \
+		echo ""; \
+		echo "With the correct version of node is installed, then you can re-run your make target"; \
 		exit 1; \
 	)
 	@npm ls -g "npm@$(NPM_VERSION)" | grep "empty" > /dev/null && ( \
