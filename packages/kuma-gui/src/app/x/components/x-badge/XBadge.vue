005e9394b0adc546ce8dbe7f123b61f62505181c
schogges <moritz.fleck@konghq.com>
2025-04-17 10:05:28 +0200

    remove explicit appearance prop from xbadge
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/kuma-gui/src/app/x/components/x-badge/XBadge.vue
+++ packages/kuma-gui/src/app/x/components/x-badge/XBadge.vue
@@ -1,6 +1,5 @@
 <template>
   <KBadge
-    :appearance="props.appearance"
     :max-width="props.maxWidth"
   >
     <slot name="default" />
@@ -11,9 +10,7 @@
 import { KBadge } from '@kong/kongponents'
 const props = withDefaults(defineProps<{
   maxWidth?: string
-  appearance?: InstanceType<typeof KBadge>['$props']['appearance']
 }>(), {
   maxWidth: 'auto',
-  appearance: undefined,
 })
 </script>
