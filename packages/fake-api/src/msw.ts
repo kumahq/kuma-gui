0ebded69c7119c137a7e815d2feef4216946eb17
John Cowen <johncowen@users.noreply.github.com>
2025-05-13 10:08:41 +0100

    chore(ci): reduce the majority of fake-api to a createFetch function (#3790)
    Note this was reviewed previously and reverted, I've made additional
    amends to the original PR in the second commit here to keep the changes
    separate. I've also done some "pre-testing" elsewhere to make sure it
    works once we merge to try and avoid having to revert again if I messed
    up.
    
    ---
    
    Refactors some more of fake-api, mostly:
    
    - Reduce everything down to a reusable `createFetch`/`createFetchSync`,
    that returns something that is very fetch-like (i.e. very recognisable).
    Note: `createFetchSync` is needed for Cypress which is notorious weird
    with async things.
    - I moved the YAML/JSON merging we do for overwriting responses during
    tests out of fake-api entirely. If you aren't doing tests like we are
    then you wouldn't need this in order to build basic fake-apis.
    
    Probably a few other little bits. Unfortunately the PR is a little
    large, if it was me I'd look at/compare this locally to get a better
    feel for the improvement here.
    
    We are still not quite done here, but with what we did before extracting
    things out into its own module, and what is here, I'm far happier with
    this code now.
    
    ---------
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- packages/fake-api/src/msw.ts
+++ packages/fake-api/src/msw.ts
@@ -1,92 +1,81 @@
 import { http, HttpResponse, passthrough } from 'msw'
 
-import { createMerge, escapeRoute } from './index.ts'
-import type { Callback, Dependencies, FS, MockEndpoint, MockResponse, Options, RestRequest } from './index.ts'
+import { createFetch } from './index.ts'
+import type { Dependencies, FS, MockEndpoint } from './index.ts'
 
-const noop: Callback = (_merge, _req, response) => response
-
-export const useResponder = <TDependencies extends object = {}>(fs: FS, dependencies: Dependencies<TDependencies>) => {
-  return (route: string, opts: Options = {}, cb: Callback = noop) => {
-    const mockEnv = (key: string, d = '') => (opts[key] ?? '') || dependencies.env(key, d)
-    if (route !== '*') {
-      dependencies.fake.seed(typeof opts.FAKE_SEED !== 'undefined' ? parseInt(typeof opts.FAKE_SEED) : 1)
-    }
-    const endpoint = fs[route]
-    const fetch = endpoint({
-      ...dependencies,
-      env: mockEnv,
-    })
-    return async (req: RestRequest): Promise<MockResponse> => {
-      const _response = fetch(req)
-      const latency = parseInt(mockEnv('KUMA_LATENCY', '0'))
-      if (latency !== 0) {
-        await new Promise(resolve => setTimeout(resolve, latency))
-      }
-      return cb(createMerge(_response), req, _response)
-    }
-  }
-}
-
-export const server = <TDependencies extends object = {}, TEnvKeys extends string = string>(mock: MockEndpoint<TDependencies>, options: {
-  env?: Record<TEnvKeys, string>
-  params?: Record<string, string>
-}, dependencies: Dependencies<TDependencies>) => {
+export const server = <TDependencies extends object = {}>(
+  mock: MockEndpoint<TDependencies>,
+  options: {
+    params?: Record<string, string>
+  },
+  dependencies: Dependencies<TDependencies>,
+) => {
   return async (env: Record<string, string>) => {
-    const responder = useResponder({
-      _: mock,
-    }, {
-      ...dependencies,
-      env: (key: keyof typeof env, d = '') => env[key] ?? d,
-    })
-    const request = responder('_')
-    return (await request(
-      {
-        method: 'GET',
-        body: {},
-        url: {
-          searchParams: new URLSearchParams(),
-        },
-        params: options.params ?? {},
+
+    dependencies.env = (key: keyof typeof env, d = '') => {
+      return env[key] ?? d
+    }
+
+    const route = Object.keys(options.params ?? {}).reduce((prev, item) => {
+      return `${prev}:${item}/`
+    }, 'http://localhost/')
+    const path = Object.values(options.params ?? {}).reduce((prev, item) => {
+      return `${prev}${item}/`
+    }, 'http://localhost/')
+
+    const fetch = createFetch({
+      dependencies,
+      fs: {
+        [route]: mock,
       },
-    ))?.body
+    })
+    const response = await fetch(path, {
+      method: 'GET',
+    })
+    return response.json()
   }
 }
 
 export const handler = <TDependencies extends object = {}>(fs: FS, dependencies: Dependencies<TDependencies>) => {
-  const baseUrl = dependencies.env('KUMA_API_URL')
-  const responder = useResponder(fs, dependencies)
+  const fetch = createFetch({
+    dependencies,
+    fs,
+  })
   return (route: string) => {
-    const respond = responder(route)
-    const base = route.includes('://') ? '' : baseUrl
-    if (route.startsWith('://')) {
-      route = route.replace('://', '')
-    }
-    return http.all(`${base}${escapeRoute(route)}`, async ({ request, params }) => {
-      const response = await respond({
-        method: request.method,
-        url: new URL(request.url),
-        body: request.body ? JSON.parse(await new Response(request.body).text() || '{}') : {},
-        params: Object.fromEntries(
-          Object.entries(params).filter(
-            (entry): entry is [string, string | readonly string[]] => typeof entry[1] !== 'undefined',
-          ),
-        ),
+    return http.all(`${route}`, async ({ request: req }) => {
+      // headers can be string | string[] | undefined, not string
+      const headers = Object.entries(req.headers).reduce((prev, [key, item]) => {
+        if (typeof item !== 'undefined') {
+          prev[key] = Array.isArray(item) ? item[0] : item
+        }
+        return prev
+      }, {} as Record<string, string>)
+      const response = await fetch(`${req.url ?? ''}`, {
+        method: req.method,
+        headers,
+        body: req.body ? JSON.parse(await new Response(req.body).text() || '{}') : {},
       })
-
+      const latency = parseInt(dependencies.env('KUMA_LATENCY', '0'))
+      if (latency !== 0) {
+        await new Promise(resolve => setTimeout(resolve, latency))
+      }
       if (typeof response === 'undefined') {
         return passthrough()
       }
-
-      return HttpResponse.json(response.body, {
-        status: parseInt(response.headers?.['Status-Code'] ?? '200'),
+      return HttpResponse.json((await response.json()), {
+        status: parseInt(response.headers?.get('Status-Code') ?? '200'),
       })
     })
   }
 }
 
 export const mswHandlers = <TDependencies extends object = {}>(fs: FS, dependencies: Dependencies<TDependencies>) => {
-  const handlerFor = handler(fs, dependencies)
-  return Object.keys(fs).map(route => {
+  const baseUrl = dependencies.env('KUMA_API_URL')
+  const _fs = Object.fromEntries(Object.entries(fs).map(([route, response]) => {
+    return [route.includes('://') ? route : `${baseUrl}${route}`, response]
+  }))
+  const handlerFor = handler(_fs, dependencies)
+  return Object.keys(_fs).map(route => {
     return handlerFor(route)
   })
 }
