ad66d6f924ab80e1ba81e1ae8f1ec80a4e2333c6
schogges <moritz.fleck@konghq.com>
2025-05-20 16:21:10 +0200

    Resource.spec: use one describe
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/kuma-gui/src/app/resources/data/Resource.spec.ts
+++ packages/kuma-gui/src/app/resources/data/Resource.spec.ts
@@ -3,112 +3,115 @@ import { describe, expect, test } from 'vitest'
 import { Resource } from './Resource'
 
 describe('Resource.search', () => {
-  describe('commons', () => {
-    test.each([
+  test.each([
     // Name
-      [
-        'foo',
-        {
-          name: 'foo',
-        },
-      ],
-      [
-        'name:foo',
-        {
-          name: 'foo',
-        },
-      ],
+    [
+      'foo',
+      {
+        name: 'foo',
+      },
+    ],
+    [
+      'name:foo',
+      {
+        name: 'foo',
+      },
+    ],
 
-      // Shortcuts
-      [
-        'namespace:foo',
-        {
-          ['filter[labels.k8s.kuma.io/namespace]']: 'foo',
-        },
-      ],
-      [
-        'zone:foo',
-        {
-          ['filter[labels.kuma.io/zone]']: 'foo',
-        },
-      ],
-      [
-        'service:foo',
-        {
-          ['filter[labels.kuma.io/service]']: 'foo',
-        },
-      ],
-      [
-        'namespace:foo service:bar zone:baz',
-        {
-          ['filter[labels.k8s.kuma.io/namespace]']: 'foo',
-          ['filter[labels.kuma.io/service]']: 'bar',
-          ['filter[labels.kuma.io/zone]']: 'baz',
-        },
-      ],
+    // Shortcuts
+    [
+      'namespace:foo',
+      {
+        ['filter[labels.k8s.kuma.io/namespace]']: 'foo',
+      },
+    ],
+    [
+      'zone:foo',
+      {
+        ['filter[labels.kuma.io/zone]']: 'foo',
+      },
+    ],
+    [
+      'service:foo',
+      {
+        ['filter[labels.kuma.io/service]']: 'foo',
+      },
+    ],
+    [
+      'namespace:foo service:bar zone:baz',
+      {
+        ['filter[labels.k8s.kuma.io/namespace]']: 'foo',
+        ['filter[labels.kuma.io/service]']: 'bar',
+        ['filter[labels.kuma.io/zone]']: 'baz',
+      },
+    ],
 
-      // Labels
-      [
-        'foo:bar',
-        {
-          ['filter[labels.foo]']: 'bar',
-        },
-      ],
-      [
-        'label:foo:bar',
-        {
-          ['filter[labels.foo]']: 'bar',
-        },
-      ],
-      [
-        'label: foo:bar',
-        {
-          ['filter[labels.foo]']: 'bar',
-        },
-      ],
-      [
-        'label:foo:bar label:baz=qux label: quux:quuux label: kuma.io/service=foo',
-        {
-          ['filter[labels.foo]']: 'bar',
-          ['filter[labels.baz]']: 'qux',
-          ['filter[labels.quux]']: 'quuux',
-          ['filter[labels.kuma.io/service]']: 'foo',
-        },
-      ],
+    // Labels
+    [
+      'foo:bar',
+      {
+        ['filter[labels.foo]']: 'bar',
+      },
+    ],
+    [
+      'label:foo:bar',
+      {
+        ['filter[labels.foo]']: 'bar',
+      },
+    ],
+    [
+      'label: foo:bar',
+      {
+        ['filter[labels.foo]']: 'bar',
+      },
+    ],
+    [
+      'label:foo:bar label:baz=qux label: quux:quuux label: kuma.io/service=foo',
+      {
+        ['filter[labels.foo]']: 'bar',
+        ['filter[labels.baz]']: 'qux',
+        ['filter[labels.quux]']: 'quuux',
+        ['filter[labels.kuma.io/service]']: 'foo',
+      },
+    ],
 
-      // Invalids
-      [
-        ':',
-        {},
-      ],
-      [
-        'foo:',
-        {},
-      ],
-      [
-        ':foo',
-        {},
-      ],
-    ])('%s works', (search, expected) => {
-      const actual = Resource.search(search)
-      expect(actual).toStrictEqual(expected)
-    })
+    // Invalids
+    [
+      ':',
+      {},
+    ],
+    [
+      'foo:',
+      {},
+    ],
+    [
+      ':foo',
+      {},
+    ],
+  ])('searching by %s works', (search, expected) => {
+    const actual = Resource.search(search)
+    expect(actual).toStrictEqual(expected)
   })
 
-  describe('defaultKey', () => {
-    for(const defaultKey in ['foo', 'bar']) {
-      test.each([
-        // Name
-        [
-          'foo',
-          {
-            [defaultKey]: 'foo',
-          },
-        ],
-      ])('%s works', (search, expected) => {
-        const actual = Resource.search(search, { defaultKey })
-        expect(actual).toStrictEqual(expected)
-      })
-    }
+  // defaultKey
+  test.each([
+    [
+      'bar',
+      'foo',
+      {
+        foo: 'bar',
+      },
+    ],
+    [
+      'foo',
+      'bar',
+      {
+        bar: 'foo',
+      },
+    ],
+  ])('using %s as the defaultKey works', (search, defaultKey, expected) => {
+    const actual = Resource.search(search, { defaultKey })
+    expect(actual).toStrictEqual(expected)
   })
+
 })
