fb5dd5ebe6132cf38e2235380a0807487004bbf7
schogges <moritz.fleck@konghq.com>
2025-06-10 15:00:20 +0200

    chore(config/cypress/e2e): ignore any ResizeObserver loop related error
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/config/src/cypress/e2e.ts
+++ packages/config/src/cypress/e2e.ts
@@ -2,8 +2,8 @@ import 'cypress-fail-fast'
 import failOnConsoleError from 'cypress-fail-on-console-error'
 import installLogsCollector from 'cypress-terminal-report/src/installLogsCollector'
 
-// ignore ResizeObserver loop errors, they seem to be a common occurance with Cypress
-Cypress.on('uncaught:exception', e => !e.message.includes('ResizeObserver loop limit exceeded'))
+// ignore ResizeObserver loop errors, they seem to be a common occurrence with Cypress
+Cypress.on('uncaught:exception', e => !e.message.includes('ResizeObserver loop'))
 
 installLogsCollector({
   collectTypes: ['cons:warn', 'cons:debug'],
