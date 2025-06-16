95d458e95f3e627e304336c2a497aa9387e91546
Mo <moritz.fleck@konghq.com>
2025-06-16 11:11:59 +0200

    revert changes to ControlPlaneActionGroup
    Signed-off-by: Mo <moritz.fleck@konghq.com>

--- packages/kuma-gui/src/app/control-planes/components/ControlPlaneActionGroup.vue
+++ packages/kuma-gui/src/app/control-planes/components/ControlPlaneActionGroup.vue
@@ -1,25 +1,19 @@
 <template>
-  <XLayout
-    type="separated"
-    justify="end"
-  >
-    <slot />
-    <XActionGroup>
-      <template #control>
-        <XAction
-          action="expand"
-          appearance="primary"
-        >
-          {{ t("main-overview.action_menu.toggle_button") }}
-        </XAction>
-      </template>
-      <XAction :to="{ name: 'hostname-generator-root-view' }">
-        {{ t("main-overview.action_menu.items.hostname_generators") }}
+  <XActionGroup>
+    <template #control>
+      <XAction
+        action="expand"
+        appearance="primary"
+      >
+        {{ t("main-overview.action_menu.toggle_button") }}
       </XAction>
+    </template>
+    <XAction :to="{ name: 'hostname-generator-root-view' }">
+      {{ t("main-overview.action_menu.items.hostname_generators") }}
+    </XAction>
 
-      <slot name="actions" />
-    </XActionGroup>
-  </XLayout>
+    <slot name="actions" />
+  </XActionGroup>
 </template>
 
 <script setup lang="ts">
