51aec4eea7d30fd191c43317424b819b95766787
John Cowen <john.cowen@konghq.com>
2025-06-24 15:53:24 +0100

    add additional touch, when cached don't check lockfile date
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- packages/config/src/mk/install.mk
+++ packages/config/src/mk/install.mk
@@ -1,11 +1,20 @@
+# In CI we use a cache id'ed by the has of the package-lock, so they must equal
+# each other therefore ignore the age of the package-lock if the cache is found
+# if the cache is not found, then node-modules will not exist and be made
+
+# The `touch` is used to ensure node_modules modification date is updated to
+# the same as the lockfile. It is possible (via branch switching) to keep
+# bumping the date of the lockfile, and a npm install will not bump the date of
+# node-modules, the touch make sure the date of node_modules is bumped always
 .PHONY: .install
 .install: check/node $(NPM_WORKSPACE_ROOT)/node_modules
 
-$(NPM_WORKSPACE_ROOT)/node_modules: $(NPM_WORKSPACE_ROOT)/package-lock.json
+$(NPM_WORKSPACE_ROOT)/node_modules: $(if $(CI),,$(NPM_WORKSPACE_ROOT)/package-lock.json)
 	@cd $(NPM_WORKSPACE_ROOT) \
 		&& npm $(if $(CI),clean-install,install) \
-					--ignore-scripts
-
+					--ignore-scripts \
+		&& touch $(NPM_WORKSPACE_ROOT)/node_modules
+#
 .PHONY: .sync
 .sync: check/node
 	@cd $(NPM_WORKSPACE_ROOT) \
