2c2d2ee6f018638ec9e1f76236ebf0aacd83d891
schogges <moritz.fleck@konghq.com>
2025-06-27 09:54:08 +0200

    chore(cypress): move step setup to main file
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/kuma-gui/cypress/support/step_definitions/index.ts
+++
@@ -1,8 +0,0 @@
-import { setupSteps } from '@kumahq/config/cypress/steps'
-
-import { useMock, useClient } from '../../services'
-
-const mock = useMock()
-const client = useClient()
-
-setupSteps({ mock, client })
