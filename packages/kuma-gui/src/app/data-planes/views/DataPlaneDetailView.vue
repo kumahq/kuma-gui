5fb8e604807322a257e71e91215a6eaa797a6781
schogges <moritz.fleck@konghq.com>
2025-07-02 14:04:52 +0200

    fix(dataplane-detail-view): infinite rerenders during development
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/kuma-gui/src/app/data-planes/views/DataPlaneDetailView.vue
+++ packages/kuma-gui/src/app/data-planes/views/DataPlaneDetailView.vue
@@ -644,75 +644,80 @@
 
               <XLayout>
                 <XI18n path="data-planes.routes.item.subscriptions.description" />
-                <AppCollection
-                  :headers="[
-                    { ...me.get('headers.connection'), label: '&nbsp;', key: 'connection' },
-                    { ...me.get('headers.instanceId'), label: t('http.api.property.instanceId'), key: 'instanceId' },
-                    { ...me.get('headers.version'), label: t('http.api.property.version'), key: 'version' },
-                    { ...me.get('headers.connected'), label: t('http.api.property.connected'), key: 'connected' },
-                    { ...me.get('headers.disconnected'), label: t('http.api.property.disconnected'), key: 'disconnected' },
-                    { ...me.get('headers.responses'), label: t('http.api.property.responses'), key: 'responses' },
-                  ]"
-                  :is-selected-row="item => item.id === route.params.subscription"
-                  :items="props.data.dataplaneInsight.subscriptions.map((_, i, arr) => arr[arr.length - (i + 1)])"
-                  @resize="me.set"
+                <DataLoader
+                  variant="list"
+                  :data="[props.data]"
                 >
-                  <template
-                    #connection="{ row: item }"
+                  <AppCollection
+                    :headers="[
+                      { ...me.get('headers.connection'), label: '&nbsp;', key: 'connection' },
+                      { ...me.get('headers.instanceId'), label: t('http.api.property.instanceId'), key: 'instanceId' },
+                      { ...me.get('headers.version'), label: t('http.api.property.version'), key: 'version' },
+                      { ...me.get('headers.connected'), label: t('http.api.property.connected'), key: 'connected' },
+                      { ...me.get('headers.disconnected'), label: t('http.api.property.disconnected'), key: 'disconnected' },
+                      { ...me.get('headers.responses'), label: t('http.api.property.responses'), key: 'responses' },
+                    ]"
+                    :is-selected-row="item => item.id === route.params.subscription"
+                    :items="props.data.dataplaneInsight.subscriptions.map((_, i, arr) => arr[arr.length - (i + 1)])"
+                    @resize="me.set"
                   >
                     <template
-                      v-for="connection in [item.connectTime && !item.disconnectTime ? 'healthy' : 'unhealthy'] as const"
-                      :key="`${connection}`"
+                      #connection="{ row: item }"
                     >
-                      <XIcon :name="connection">
-                        {{ t(`common.connection.${connection}`) }}
-                      </XIcon>
+                      <template
+                        v-for="connection in [item.connectTime && !item.disconnectTime ? 'healthy' : 'unhealthy'] as const"
+                        :key="`${connection}`"
+                      >
+                        <XIcon :name="connection">
+                          {{ t(`common.connection.${connection}`) }}
+                        </XIcon>
+                      </template>
                     </template>
-                  </template>
-                  <template
-                    #instanceId="{ row: item }"
-                  >
-                    <XAction
-                      data-action
-                      :to="{
-                        name: 'data-plane-subscription-summary-view',
-                        params: {
-                          subscription: item.id,
-                        },
-                      }"
-                    >
-                      {{ item.controlPlaneInstanceId }}
-                    </XAction>
-                  </template>
-                  <template
-                    #version="{ row: item }"
-                  >
-                    {{ item.version?.kumaDp?.version ?? '-' }}
-                  </template>
-                  <template
-                    #connected="{ row: item }"
-                  >
-                    {{ t('common.formats.datetime', { value: Date.parse(item.connectTime ?? '') }) }}
-                  </template>
-                  <template
-                    #disconnected="{ row: item }"
-                  >
                     <template
-                      v-if="item.disconnectTime"
+                      #instanceId="{ row: item }"
                     >
-                      {{ t('common.formats.datetime', { value: Date.parse(item.disconnectTime) }) }}
+                      <XAction
+                        data-action
+                        :to="{
+                          name: 'data-plane-subscription-summary-view',
+                          params: {
+                            subscription: item.id,
+                          },
+                        }"
+                      >
+                        {{ item.controlPlaneInstanceId }}
+                      </XAction>
                     </template>
-                  </template>
-                  <template
-                    #responses="{ row: item }"
-                  >
                     <template
-                      v-for="responses in [item.status?.total ?? {}]"
+                      #version="{ row: item }"
                     >
-                      {{ responses.responsesSent }}/{{ responses.responsesAcknowledged }}
+                      {{ item.version?.kumaDp?.version ?? '-' }}
                     </template>
-                  </template>
-                </AppCollection>
+                    <template
+                      #connected="{ row: item }"
+                    >
+                      {{ t('common.formats.datetime', { value: Date.parse(item.connectTime ?? '') }) }}
+                    </template>
+                    <template
+                      #disconnected="{ row: item }"
+                    >
+                      <template
+                        v-if="item.disconnectTime"
+                      >
+                        {{ t('common.formats.datetime', { value: Date.parse(item.disconnectTime) }) }}
+                      </template>
+                    </template>
+                    <template
+                      #responses="{ row: item }"
+                    >
+                      <template
+                        v-for="responses in [item.status?.total ?? {}]"
+                      >
+                        {{ responses.responsesSent }}/{{ responses.responsesAcknowledged }}
+                      </template>
+                    </template>
+                  </AppCollection>
+                </DataLoader>
               </XLayout>
             </XCard>
           </div>
