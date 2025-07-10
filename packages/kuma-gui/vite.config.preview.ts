b866a1cdb31af7d20717486a558b4a3dc2cd3d74
schogges <moritz.fleck@konghq.com>
2025-07-10 10:03:56 +0200

    mostly rollback vite.config.preview
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/kuma-gui/vite.config.preview.ts
+++ packages/kuma-gui/vite.config.preview.ts
@@ -1,12 +1,6 @@
-import { replicateKumaServer , defineConfig as defineBaseConfig, yamlLoaderPluginConfig, vuePluginConfig } from '@kumahq/config/vite'
-import fakeApi from '@kumahq/fake-api/vite'
-import yamlLoader from '@modyfi/vite-plugin-yaml'
-import vue from '@vitejs/plugin-vue'
-import { defineConfig, mergeConfig } from 'vite'
-import svgLoader from 'vite-svg-loader'
+import { replicateKumaServer } from '@kumahq/config/vite'
+import { defineConfig } from 'vite'
 
-import { dependencies } from './src/test-support'
-import { fs } from './src/test-support/mocks/fs'
 import type { UserConfigFn } from 'vite'
 
 export const config: UserConfigFn = () => {
@@ -15,20 +9,11 @@ export const config: UserConfigFn = () => {
       port: 5681,
     },
     plugins: [
-      vue(vuePluginConfig()),
-      yamlLoader(yamlLoaderPluginConfig()),
-      svgLoader(),
       replicateKumaServer({
         template: './dist/gui/index.html',
       }),
-      fakeApi({ dependencies, fs }),
     ],
   }
 }
 
-export default defineConfig((env) => 
-  mergeConfig(
-    defineBaseConfig(env),
-    config(env),
-  ),
-)
+export default defineConfig(config)
