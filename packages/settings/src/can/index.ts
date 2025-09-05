0a5785386ef5231e486766370cb5ebcaa48d727a
schogges <moritz.fleck@konghq.com>
2025-09-05 10:55:25 +0200

    remove '& string' from key lookups
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/settings/src/can/index.ts
+++ packages/settings/src/can/index.ts
@@ -1,13 +1,12 @@
-
 type EnsureFunction<T> = T extends (...args: any[]) => any ? T : never
 type ParamsExceptFirst<T extends any[]> = (T extends [any, ...infer Rest] ? Rest : []) | []
 
 export type Features<T extends Record<string, unknown>> = {
-  [K in keyof T & string]: ParamsExceptFirst<Parameters<EnsureFunction<T[K]>>> extends [] ? [K] : [K, ...ParamsExceptFirst<Parameters<EnsureFunction<T[K]>>>]
-}[keyof T & string]
+  [K in keyof T]: ParamsExceptFirst<Parameters<EnsureFunction<T[K]>>> extends [] ? [K] : [K, ...ParamsExceptFirst<Parameters<EnsureFunction<T[K]>>>]
+}[keyof T]
 
 export type FeatureSpec<T extends Record<string, unknown>> = {
-  [K in keyof T & string]: (
+  [K in keyof T]: (
     can: (...args: Features<T>) => boolean,
     //
     ...rest: ParamsExceptFirst<Parameters<EnsureFunction<T[K]>>>
