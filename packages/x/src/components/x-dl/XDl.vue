95a26cda90ca892d4b7034230a7690054e0926ea
John Cowen <john.cowen@konghq.com>
2025-09-09 15:31:40 +0100

    chore(x): adds XDl component
    Signed-off-by: John Cowen <john.cowen@konghq.com>

---
+++ packages/x/src/components/x-dl/XDl.vue
@@ -0,0 +1,40 @@
+<template>
+  <dl
+    :class="{
+      [`variant-${props.variant}`]: true,
+    }"
+  >
+    <slot name="default" />
+  </dl>
+</template>
+<script lang="ts" setup>
+
+const props = defineProps<{
+  variant: 'x-stack'
+}>()
+</script>
+
+<style lang="scss" scoped>
+/* variants */
+dl.variant-x-stack {
+  &, :deep(div) {
+    display: flex;
+    align-items: baseline;
+  }
+  :deep(div) {
+    gap: $kui-space-40;
+  }
+  & {
+    flex-wrap: wrap;
+    row-gap: $kui-space-30;
+    column-gap: $kui-space-70;
+  }
+  :deep(dt) {
+    gap: unset;
+    &::after {
+      content: ": ";
+      display: inline;
+    }
+  }
+}
+</style>
