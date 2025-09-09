26bf13498c605587c77a1e1c4bb3c51ed9c603af
schogges <moritz.fleck@konghq.com>
2025-09-09 16:17:53 +0200

    simplify types, add extra unit test
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/settings/src/can/index.spec.ts
+++ packages/settings/src/can/index.spec.ts
@@ -35,4 +35,15 @@ describe('can', () => {
     expect(can('use enabled-feature')).toBe(true)
     expect(can('use disabled-feature')).toBe(false)
   })
-})
+  test('allows multiple arguments to provide additional data as context', () => {
+    const can = features({
+      // @ts-expect-error -- need to allow in TS to pass extra arguments
+      'use enabled-feature': (_can, context: { enabled: boolean }) => context.enabled,
+      // @ts-expect-error -- need to allow in TS to pass extra arguments
+      'use disabled-feature': (can, context: { enabled: boolean }) => !can('use enabled-feature', context),
+    })
+    // @ts-expect-error -- need to allow in TS to pass extra arguments
+    expect(can('use enabled-feature', { enabled: true })).toBe(true)
+    // @ts-expect-error -- need to allow in TS to pass extra arguments
+    expect(can('use disabled-feature', { enabled: true })).toBe(false)
+  })})
--- packages/settings/src/can/index.ts
+++ packages/settings/src/can/index.ts
@@ -1,24 +1,18 @@
-type EnsureFunction<T> = T extends (...args: any[]) => any ? T : never
-type ParamsExceptFirst<T extends any[]> = (T extends [any, ...infer Rest] ? Rest : []) | []
+type Fn = (...args: any[]) => any
+type RestParams<T extends Fn> = Parameters<T> extends [any, ...infer Rest] ? Rest : []
 
-export type Features<T extends Record<string, unknown>> = {
-  [K in keyof T]: ParamsExceptFirst<Parameters<EnsureFunction<T[K]>>> extends [] ? [K] : [K, ...ParamsExceptFirst<Parameters<EnsureFunction<T[K]>>>]
+export type Features<T extends Record<string, Fn>> = {
+  [K in keyof T]: [K, ...RestParams<T[K]>]
 }[keyof T]
 
-export type FeatureSpec<T extends Record<string, unknown>> = {
+export type FeatureSpec<T extends Record<string, Fn>> = {
   [K in keyof T]: (
     can: (...args: Features<T>) => boolean,
-    //
-    ...rest: ParamsExceptFirst<Parameters<EnsureFunction<T[K]>>>
-    ///
+    ...rest: RestParams<T[K]>
   ) => any
 }
-// export type FeatureSpec<T extends Record<string, unknown>> = {
-//   [K in keyof T & string]: EnsureFunction<T[K]>
-// }
 
-
-export default <T extends Record<string, unknown>>(features: FeatureSpec<T>) => {
+export default <T extends Record<string, Fn>>(features: FeatureSpec<T>) => {
   const can = (...args: Features<T>) => {
     const [str, ...rest] = args
     const feature = features[str]
