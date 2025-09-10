965ea2f298786889e4b0a821f7b4b10bdf7e9d05
John Cowen <john.cowen@konghq.com>
2025-09-10 10:41:50 +0100

    feat(dataplanes): add link icon to linked badges
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- packages/x/src/components/x-badge/XBadge.vue
+++ packages/x/src/components/x-badge/XBadge.vue
@@ -3,14 +3,28 @@
     :max-width="props.maxWidth"
   >
     <slot name="default" />
+    <XIcon
+      v-if="xAction"
+      name="link"
+    />
   </KBadge>
 </template>
 
 <script lang="ts" setup>
 import { KBadge } from '@kong/kongponents'
+import { inject } from 'vue'
 const props = withDefaults(defineProps<{
   maxWidth?: string
 }>(), {
   maxWidth: 'auto',
 })
+const xAction = inject<{} | undefined>('x-action', undefined)
 </script>
+<style scoped>
+/* ideally we wouldn't use this, but its works to centralize the icon correctly */
+/* feel free to change if you find something else*/
+:deep(.x-icon-icon) {
+  position: relative;
+  top: -1px;
+}
+</style>
