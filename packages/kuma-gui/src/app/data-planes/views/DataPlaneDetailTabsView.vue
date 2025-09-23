aa0fbca1520393f3f452ae1582db03170f8f4771
John Cowen <johncowen@users.noreply.github.com>
2025-09-23 15:29:43 +0100

    feat(dataplanes): adds warning icon/tooltip to non-ready inbounds (#4261)
    Closes https://github.com/kumahq/kuma-gui/issues/3984
    
    ---
    
    Wanted to ask if you think we should remove the status circled in the
    screengrab below? We have the pattern of putting the "Status Type Thing"
    underneath the title so its visible no matter what tab you are on, and
    even before this PR it felt to me like we were doubling up data for no
    reason. Now we have the error message in the individual inbound card,
    this feels even more redundant. Wdyt? Should we remove this one? (the
    circled one)
    
    <img width="699" height="854" alt="Screenshot 2025-09-19 at 09 46 12"
    src="https://github.com/user-attachments/assets/b7c37684-a517-4f4e-ac45-e4d9339de44a"
    />
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- packages/kuma-gui/src/app/data-planes/views/DataPlaneDetailTabsView.vue
+++ packages/kuma-gui/src/app/data-planes/views/DataPlaneDetailTabsView.vue
@@ -7,218 +7,207 @@
     }"
     v-slot="{ route, t, uri }"
   >
-    <DataLoader
-      :src="uri(sources, '/meshes/:mesh/dataplane-overviews/:name', {
-        mesh: route.params.mesh,
-        name: route.params.proxy,
-      })"
-      v-slot="{ data, error }"
+    <AppView
+      :breadcrumbs="[
+        {
+          to: {
+            name: 'mesh-detail-view',
+            params: {
+              mesh: route.params.mesh,
+            },
+          },
+          text: route.params.mesh,
+        },
+        {
+          to: {
+            name: 'data-plane-list-view',
+            params: {
+              mesh: route.params.mesh,
+            },
+          },
+          text: t('data-planes.routes.item.breadcrumbs'),
+        },
+      ]"
     >
-      <AppView
-        :breadcrumbs="[
-          {
-            to: {
-              name: 'mesh-detail-view',
-              params: {
-                mesh: route.params.mesh,
-              },
-            },
-            text: route.params.mesh,
-          },
-          {
-            to: {
-              name: 'data-plane-list-view',
-              params: {
-                mesh: route.params.mesh,
-              },
-            },
-            text: t('data-planes.routes.item.breadcrumbs'),
-          },
-        ]"
+      <template
+        v-if="data"
+        #title
+      >
+        <XLayout size="small">
+          <h1>
+            <XCopyButton
+              :text="props.data.name"
+            >
+              <RouteTitle
+                :title="t('data-planes.routes.item.title', { name: props.data.name })"
+              />
+            </XCopyButton>
+          </h1>
+          <XBadge
+            :appearance="t(`common.status.appearance.${props.data.status}`, undefined, { defaultMessage: 'neutral' })"
+          >
+            {{ t(`http.api.value.${props.data.status}`) }}
+          </XBadge>
+        </XLayout>
+      </template>
+      <template
+        #actions
+      >
+        <XDisclosure
+          v-slot="{ expanded, toggle }"
+        >
+          <XAction
+            appearance="primary"
+            @click="toggle"
+          >
+            Download bundle
+          </XAction>
+          <XTeleportTemplate
+            v-if="expanded"
+            :to="{ name: 'modal-layer' }"
+          >
+            <XDisclosure
+              v-slot="{ expanded: downloading, toggle: download }"
+            >
+              <form
+                @submit.prevent="download"
+              >
+                <XModal
+                  :title="t('data-planes.routes.item.download.title')"
+                  @cancel="toggle"
+                >
+                  <fieldset
+                    :disabled="downloading"
+                  >
+                    <XI18n
+                      path="data-planes.routes.item.download.description"
+                    />
+                    <ul>
+                      <template
+                        v-for="(spec, key) in specs"
+                        :key="typeof spec"
+                      >
+                        <li
+                          v-if="key !== 'eds'"
+                        >
+                          <XCheckbox
+                            v-model="specs[key]"
+                            @change="(bool: boolean) => {
+                              if(key === 'xds' && !bool) {
+                                specs.eds = false
+                              }
+                            }"
+                          >
+                            {{ t(`data-planes.routes.item.download.options.${key}`) }}
+                          </XCheckbox>
+                          <ul
+                            v-if="key === 'xds'"
+                          >
+                            <li>
+                              <XCheckbox
+                                v-model="specs.eds"
+                                :disabled="!specs.xds"
+                              >
+                                {{ t('data-planes.routes.item.download.options.eds') }}
+                              </XCheckbox>
+                            </li>
+                          </ul>
+                        </li>
+                      </template>
+                    </ul>
+                  </fieldset>
+                  <template
+                    #footer-actions
+                  >
+                    <XLayout
+                      type="separated"
+                    >
+                      <XDownload
+                        @start="toggle"
+                        v-slot="{ download: bundle }"
+                      >
+                        <DataLoader
+                          variant="spinner"
+                          :src="downloading ? uri(sources, '/meshes/:mesh/dataplanes/:name/as/tarball/:spec', {
+                            mesh: route.params.mesh,
+                            name: route.params.proxy,
+                            spec: JSON.stringify(
+                              specs,
+                            ),
+                          }, {
+                            cacheControl: 'no-cache',
+                          }) : ''"
+                          @change="bundle"
+                          @error="download"
+                        >
+                          <template
+                            #error
+                          >
+                            <XAlert
+                              appearance="warning"
+                              show-icon
+                            >
+                              <XI18n
+                                t="data-planes.routes.item.download.error"
+                              />
+                            </XAlert>
+                          </template>
+                        </DataLoader>
+                      </XDownload>
+                      <XAction
+                        appearance="primary"
+                        type="submit"
+                        :disabled="downloading || Object.values(specs).every(bool => !bool)"
+                      >
+                        {{ t('data-planes.routes.item.download.action') }}
+                      </XAction>
+                    </XLayout>
+                  </template>
+                </XModal>
+              </form>
+            </XDisclosure>
+          </XTeleportTemplate>
+        </XDisclosure>
+      </template>
+
+      <XTabs
+        :selected="route.child()?.name"
       >
         <template
-          v-if="data"
-          #title
+          v-for="{ name } in route.children"
+          :key="name"
+          #[`${name}-tab`]
         >
-          <XLayout size="small">
-            <h1>
-              <XCopyButton
-                :text="data.name"
-              >
-                <RouteTitle
-                  :title="t('data-planes.routes.item.title', { name: data.name })"
-                />
-              </XCopyButton>
-            </h1>
-            <XBadge
-              :appearance="t(`common.status.appearance.${data.status}`, undefined, { defaultMessage: 'neutral' })"
-            >
-              {{ t(`http.api.value.${data.status}`) }}
-            </XBadge>
-          </XLayout>
+          <XAction
+            :to="{ name }"
+          >
+            {{ t(`data-planes.routes.item.navigation.${name}`) }}
+          </XAction>
         </template>
-        <template
-          #actions
-        >
-          <XDisclosure
-            v-slot="{ expanded, toggle }"
-          >
-            <XAction
-              appearance="primary"
-              @click="toggle"
-            >
-              Download bundle
-            </XAction>
-            <XTeleportTemplate
-              v-if="expanded"
-              :to="{ name: 'modal-layer' }"
-            >
-              <XDisclosure
-                v-slot="{ expanded: downloading, toggle: download }"
-              >
-                <form
-                  @submit.prevent="download"
-                >
-                  <XModal
-                    :title="t('data-planes.routes.item.download.title')"
-                    @cancel="toggle"
-                  >
-                    <fieldset
-                      :disabled="downloading"
-                    >
-                      <XI18n
-                        path="data-planes.routes.item.download.description"
-                      />
-                      <ul>
-                        <template
-                          v-for="(spec, key) in specs"
-                          :key="typeof spec"
-                        >
-                          <li
-                            v-if="key !== 'eds'"
-                          >
-                            <XCheckbox
-                              v-model="specs[key]"
-                              @change="(bool: boolean) => {
-                                if(key === 'xds' && !bool) {
-                                  specs.eds = false
-                                }
-                              }"
-                            >
-                              {{ t(`data-planes.routes.item.download.options.${key}`) }}
-                            </XCheckbox>
-                            <ul
-                              v-if="key === 'xds'"
-                            >
-                              <li>
-                                <XCheckbox
-                                  v-model="specs.eds"
-                                  :disabled="!specs.xds"
-                                >
-                                  {{ t('data-planes.routes.item.download.options.eds') }}
-                                </XCheckbox>
-                              </li>
-                            </ul>
-                          </li>
-                        </template>
-                      </ul>
-                    </fieldset>
-                    <template
-                      #footer-actions
-                    >
-                      <XLayout
-                        type="separated"
-                      >
-                        <XDownload
-                          @start="toggle"
-                          v-slot="{ download: bundle }"
-                        >
-                          <DataLoader
-                            variant="spinner"
-                            :src="downloading ? uri(sources, '/meshes/:mesh/dataplanes/:name/as/tarball/:spec', {
-                              mesh: route.params.mesh,
-                              name: route.params.proxy,
-                              spec: JSON.stringify(
-                                specs,
-                              ),
-                            }, {
-                              cacheControl: 'no-cache',
-                            }) : ''"
-                            @change="bundle"
-                            @error="download"
-                          >
-                            <template
-                              #error
-                            >
-                              <XAlert
-                                appearance="warning"
-                                show-icon
-                              >
-                                <XI18n
-                                  t="data-planes.routes.item.download.error"
-                                />
-                              </XAlert>
-                            </template>
-                          </DataLoader>
-                        </XDownload>
-                        <XAction
-                          appearance="primary"
-                          type="submit"
-                          :disabled="downloading || Object.values(specs).every(bool => !bool)"
-                        >
-                          {{ t('data-planes.routes.item.download.action') }}
-                        </XAction>
-                      </XLayout>
-                    </template>
-                  </XModal>
-                </form>
-              </XDisclosure>
-            </XTeleportTemplate>
-          </XDisclosure>
-        </template>
+      </XTabs>
 
-        <DataLoader
-          :data="[data]"
-          :errors="[error]"
-        >
-          <XTabs
-            :selected="route.child()?.name"
-          >
-            <template
-              v-for="{ name } in route.children"
-              :key="name"
-              #[`${name}-tab`]
-            >
-              <XAction
-                :to="{ name }"
-              >
-                {{ t(`data-planes.routes.item.navigation.${name}`) }}
-              </XAction>
-            </template>
-          </XTabs>
-
-          <RouterView
-            v-slot="{ Component }"
-          >
-            <component
-              :is="Component"
-              :data="data"
-              :networking="data?.dataplane.networking"
-              :mesh="props.mesh"
-            />
-          </RouterView>
-        </DataLoader>
-      </AppView>
-    </DataLoader>
+      <RouterView
+        v-slot="{ Component }"
+      >
+        <component
+          :is="Component"
+          :data="props.data"
+          :networking="props.data.dataplane.networking"
+          :mesh="props.mesh"
+        />
+      </RouterView>
+    </AppView>
   </RouteView>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 
+import type { DataplaneOverview } from '../data'
 import { sources } from '../sources'
 import type { Mesh } from '@/app/meshes/data'
 const props = defineProps<{
+  data: DataplaneOverview
   mesh: Mesh
 }>()
 
