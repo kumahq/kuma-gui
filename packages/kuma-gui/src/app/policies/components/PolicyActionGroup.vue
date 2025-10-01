88d3728767c1100627ea71971d949b674a84d49c
John Cowen <johncowen@users.noreply.github.com>
2025-10-01 12:21:32 +0100

    chore: adds PolicyActionGroup (#4290)
    A while back we decided to put all menu like "ActionGroups" into their
    own individual component per resource (or "resource collection" in the
    case of "policies")
    
    This PR adds a PolicyActionGroup for policies
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

---
+++ packages/kuma-gui/src/app/policies/components/PolicyActionGroup.vue
@@ -0,0 +1,28 @@
+<template>
+  <XActionGroup
+    v-bind="attrs"
+  >
+    <template
+      v-for="(_, slotName) in slots"
+      :key="slotName"
+      #[slotName]="slotProps"
+    >
+      <slot
+        :name="slotName"
+        v-bind="(slotProps)"
+      />
+    </template>
+  </XActionGroup>
+</template>
+<script lang="ts" generic="T extends { name: string } | undefined" setup>
+import { useAttrs } from 'vue'
+const slots = defineSlots()
+const attrs = useAttrs()
+const _props = withDefaults(defineProps<{
+  item?: T
+  type?: unknown
+}>(), {
+  item: undefined,
+  type: undefined,
+})
+</script>
