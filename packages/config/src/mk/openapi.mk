6cdedd67ab74fe5ce71ba6de3606e03cb426e11b
John Cowen <john.cowen@konghq.com>
2025-11-12 14:30:23 +0000

    work on make targets, add versioning and update workflow
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- packages/config/src/mk/openapi.mk
+++ packages/config/src/mk/openapi.mk
@@ -10,27 +10,21 @@ YAML_SRC_FILES := $(shell find ./src -name "*.yaml" -not -name "*.overlay.yaml" 
 YAML_OUTPUT_FILES := $(patsubst ./src/%,./dist/%,$(YAML_SRC_FILES))
 
 index.d.ts: openapi.yaml
+	@echo "Generating Types from $< to $@..."
 	@$(OPENAPI_TS) $< > $@
 
-openapi.yaml: ./dist overlays
+openapi.yaml: OPENAPI_SHA?=unknown
+openapi.yaml: openapi.overlay.tmpl.yaml generated/openapi.yaml $(OVERLAY_OUTPUT_FILES) $(YAML_OUTPUT_FILES) dist/paths/.yaml
+	@echo "Overlaying final $(basename $@).overlay$(suffix $@) to $@..."
+	@cat $(basename $@).overlay.tmpl$(suffix $@) | sed "s/\$${GIT_SHA}/$(OPENAPI_SHA)/g" > $(basename $@).overlay$(suffix $@)
 	@$(OPENAPI_FORMAT) \
 		--keepComments \
 		--overlayFile $(basename $@).overlay$(suffix $@) \
 		--output $@
+	@rm $(basename $@).overlay$(suffix $@)
 
-./dist/%.yaml: ./src/%.yaml
-	@cp $< $@
-
-./dist: ./generated/openapi.yaml
-	@cp -R $(dir $<) $@
-	@$(MAKE) -f $(OPENAPI_MAKEFILE) $(YAML_OUTPUT_FILES)
-
-.PHONY: overlays
-overlays: ./generated/openapi.yaml
-	@rm -f $(OVERLAY_OUTPUT_FILES)
-	@$(MAKE) -f $(OPENAPI_MAKEFILE) $(OVERLAY_OUTPUT_FILES)
-
-%.yaml: %.overlay.yaml
+$(OVERLAY_OUTPUT_FILES): dist/%.yaml: src/%.overlay.yaml
+	@echo "Overlaying $< to $@..."
 	@mkdir -p $(dir $@)
 	@$(OPENAPI_FORMAT) \
 		--keepComments \
@@ -38,13 +32,28 @@ overlays: ./generated/openapi.yaml
 		--overlayFile $< \
 		--output $@
 
-./generated/openapi.yaml: $(OPENAPI_SRC)
+$(YAML_OUTPUT_FILES): dist/%.yaml: src/%.yaml
+	@echo "Copying $< to $@..."
+	@mkdir -p $(dir $@)
+	@cp $< $@
+
+## dist will already exist from previous steps so we use paths/.yaml (which is `/` root)
+## to signal that dist needs copying and we then skip anything that exists
+dist/paths/.yaml: generated/openapi.yaml
+	@echo "Copying generated to dist..."
+	@cp -nR generated/ dist/ || true
+
+generated/openapi.yaml: $(OPENAPI_SRC)
+	@echo "Splitting $<..."
 	@mkdir -p $(dir $@)
 	@$(OPENAPI_FORMAT) \
 		--keepComments \
 		--split $< \
 		--output $@
+	@echo "Removing dist to be refreshed..."
+	@rm -rf ./dist
 
 
-
-
+.PHONY: version
+version:
+	@cat openapi.yaml | grep x-oas-source | cut -d'@' -f2
