795dd02c7a2911b5a401db4e642cb823a12fde91
John Cowen <john.cowen@konghq.com>
2025-12-15 11:51:40 +0000

    chore(deps): remove vitepress
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- packages/config/src/mk/run.mk
+++ packages/config/src/mk/run.mk
@@ -5,12 +5,6 @@
 		--configLoader runner \
 		-c ./vite.config.production.ts
 
-.PHONY: .run/docs
-.run/docs: VITEPRESS ?= $(shell $(MAKE) resolve/bin BIN=vitepress)
-.run/docs: install
-	@$(VITEPRESS) \
-		dev
-
 .PHONY: .run/e2e
 .run/e2e: VITE ?= $(shell $(MAKE) resolve/bin BIN=vite)
 .run/e2e:
