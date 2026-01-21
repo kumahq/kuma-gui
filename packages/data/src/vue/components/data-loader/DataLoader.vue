07c8785cdf9944a8ce93ce67347c211933a627ae
schogges <moritz.fleck@konghq.com>
2026-01-21 15:14:14 +0100

    wip
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/data/src/vue/components/data-loader/DataLoader.vue
+++ packages/data/src/vue/components/data-loader/DataLoader.vue
@@ -10,7 +10,7 @@
     >
       <slot
         name="error"
-        :data="srcData as TypeOf<T>"
+        :data="d as D"
         :error="allErrors[0]"
         :refresh="props.src !== '' ? refresh : () => {}"
       >
@@ -25,7 +25,7 @@
     >
       <slot
         name="default"
-        :data="srcData as NonNullable<TypeOf<T>>"
+        :data="d as D"
         :error="srcError"
         :refresh="props.src !== '' ? refresh : () => {}"
       />
@@ -40,7 +40,7 @@
         :refresh="props.src !== '' ? refresh : () => {}"
       >
         <XProgress
-          v-if="props.data.length > 0 || props.src !== ''"
+          v-if="(props.data ?? []).length > 0 || props.src !== ''"
           v-bind="$attrs"
           :variant="props.variant === 'default' ? 'legacy' : props.variant"
         />
@@ -48,7 +48,7 @@
       <slot
         v-else
         name="default"
-        :data="srcData as NonNullable<TypeOf<T>>"
+        :data="d as D"
         :error="srcError"
         :refresh="props.src !== '' ? refresh : () => {}"
       />
@@ -71,18 +71,36 @@ const props = withDefaults(defineProps<{
   loader?: boolean
   variant?: 'default' | 'list' | 'spinner'
 }>(), {
-  errors: () => [],
-  data: () => [],
-  src: '' as any,
+  errors: undefined,
+  data: undefined,
+  src: undefined,
   loader: true,
   variant: 'default',
 })
 
 type Data = NonNullable<TypeOf<T>>
+type D = 
+  T extends undefined
+    ? K[] extends undefined
+      ? undefined                         // neither defined
+      : K[]                               // only data defined
+    : K[] extends undefined  
+      ? NonNullable<TypeOf<T>>            // only src defined
+      : [NonNullable<TypeOf<T>>, ...K[]]  // both src and data defined
+type SrcType = typeof props.src
+type DataType = typeof props.data
+type DD = 
+  SrcType extends undefined
+    ? DataType extends undefined
+      ? undefined                         // neither defined
+      : DataType                               // only data defined
+    : DataType extends undefined  
+      ? NonNullable<TypeOf<T>>            // only src defined
+      : [NonNullable<TypeOf<T>>, ...K[]]  // both src and data defined
 
 defineSlots<{
   default(props: {
-    data: Data
+    data: D
     error: Error | undefined
     refresh: () => void
   }): any
@@ -92,12 +110,12 @@ defineSlots<{
     refresh: () => void
   }): any
   error(props: {
-    data: Data
+    data: D
     error: Error | undefined
     refresh: () => void
   }): any
   disconnected(props: {
-    data: Data
+    data: D
     error: Error | undefined
     refresh: () => void
   }): any
@@ -112,17 +130,35 @@ const srcData = ref<unknown>(undefined)
 const srcError = ref<Error | undefined>(undefined)
 
 const allData = computed(() => {
-  const data = props.data.filter(item => !(item instanceof Error))
+  const data = props.data?.filter(item => !(item instanceof Error)) ?? []
+  /*
   if (props.src !== '') {
     return [srcData.value as unknown].concat(data)
   }
+  */
   return data
 })
 
 const allErrors = computed(() => {
-  const dataErrors = props.data.filter(item => item instanceof Error)
+  const dataErrors = props.data?.filter(item => item instanceof Error) ?? []
   const errors = typeof srcError.value === 'undefined' ? props.errors : ([srcError.value] as (Error | undefined)[]).concat(props.errors)
-  return errors.concat(dataErrors).filter(<T>(item: T): item is NonNullable<T> => Boolean(item))
+  return (errors ?? []).concat(dataErrors).filter(<T>(item: T): item is NonNullable<T> => Boolean(item))
 })
+
+const d = computed(() => {
+  const result = props.src && props.data?.length ? [srcData.value as NonNullable<TypeOf<T>>, ...allData.value]
+  : props.src
+    ? srcData.value as NonNullable<TypeOf<T>>
+    : allData.value
+
+  console.log({result, 'props.data': props.data, 'props.src': props.src})
+  return result
+})
+/*
+const d: D = props.src && props.data ? [srcData.value as NonNullable<TypeOf<T>>, ...allData.value] as D
+  : props.src
+    ? srcData.value as NonNullable<TypeOf<T>>
+    : allData.value as D
+*/
 
 </script>
