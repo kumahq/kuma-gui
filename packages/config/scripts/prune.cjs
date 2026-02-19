a1b7326318cf989e2a4019e1e4e9f62c2b873166
John Cowen <john.cowen@konghq.com>
2026-02-19 16:10:10 +0000

    chore: add simple filepath sanitation
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- packages/config/scripts/prune.cjs
+++ packages/config/scripts/prune.cjs
@@ -1,6 +1,9 @@
 const { readFileSync: read } = require('fs')
 module.exports = {
   depsToDevDeps: (path) => {
+    if (path && path.includes('..')) {
+      throw new Error('Invalid file path, contains ..')
+    }
     const pkg = JSON.parse(read(path, 'utf-8'))
     return JSON.stringify({
       ...pkg,
