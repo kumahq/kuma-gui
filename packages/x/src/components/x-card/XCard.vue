ae10a52b58f9990d129280a5f23789f0d7eb40d6
John Cowen <john.cowen@konghq.com>
2026-02-19 13:41:18 +0000

    remove unnecessary SASS var
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- packages/x/src/components/x-card/XCard.vue
+++ packages/x/src/components/x-card/XCard.vue
@@ -24,13 +24,12 @@ const xAction = inject<{} | undefined>('x-action', undefined)
 </script>
 <style lang="scss" scoped>
 /* copied from kong/kongponents KRadio::card=true */
-$kongponentsTransitionDurTimingFunc: var(--x-animation-duration-20) ease-in-out;
 .in-x-action {
   & {
     cursor: pointer;
     border: 0;
     box-shadow: var(--x-shadow-border);
-    transition: box-shadow $kongponentsTransitionDurTimingFunc, background-color $kongponentsTransitionDurTimingFunc;
+    transition: box-shadow var(--x-animation-duration-20) ease-in-out, background-color var(--x-animation-duration-20) ease-in-out;
   }
   &:hover {
     box-shadow: var(--x-shadow-border-primary-weak);
