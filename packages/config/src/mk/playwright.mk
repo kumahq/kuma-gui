acd661ce7b66d311fed347369860f7a12fa4f1ba
John Cowen <johncowen@users.noreply.github.com>
2026-03-20 14:06:19 +0000

    feat(ci): add initial playwright support (#4724)
    Adds initial playwright browser runner support for our gherkin e2e tests
    
    This is not yet wired into CI, but I've added a `make playwright` which
    you can use in kuma-gui to test with (and also play with extra flags
    like `--headed` and `--ui`).
    
    I would like to merge it in this state, and then get it wired into CI as
    a follow up.
    
    ---
    
    This PR is kind of a "middle stage" with an eye to replacing Cypress in
    CI as soon as possible. There are some semi-obvious TODOs in here that
    will be done at a later stage. I'll make some inline notes here and we
    can also chat in sync. Its mostly just a reimplementation of our steps
    using Playwright APIs.
    
    ---------
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

---
+++ packages/config/src/mk/playwright.mk
@@ -0,0 +1,14 @@
+PLAYWRIGHT_MAKEFILE := $(lastword $(MAKEFILE_LIST))
+PLAYWRIGHT_MAKEFILE_DIR := $(dir $(abspath $(PLAYWRIGHT_MAKEFILE)))
+PLAYWRIGHT ?= $(shell $(MAKE) resolve/bin PACKAGE=@playwright/test BIN=playwright)
+BDDGEN ?= $(shell $(MAKE) resolve/bin PACKAGE=playwright-bdd BIN=bddgen)
+
+.PHONY: build
+build:
+	@$(BDDGEN)
+
+.PHONY: test
+test: build
+	@$(PLAYWRIGHT) test
+
+
--- packages/gherkin-web/src/cypress/steps/index.ts
+++ packages/gherkin-web/src/cypress/steps/index.ts
@@ -1,9 +1,6 @@
 import { When, Then, Before, Given, DataTable, After } from '@badeball/cypress-cucumber-preprocessor'
-import deepmerge from 'deepmerge'
-import jsYaml, { DEFAULT_SCHEMA, Type } from 'js-yaml'
 
-import { getClient } from '../../client'
-import type { ArrayMergeOptions } from 'deepmerge'
+import { getClient, YAML, merge } from '../../'
 
 type Middleware = (req: { url: URL, method: string, body: any }, res: any) => typeof res
 type BaseMock = (route: string, opts?: Record<string, string>, cb?: Middleware) => ReturnType<typeof cy['intercept']>
@@ -16,49 +13,8 @@ type Options<TMock extends BaseMock> = {
 }
 
 export async function setupSteps<TMock extends BaseMock>({ mock, client = getClient() }: Options<TMock>) {
-  // merges objects in array positions rather than replacing
-  const undefinedSymbol = Symbol('undefined')
-  const combineMerge = (target: object[], source: object[], options: ArrayMergeOptions): object[] => {
-    const destination = target.slice()
-
-    source.forEach((item, index) => {
-      if (typeof destination[index] === 'undefined') {
-        destination[index] = options.cloneUnlessOtherwiseSpecified(item, options)
-      } else if (options.isMergeableObject(item)) {
-        destination[index] = deepmerge(target[index], item, options)
-      } else if (target.indexOf(item) === -1) {
-        destination.push(item)
-      }
-    })
-    return destination
-  }
-
-  const merge = <T>(response: T, obj: Partial<T>): T => {
-    const merged = deepmerge<T>(response, obj, { arrayMerge: combineMerge })
-    return JSON.parse(JSON.stringify(merged, (_key, value) => {
-      if (value === undefinedSymbol) {
-        return
-      }
-      return value
-    }))
-  }
-
   const console = {
     log: (message: unknown) => Cypress.log({ displayName: 'LOG', message: JSON.stringify(message) }),
-  }
-  const YAML = {
-    parse: (str: string) => {
-      return jsYaml.load(str, {
-        schema: DEFAULT_SCHEMA.extend([
-          new Type('tag:yaml.org,2002:js/undefined', {
-            kind: 'scalar',
-            construct: () => {
-              return undefinedSymbol
-            },
-          }),
-        ]),
-      })
-    },
   }
 
   let env = {}
