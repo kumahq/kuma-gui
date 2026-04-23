6ece6fa22063d4d764aa7b826b0147433ee793d5
schogges <moritz.fleck@konghq.com>
2026-04-23 19:05:46 +0200

    add tests for KRI usage
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/kuma-gui/src/app/services/views/MeshMultiZoneServiceListView.vue
+++ packages/kuma-gui/src/app/services/views/MeshMultiZoneServiceListView.vue
@@ -70,7 +70,7 @@
                         name: 'mesh-multi-zone-service-summary-view',
                         params: {
                           mesh: item.mesh,
-                          service: item.kri,
+                          service: item.id,
                         },
                         query: {
                           page: route.params.page,
@@ -123,7 +123,7 @@
                         name: 'mesh-multi-zone-service-detail-view',
                         params: {
                           mesh: item.mesh,
-                          service: item.kri,
+                          service: item.id,
                         },
                       }"
                     >
