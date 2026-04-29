5c6c40aa6cb7fbe986404fedced8d790f0522aa0
schogges <moritz.fleck@konghq.com>
2026-04-29 10:09:13 +0200

    revert pre-work in summary view
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/kuma-gui/src/app/hostname-generators/views/HostnameGeneratorSummaryView.vue
+++ packages/kuma-gui/src/app/hostname-generators/views/HostnameGeneratorSummaryView.vue
@@ -10,121 +10,136 @@
     }"
     v-slot="{ route, t, can, uri }"
   >
-    <DataLoader
-      :src="typeof props.items === 'undefined' ? uri(sources, '/hostname-generators/:name', {
-        name: route.params.name,
-      }) : undefined"
-      v-slot="{ data: [hostnameGenerator] }"
+    <DataCollection
+      :items="props.items"
+      :predicate="item => item.id === route.params.name || item.kri === route.params.name"
     >
-      <DataCollection
-        :items="props.items ?? [hostnameGenerator]"
-        :predicate="item => item.id === route.params.name || item.kri === route.params.name"
+      <template
+        #item="{ item }"
       >
-        <template
-          #item="{ item }"
-        >
-          <AppView>
-            <template #title>
-              <h2>
-                <XAction
-                  :to="{
-                    name: 'hostname-generator-detail-view',
-                    params: {
-                      name: route.params.name,
-                    },
+        <AppView>
+          <template #title>
+            <h2>
+              <XAction
+                :to="{
+                  name: 'hostname-generator-detail-view',
+                  params: {
+                    name: route.params.name,
+                  },
 
-                  }"
+                }"
+              >
+                <RouteTitle
+                  :title="t('hostname-generators.routes.item.title', { name: item.name })"
+                />
+              </XAction>
+            </h2>
+          </template>
+
+          <XLayout
+            variant="y-stack"
+          >
+            <header>
+              <XLayout
+                variant="separated"
+                size="max"
+              >
+                <h3>
+                  {{ t('hostname-generators.routes.item.config') }}
+                </h3>
+                <div
+                  v-for="options in [['structured', 'universal', 'k8s']]"
+                  :key="typeof options"
                 >
-                  <RouteTitle
-                    :title="t('hostname-generators.routes.item.title', { name: item.name })"
-                  />
-                </XAction>
-              </h2>
+                  <XSelect
+                    :label="t('hostname-generators.routes.item.format')"
+                    :selected="route.params.format"
+                    @change="(value) => {
+                      route.update({ format: value })
+                    }"
+                    @vue:before-mount="$event?.props?.selected && options.includes($event.props.selected) && $event.props.selected !== route.params.format && route.update({ format: $event.props.selected })"
+                  >
+                    <template
+                      v-for="value in options"
+                      :key="value"
+                      #[`${value}-option`]
+                    >
+                      {{ t(`hostname-generators.routes.item.formats.${value}`) }}
+                    </template>
+                  </XSelect>
+                </div>
+              </XLayout>
+            </header>
+
+            <template v-if="route.params.format === 'structured'">
+              <XTable
+                variant="kv"
+                data-testid="structured-view"
+              >
+                <tr
+                  v-if="item.namespace.length > 0"
+                >
+                  <th scope="row">
+                    {{ t('hostname-generators.common.namespace') }}
+                  </th>
+                  <td>{{ item.namespace }}</td>
+                </tr>
+                <tr
+                  v-if="can('use zones') && item.zone"
+                >
+                  <th scope="row">
+                    {{ t('hostname-generators.common.zone') }}
+                  </th>
+                  <td>
+                    <XAction
+                      :to="{
+                        name: 'zone-cp-detail-view',
+                        params: {
+                          zone: item.zone,
+                        },
+                      }"
+                    >
+                      {{ item.zone }}
+                    </XAction>
+                  </td>
+                </tr>
+                <tr
+                  v-if="item.spec.template"
+                >
+                  <th scope="row">
+                    {{ t('hostname-generators.common.template') }}
+                  </th>
+                  <td>{{ item.spec.template }}</td>
+                </tr>
+              </XTable>
             </template>
 
-            <XLayout
-              variant="y-stack"
-            >
-              <header>
-                <XLayout
-                  variant="separated"
-                  size="max"
-                >
-                  <h3>
-                    {{ t('hostname-generators.routes.item.config') }}
-                  </h3>
-                  <div
-                    v-for="options in [['structured', 'universal', 'k8s']]"
-                    :key="typeof options"
-                  >
-                    <XSelect
-                      :label="t('hostname-generators.routes.item.format')"
-                      :selected="route.params.format"
-                      @change="(value) => {
-                        route.update({ format: value })
-                      }"
-                      @vue:before-mount="$event?.props?.selected && options.includes($event.props.selected) && $event.props.selected !== route.params.format && route.update({ format: $event.props.selected })"
-                    >
-                      <template
-                        v-for="value in options"
-                        :key="value"
-                        #[`${value}-option`]
-                      >
-                        {{ t(`hostname-generators.routes.item.formats.${value}`) }}
-                      </template>
-                    </XSelect>
-                  </div>
-                </XLayout>
-              </header>
+            <template v-else-if="route.params.format === 'universal'">
+              <XCodeBlock
+                data-testid="codeblock-yaml-universal"
+                language="yaml"
+                :code="YAML.stringify(item.$raw)"
+                is-searchable
+                :query="route.params.codeSearch"
+                :is-filter-mode="route.params.codeFilter"
+                :is-reg-exp-mode="route.params.codeRegExp"
+                @query-change="route.update({ codeSearch: $event })"
+                @filter-mode-change="route.update({ codeFilter: $event })"
+                @reg-exp-mode-change="route.update({ codeRegExp: $event })"
+              />
+            </template>
 
-              <template v-if="route.params.format === 'structured'">
-                <XTable
-                  variant="kv"
-                  data-testid="structured-view"
-                >
-                  <tr
-                    v-if="item.namespace.length > 0"
-                  >
-                    <th scope="row">
-                      {{ t('hostname-generators.common.namespace') }}
-                    </th>
-                    <td>{{ item.namespace }}</td>
-                  </tr>
-                  <tr
-                    v-if="can('use zones') && item.zone"
-                  >
-                    <th scope="row">
-                      {{ t('hostname-generators.common.zone') }}
-                    </th>
-                    <td>
-                      <XAction
-                        :to="{
-                          name: 'zone-cp-detail-view',
-                          params: {
-                            zone: item.zone,
-                          },
-                        }"
-                      >
-                        {{ item.zone }}
-                      </XAction>
-                    </td>
-                  </tr>
-                  <tr
-                    v-if="item.spec.template"
-                  >
-                    <th scope="row">
-                      {{ t('hostname-generators.common.template') }}
-                    </th>
-                    <td>{{ item.spec.template }}</td>
-                  </tr>
-                </XTable>
-              </template>
-
-              <template v-else-if="route.params.format === 'universal'">
+            <template v-else>
+              <DataLoader
+                :src="uri(sources, '/hostname-generators/:name/as/kubernetes', {
+                  name: route.params.name,
+                })"
+                v-slot="{ data: [k8sConfig] }"
+              >
                 <XCodeBlock
-                  data-testid="codeblock-yaml-universal"
+                  data-testid="codeblock-yaml-k8s"
                   language="yaml"
-                  :code="YAML.stringify(item.$raw)"
+                  :code="YAML.stringify(k8sConfig)"
                   is-searchable
                   :query="route.params.codeSearch"
                   :is-filter-mode="route.params.codeFilter"
@@ -133,34 +148,12 @@
                   @filter-mode-change="route.update({ codeFilter: $event })"
                   @reg-exp-mode-change="route.update({ codeRegExp: $event })"
                 />
-              </template>
-
-              <template v-else>
-                <DataLoader
-                  :src="uri(sources, '/hostname-generators/:name/as/kubernetes', {
-                    name: route.params.name,
-                  })"
-                  v-slot="{ data: [k8sConfig] }"
-                >
-                  <XCodeBlock
-                    data-testid="codeblock-yaml-k8s"
-                    language="yaml"
-                    :code="YAML.stringify(k8sConfig)"
-                    is-searchable
-                    :query="route.params.codeSearch"
-                    :is-filter-mode="route.params.codeFilter"
-                    :is-reg-exp-mode="route.params.codeRegExp"
-                    @query-change="route.update({ codeSearch: $event })"
-                    @filter-mode-change="route.update({ codeFilter: $event })"
-                    @reg-exp-mode-change="route.update({ codeRegExp: $event })"
-                  />
-                </DataLoader>
-              </template>
-            </XLayout>
-          </AppView>
-        </template>
-      </DataCollection>
-    </DataLoader>
+              </DataLoader>
+            </template>
+          </XLayout>
+        </AppView>
+      </template>
+    </DataCollection>
   </RouteView>
 </template>
 
@@ -169,6 +162,6 @@ import { sources } from '../sources'
 import { YAML } from '@/app/application'
 import type { HostnameGenerator } from '@/app/hostname-generators/data'
 const props = defineProps<{
-  items: HostnameGenerator[] | undefined
+  items: HostnameGenerator[]
 }>()
 </script>
