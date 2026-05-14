b15a60e5627277a26c301c34a3704c2521338f23
John Cowen <john.cowen@konghq.com>
2026-05-14 16:58:42 +0100

    Revert "temporary commit"
    This reverts commit a3caaa84f0b22eb44c285c1c99dc22b48ae39ffe.
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- packages/kuma-gui/src/app/policies/views/PolicyTypeListView.vue
+++ packages/kuma-gui/src/app/policies/views/PolicyTypeListView.vue
@@ -71,6 +71,7 @@
                             :data-testid="`policy-type-link-${policyType.name}`"
                             @vue:mounted="async (vNode) => {
                               if(route.params.policyPath.length === 0 && i === 0 && vNode.props?.to) {
+                                await nextTick()
                                 route.replace(vNode.props.to)
                               }
                             }"
@@ -109,6 +110,7 @@
   </RouteView>
 </template>
 <script lang="ts" setup>
+import { nextTick } from 'vue'
 
 import { sources as meshSources } from '@/app/meshes/sources'
 import { sources } from '@/app/policies/sources'
