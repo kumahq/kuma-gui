ae8fecebfcfd59560e2ee8b6c3c4097bd4afb55e
John Cowen <john.cowen@konghq.com>
2026-05-27 12:10:05 +0100

    chore(mesh-identity): data cleanup for MeshIdentity and MeshTrust
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- packages/kuma-gui/src/app/mesh-trusts/data/MeshTrust.ts
+++ packages/kuma-gui/src/app/mesh-trusts/data/MeshTrust.ts
@@ -6,9 +6,29 @@ export type KumaMeshTrust = components['schemas']['MeshTrustItem']
 
 export const MeshTrust = {
   fromObject: (item: KumaMeshTrust) => {
+    const labels = item.labels ?? {}
+    const id = item.name
+    const mesh = item.mesh
+    const zone = labels['kuma.io/origin'] === 'zone' && labels['kuma.io/zone'] ? labels['kuma.io/zone'] : ''
+    const namespace = labels['k8s.kuma.io/namespace'] ?? ''
+    const name = labels['kuma.io/display-name'] ?? item.name
+
     return {
-      kri: Kri.toString({ shortName: 'mtrust', mesh: item.mesh, name: item.name }),
+      // defaults
+      kri: Kri.toString({ shortName: 'mtrust', mesh, zone, namespace, name }),
+      creationTime: '',
+      modificationTime: '',
+      labels,
+      // overwrite
       ...item,
+      name,
+      mesh,
+      // aliases
+      id,
+      namespace,
+      zone,
+      raw: item,
+      //
       status: {
         ...item.status,
         origin: {
@@ -16,14 +36,15 @@ export const MeshTrust = {
           kri: item.status?.origin?.kri ?? '',
         },
       },
-      raw: item,
     }
   },
 
   fromCollection: (collection: KumaMeshTrustList) => {
+    const items = Array.isArray(collection.items) ? collection.items.map(MeshTrust.fromObject) : []
     return {
       ...collection,
-      items: collection.items?.map((item) => MeshTrust.fromObject(item)) ?? [],
+      items,
+      total: collection.total ?? items.length,
     }
   },
 }
