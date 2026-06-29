e9095fa4a12cc0d70f9a677d4bed7a30348ed097
John Cowen <john.cowen@konghq.com>
2026-06-29 10:49:01 +0100

    chore(kumahq/container): allows us to use multiple containers at once
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- packages/kuma-gui/src/app/application/debug.ts
+++ packages/kuma-gui/src/app/application/debug.ts
@@ -1,4 +1,4 @@
-import { token, get } from '@kumahq/container'
+import { token } from '@kumahq/container'
 import { Cookie } from '@kumahq/fake-api'
 
 import debugI18n from './services/i18n/DebugI18n'
@@ -6,26 +6,32 @@ import type { Env } from '@/app/application'
 import type { ServiceDefinition, Token } from '@kumahq/container'
 
 type I18n = ReturnType<typeof debugI18n>
-
 export const services = (app: Record<string, Token>, prefix = 'KUMA'): ServiceDefinition[] => [
   [token('i18n.debug'), {
-    service: (i18n: () => I18n) => {
-      const env = get(app.env) as Env
-      // @ts-ignore PREFIX_I18N_DEBUG_ENABLED is debug only
-      if (env(`${prefix}_I18N_DEBUG_ENABLED`).length > 0) {
-        return debugI18n(i18n())
+    service: (useI18n: () => I18n) => {
+      const i18n = useI18n()
+      return {
+        ...i18n,
+        t: (...args: Parameters<typeof i18n['t']>) => {
+          const cookies = Cookie.parse(document.cookie ?? '', { prefix: `${prefix}_` })
+          const env = (key: string) => {
+            return cookies[key] ?? ''
+          }
+          if (env(`${prefix}_I18N_DEBUG_ENABLED`).length > 0) {
+            return args[0]
+          }
+          return i18n.t(...args)
+        } ,
       }
-      return i18n()
     },
     decorates: app.i18n,
   }],
   [token('env.debug'), {
-    service: (getEnv: () => Env) => {
-
-      const env = getEnv()
-      return (...[ key, ...rest ]: Parameters<Env>) => {
+    service: (useEnv: () => Env) => {
+      const env = useEnv()
+      return (...[key, ...rest]: Parameters<Env>) => {
         // make sure you can't read/replace anything but PREFIX_*
-        return Cookie.parse(document.cookie ?? '', { prefix: `${prefix}_`})[key] ?? env(key, ...rest)
+        return Cookie.parse(document.cookie ?? '', { prefix: `${prefix}_` })[key] ?? env(key, ...rest)
       }
     },
     decorates: app.env,
