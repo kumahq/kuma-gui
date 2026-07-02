a8d933b81b3fb125aef8325eccc78aa661203c04
schogges <moritz.fleck@konghq.com>
2026-07-02 11:47:45 +0200

    accidentally removed conditional fetch
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/kuma-gui/src/app/legacy-data-planes/views/DataPlaneDetailView.vue
+++ packages/kuma-gui/src/app/legacy-data-planes/views/DataPlaneDetailView.vue
@@ -20,10 +20,10 @@
       v-slot="{ data: traffic, error, refresh }"
     >
       <DataSource
-        :src="uri(sources, '/meshes/:mesh/dataplanes/:name/layout', {
+        :src="props.mesh.meshServices.mode === 'Exclusive' ? uri(sources, '/meshes/:mesh/dataplanes/:name/layout', {
           mesh: route.params.mesh,
           name: route.params.proxy,
-        })"
+        }) : ''"
         v-slot="{ data: sourceDataplaneLayout }: DataSourceResponse<DataplaneNetworkingLayout>"
       >
         <AppView
