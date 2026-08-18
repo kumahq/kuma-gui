02dd3562cd8f3fe534795707abeec26ac4d48a8c
John Cowen <johncowen@users.noreply.github.com>
2026-08-18 16:12:14 +0100

    chore(search): move search from resources to kuma (#5226)
    TL;DR: move our generic/abstract `search` out of `resources` (now a
    non-abstract module) to `kuma`
    
    ---
    
    Originally our `resources` module (mainly its data layer) was like an
    abstract class for any resource type to inherit from. Therefore we had a
    generic resource `search` function attached to it: `Resource.search`.
    
    Since then we've added a `Resource` resource, so the `resources` module
    being an "abstract" class no longer made sense.
    
    Therefore we decided we would move the `search` function off of
    `Resource` to somewhere else.
    
    That's what this PR does, although for the moment it keeps a passthrough
    from `Resource.search` to `search`, apart from for the new
    `legacy-services` module (which is mainly for illustration purposes of
    what is to come in a follow up PR).
    
    I chose to move the `search` code into `kuma` seeing that is now our "we
    use this everywhere" bucket/module.
    
    Depending on my workload, I plan on migrating all other modules away
    from using `Resources.search` so that they just use `search` instead,
    imported from `kuma`.
    
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- packages/kuma-gui/src/app/kuma/utils/index.spec.ts
+++ packages/kuma-gui/src/app/kuma/utils/index.spec.ts
@@ -1,6 +1,160 @@
 import { describe, expect, test } from 'vitest'
 
-import { semver } from './'
+import { semver, search } from './'
+
+describe('search', () => {
+  test.each([
+    // Name
+    [
+      'foo',
+      {
+        name: 'foo',
+      },
+    ],
+    [
+      'name',
+      {
+        name: 'name',
+      },
+    ],
+    [
+      'name:foo',
+      {
+        name: 'foo',
+      },
+    ],
+
+    // Shortcuts
+    [
+      'namespace:foo',
+      {
+        ['filter[labels.k8s.kuma.io/namespace]']: 'foo',
+      },
+    ],
+    [
+      'zone:foo',
+      {
+        ['filter[labels.kuma.io/zone]']: 'foo',
+      },
+    ],
+    [
+      'service:foo',
+      {
+        ['filter[labels.kuma.io/service]']: 'foo',
+      },
+    ],
+    [
+      'namespace:foo service:bar zone:baz',
+      {
+        ['filter[labels.k8s.kuma.io/namespace]']: 'foo',
+        ['filter[labels.kuma.io/service]']: 'bar',
+        ['filter[labels.kuma.io/zone]']: 'baz',
+      },
+    ],
+    [
+      'tag:version',
+      {
+        tag: ['version'],
+      },
+    ],
+    [
+      'tag:namespace tag:zone tag:kuma.io/service',
+      {
+        tag: ['k8s.kuma.io/namespace', 'kuma.io/zone', 'kuma.io/service'],
+      },
+    ],
+    [
+      'tag:namespace:foo tag:zone:bar tag:kuma.io/service:baz',
+      {
+        tag: ['k8s.kuma.io/namespace:foo', 'kuma.io/zone:bar', 'kuma.io/service:baz'],
+      },
+    ],
+    [
+      'label:namespace:foo label:zone:bar label:kuma.io/service:baz',
+      {
+        ['filter[labels.k8s.kuma.io/namespace]']: 'foo',
+        ['filter[labels.kuma.io/zone]']: 'bar',
+        ['filter[labels.kuma.io/service]']: 'baz',
+      },
+    ],
+    [
+      'label:namespace:foo tag:zone:bar',
+      {
+        ['filter[labels.k8s.kuma.io/namespace]']: 'foo',
+        tag: ['kuma.io/zone:bar'],
+      },
+    ],
+
+    // Labels
+    [
+      'foo:bar',
+      {
+        ['filter[labels.foo]']: 'bar',
+      },
+    ],
+    [
+      'label:foo:bar',
+      {
+        ['filter[labels.foo]']: 'bar',
+      },
+    ],
+    [
+      'label: foo:bar',
+      {
+        ['filter[labels.foo]']: 'bar',
+      },
+    ],
+    [
+      'label:foo:bar label:baz=qux label: quux:quuux label: kuma.io/service=foo',
+      {
+        ['filter[labels.foo]']: 'bar',
+        ['filter[labels.baz]']: 'qux',
+        ['filter[labels.quux]']: 'quuux',
+        ['filter[labels.kuma.io/service]']: 'foo',
+      },
+    ],
+
+    // Invalids
+    [
+      ':',
+      {},
+    ],
+    [
+      'foo:',
+      {},
+    ],
+    [
+      ':foo',
+      {},
+    ],
+  ])('searching by %s works', (str, expected) => {
+    const actual = search(str)
+    expect(actual).toStrictEqual(expected)
+  })
+
+  // defaultKey
+  test.each([
+    [
+      'bar',
+      'foo',
+      {
+        foo: 'bar',
+      },
+    ],
+    [
+      'foo',
+      'bar',
+      {
+        bar: 'foo',
+      },
+    ],
+  ])('using %s as the defaultKey works', (str, defaultKey, expected) => {
+    const actual = search(str, { defaultKey })
+    expect(actual).toStrictEqual(expected)
+  })
+
+})
+
 describe('utilities', () => {
   test('semver', () => {
     expect(semver('1.1.1').patch).toBe('1.1.1')
--- packages/kuma-gui/src/app/resources/data/Resource.ts
+++ packages/kuma-gui/src/app/resources/data/Resource.ts
@@ -1,4 +1,4 @@
-import { Kri } from '@/app/kuma'
+import { Kri, search } from '@/app/kuma'
 import type { paths } from '@kumahq/kuma-http-api'
 
 /**
@@ -8,110 +8,17 @@ import type { paths } from '@kumahq/kuma-http-api'
 type KumaResourceCollection = Record<string, unknown>
 type KumaResource = paths['/_kri/{kri}']['get']['responses']['200']['content']['application/json']
 
-
-/**
- * Filters should follow the rules of [kong-aip#160](https://kong-aip.netlify.app/aip/160/).
- *
- * `filters` are the current defaults, but may not necessarily be used depending on the actual requirements of an API.
- */
-const shortFilters: Record<string, string> = {
-  namespace: 'k8s.kuma.io/namespace',
-  zone: 'kuma.io/zone',
-  service: 'kuma.io/service',
-  protocol: 'kuma.io/protocol',
-}
-
-type SearchOptions = {
-  defaultKey?: string
-}
-
-export const searchRegex = /(\S+:\s*\S*)|(\S+)/
-const kvSeparatorRegex = /:(.*)/
-
 export const Resource = {
-  parseSearch(query: string, options: SearchOptions = {}) {
-    const { defaultKey = 'name' } = options
-    const parts = query.trim().split(searchRegex).map((part) => part?.trim().replace(/=/, ':')).filter(Boolean)
-
-    return parts.reduce((acc, curr) => {
-      // if the part begins or ends with `:` it must be invalid
-      // `term:`, `:term`, `:`, `::::`
-      if(curr.startsWith(':') || curr.endsWith(':')) {
-        return acc
-      }
-
-      // set some defaults: default key to defaultKey and default value to ''
-      const [k, v = ''] = curr.split(kvSeparatorRegex).map((item) => item.trim())
-      const [key, value] = v.length === 0 ? [defaultKey, k] : [k, v]
-
-      // default accumulator tags/labels to {} incase we haven't already set those
-      const { tags = {}, labels = {} } = acc
-
-      switch(true) {
-        // Use defaultKey  as the key for single words, i.e. "foo" or "bar"
-        // i.e. when the key is actually the value
-        case value.length === 0:
-          return {
-            ...acc,
-            [defaultKey]: key,
-          }
-        // if the key is the default key then use that
-        case key === defaultKey:
-          return {
-            ...acc,
-            [key]: value,
-          }
-        // support old style tags, explicitly used `tag:key:value`
-        // TODO: this will need to be optional
-        case ['tag'].includes(key): {
-          const [k, v = ''] = value.split(kvSeparatorRegex)
-          return {
-            ...acc,
-            tags: {
-              ...tags,
-              [shortFilters[k] ?? k]: v,
-            },
-          }
-        }
-        // or if the key is anything other than the defaultKey, tag, or tags, turn
-        // it into a label
-        default: {
-          // explicitly used `label:key:value` or, if not, it was just `key:value`
-          const [k, v = ''] = ['label'].includes(key) ? value.split(kvSeparatorRegex) : [key, value]
-          return {
-            ...acc,
-            labels: {
-              ...labels,
-              [shortFilters[k] ?? k]: v,
-            },
-          }
-        }
-      }
-    }, {} as {
-      labels?: Record<string, string>
-      tags?: Record<string, string>
-      [key: string]: string | Record<string, string> | undefined
-    })
-
-
-  },
-
-  search(query: string, options: SearchOptions = {}) {
-    const { labels = {}, tags = {}, ...rest } = Resource.parseSearch(query, options)
-
-    return {
-      ...rest,
-      ...(Object.keys(labels).length > 0 ? Object.fromEntries(Object.entries(labels).map(([key, value]) => [`filter[labels.${key}]`, value])) : {}),
-      ...(Object.keys(tags).length > 0 ? { tag: Object.entries(tags).map(([key, value]) => `${key}${value.length > 0 ? `:${value}` : ''}`) }: {}),
-    } as typeof rest & { tag?: string[] }
+  search(...rest: Parameters<typeof search>) {
+    return search(...rest)
   },
 
   fromObject(partialResource: KumaResource) {
     const labels = 'labels' in partialResource ? partialResource.labels ?? {} : {}
     const mesh = 'mesh' in partialResource ? partialResource.mesh : labels['kuma.io/mesh'] ?? ''
     const namespace = labels['k8s.kuma.io/namespace'] ?? ''
-    const name =  labels['kuma.io/display-name'] ?? partialResource.name ?? ''
-    const zone =  labels['kuma.io/origin'] === 'zone' ? labels['kuma.io/zone'] ?? '' : ''
+    const name = labels['kuma.io/display-name'] ?? partialResource.name ?? ''
+    const zone = labels['kuma.io/origin'] === 'zone' ? labels['kuma.io/zone'] ?? '' : ''
 
     const kri = 'kri' in partialResource ? partialResource.kri ?? '' : ''
 
