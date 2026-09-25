bbc4f092b804616999f7fe18e8a68929f7265121
schogges <moritz.fleck@konghq.com>
2026-09-25 11:54:51 +0200

    change fallbacks for hasFromTargetRef and isFromAsRules
    Signed-off-by: schogges <moritz.fleck@konghq.com>

--- packages/kuma-gui/src/app/resources/data/ResourceTypeDescriptor.ts
+++ packages/kuma-gui/src/app/resources/data/ResourceTypeDescriptor.ts
@@ -64,8 +64,8 @@ export const ResourceTypeDescriptor = {
           ...item.policy,
           // Backfills some legacy flags that are gone from OAS, but we keep reading them in the views.
           isTargetRef: 'isTargetRef' in item.policy ? item.policy.isTargetRef as boolean : true,
-          hasFromTargetRef: 'hasFromTargetRef' in item.policy ? item.policy.hasFromTargetRef as boolean : true,
-          isFromAsRules: 'isFromAsRules' in item.policy ? item.policy.isFromAsRules as boolean : true,
+          hasFromTargetRef: 'hasFromTargetRef' in item.policy ? item.policy.hasFromTargetRef as boolean : false,
+          isFromAsRules: 'isFromAsRules' in item.policy ? item.policy.isFromAsRules as boolean : false,
         },
       }),
       categories: resourceCategories.get(item.name.toLowerCase()) ?? [],
